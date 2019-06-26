78951f7ab80ce28452b1d6daaa1c4ab5f06acfbb
Johannes Passing <jpassing@google.com>
2019-06-26 13:00:38 +0200

    Restore NuGet packages before build
    Change-Id: I02645389e06b3aed6ddc9434908821fd7fa1b83d

--- build.ps1
+++ build.ps1
@@ -30,7 +30,7 @@ if ($LastExitCode -ne 0)
 }
 
 Write-Host "=== Build solution ==="
-#& $Msbuild  "/t:Plugin_Google_CloudIap:Rebuild" "/p:Configuration=Release;Platform=x86" | Out-Default
+& $Msbuild  "/t:Plugin_Google_CloudIap:Rebuild" "/p:Configuration=Release;Platform=x86" | Out-Default
 if ($LastExitCode -ne 0)
 {
     exit $LastExitCode
