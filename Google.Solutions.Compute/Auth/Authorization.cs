cbc4e3c390a21606bf392486f07af207098f97a3
Johannes Passing <jpassing@google.com>
2019-10-25 14:42:30 +1100

    Support re-auth for OAuth
    Allow user to authorize the app again whenever an
    `invalid_grant` error occurs.
    
    * Catch error and trigger re-auth in WaitDialog
    * Modified code so that new credentials are used
      after a re-auth.
    
    Change-Id: Ia0bd405661d09b1890d433ec01912fdaadbc0e6a

--- Google.Solutions.Compute/Auth/Authorization.cs
+++ Google.Solutions.Compute/Auth/Authorization.cs
@@ -26,6 +26,7 @@ using System;
 using System.Threading.Tasks;
 using System.Threading;
 using System.IO;
+using Google.Apis.Http;
 
 namespace Google.Solutions.Compute.Auth
 {
@@ -33,29 +34,36 @@ namespace Google.Solutions.Compute.Auth
     {
         ICredential Credential { get; }
 
-        string UserId { get; }
-
         Task RevokeAsync();
+
+        Task ReauthorizeAsync();
     }
 
     public class OAuthAuthorization : IAuthorization
     {
         public const string StoreUserId = "oauth";
 
-        private readonly UserCredential credential;
+        private readonly GoogleAuthorizationCodeFlow.Initializer initializer;
+        private readonly string closePageReponse;
         private readonly IDataStore credentialStore;
 
-        public OAuthAuthorization(
-            UserCredential credential,
-            IDataStore credentialStore)
+        // The OAuth credential change after each reauth. Therefore, use
+        // a SwappableCredential as indirection.
+        private readonly SwappableCredential credential;
+
+        internal OAuthAuthorization(
+            GoogleAuthorizationCodeFlow.Initializer initializer,
+            string closePageReponse,
+            IDataStore credentialStore,
+            ICredential initialCredential)
         {
-            this.credential = credential;
+            this.initializer = initializer;
+            this.closePageReponse = closePageReponse;
             this.credentialStore = credentialStore;
+            this.credential = new SwappableCredential(initialCredential);
         }
 
         public ICredential Credential => this.credential;
-
-        public string UserId => this.credential.UserId;
 
         public Task RevokeAsync()
         {
@@ -78,12 +86,16 @@ namespace Google.Solutions.Compute.Auth
 
                 if (!installedApp.ShouldRequestAuthorizationCode(existingTokenResponse))
                 {
+                    // N.B. Do not dispose the GoogleAuthorizationCodeFlow as it might
+                    // be needed for re-auth later.
                     return new OAuthAuthorization(
+                        initializer,
+                        closePageReponse,
+                        initializer.DataStore,
                         new UserCredential(
                             new GoogleAuthorizationCodeFlow(initializer),
                             OAuthAuthorization.StoreUserId,
-                            existingTokenResponse),
-                        initializer.DataStore);
+                            existingTokenResponse));
                 }
                 else
                 {
@@ -96,17 +108,58 @@ namespace Google.Solutions.Compute.Auth
             GoogleAuthorizationCodeFlow.Initializer initializer,
             string closePageReponse)
         {
-            using (var flow = new GoogleAuthorizationCodeFlow(initializer))
-            {
-                var installedApp = new AuthorizationCodeInstalledApp(
-                    flow,
-                    new LocalServerCodeReceiver(closePageReponse));
+            // N.B. Do not dispose the GoogleAuthorizationCodeFlow as it might
+            // be needed for re-auth later.
+            var installedApp = new AuthorizationCodeInstalledApp(
+                new GoogleAuthorizationCodeFlow(initializer),
+                new LocalServerCodeReceiver(closePageReponse));
 
-                return new OAuthAuthorization(
-                    await installedApp.AuthorizeAsync(
-                        OAuthAuthorization.StoreUserId,
-                        CancellationToken.None),
-                    initializer.DataStore);
+            return new OAuthAuthorization(
+                initializer,
+                closePageReponse,
+                initializer.DataStore,
+                await installedApp.AuthorizeAsync(
+                    OAuthAuthorization.StoreUserId,
+                    CancellationToken.None));
+        }
+
+        public async Task ReauthorizeAsync()
+        {
+            // As this is a 3p OAuth app, we do not support Gnubby/Password-based
+            // reauth. Instead, we simply trigger a new authorization (code flow).
+            var installedApp = new AuthorizationCodeInstalledApp(
+                new GoogleAuthorizationCodeFlow(this.initializer),
+                new LocalServerCodeReceiver(this.closePageReponse));
+
+            this.credential.SwapCredential(await installedApp.AuthorizeAsync(
+                OAuthAuthorization.StoreUserId,
+                CancellationToken.None));
+        }
+
+        private class SwappableCredential : ICredential
+        {
+            private ICredential currentCredential;
+
+            public SwappableCredential(ICredential curentCredential)
+            {
+                this.currentCredential = curentCredential;
+            }
+
+            public void SwapCredential(ICredential credential)
+            {
+                this.currentCredential = credential;
+            }
+
+            public void Initialize(ConfigurableHttpClient httpClient)
+            {
+                this.currentCredential.Initialize(httpClient);
+            }
+
+            public Task<string> GetAccessTokenForRequestAsync(
+                string authUri = null, 
+                CancellationToken cancellationToken = default(CancellationToken))
+            {
+                return this.currentCredential.GetAccessTokenForRequestAsync(authUri, cancellationToken);
             }
         }
     }
@@ -119,18 +172,14 @@ namespace Google.Solutions.Compute.Auth
         private readonly IDataStore credentialStore;
 
         private GcloudAuthorization(
-            string userId,
             GoogleCredential credential,
             IDataStore credentialStore)
         {
-            this.UserId = userId;
             this.credential = credential;
             this.credentialStore = credentialStore;
         }
 
         public ICredential Credential => this.credential;
-
-        public string UserId { get; }
 
         public Task RevokeAsync()
         {
@@ -152,7 +201,6 @@ namespace Google.Solutions.Compute.Auth
                 account.Name == await credentialStore.GetAsync<string>(StoreUserId))
             {
                 return new GcloudAuthorization(
-                    account.Name,
                     account.Credential,
                     credentialStore);
             }
@@ -167,7 +215,6 @@ namespace Google.Solutions.Compute.Auth
             IDataStore credentialStore)
         {
             var authorization = new GcloudAuthorization(
-                account.Name,
                 account.Credential,
                 credentialStore);
 
@@ -179,6 +226,12 @@ namespace Google.Solutions.Compute.Auth
             }
 
             return authorization;
+        }
+
+        public Task ReauthorizeAsync()
+        {
+            throw new NotImplementedException(
+                "Reauth is currently not supported when using gcloud for authorization");
         }
     }
 }
