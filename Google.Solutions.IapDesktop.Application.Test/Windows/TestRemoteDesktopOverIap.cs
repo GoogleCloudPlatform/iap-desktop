9b10b771a32035082cb754ac9f8fee520bb4fe39
Johannes Passing <jpassing@google.com>
2020-03-25 12:09:57 +0100

    Add RDP tests
    Change-Id: If4db9a1af450fb0f6030c07d673d1f060ee8670e

--- Google.Solutions.IapDesktop.Application.Test/Windows/TestRemoteDesktopOverIap.cs
+++ Google.Solutions.IapDesktop.Application.Test/Windows/TestRemoteDesktopOverIap.cs
@@ -85,44 +85,20 @@ namespace Google.Solutions.IapDesktop.Application.Test.Windows
             }
         }
 
-        [Test]
-        public void WhenAttemptServerAuthentication_ThenWarningIsShown()
-        {
-            Assert.Inconclusive();
-        }
+        //
+        // There's no reliable way to dismiss the warning/error, so these tests seem 
+        // challenging to implement.
+        //
+        //[Test]
+        //public void WhenAttemptServerAuthentication_ThenWarningIsShown()
+        //{
+        //}
 
-        [Test]
-        public async Task WhenRequireServerAuthentication_ThenConnectionFails(
-            [WindowsInstance] InstanceRequest testInstance)
-        {
-            await testInstance.AwaitReady();
-
-            using (var tunnel = RdpTunnel.Create(testInstance.InstanceReference))
-            using (var gceAdapter = new ComputeEngineAdapter(this.serviceProvider.GetService<IAuthorizationService>()))
-            {
-                var credentials = await gceAdapter.ResetWindowsUserAsync(
-                    testInstance.InstanceReference,
-                    "test",
-                    CancellationToken.None);
-
-                var rdpService = new RemoteDesktopService(this.serviceProvider);
-                var session = rdpService.Connect(
-                    testInstance.InstanceReference,
-                    "localhost",
-                    (ushort)tunnel.LocalPort,
-                    new VmInstanceSettings()
-                    {
-                        Username = credentials.UserName,
-                        Password = credentials.SecurePassword,
-                        AuthenticationLevel = RdpAuthenticationLevel.RequireServerAuthentication
-                    });
-
-                AwaitEvent<RemoteDesktopConnectionFailedEvent>();
-                Assert.IsNotNull(this.ExceptionShown);
-                Assert.IsInstanceOf(typeof(RdpDisconnectedException), this.ExceptionShown);
-                Assert.AreEqual(2055, ((RdpDisconnectedException)this.ExceptionShown).DisconnectReason);
-            }
-        }
+        //[Test]
+        //public void WhenRequireServerAuthentication_ThenConnectionFails(
+        //    [WindowsInstance] InstanceRequest testInstance)
+        //{
+        //}
 
         [Test]
         public async Task WhenCredentialsValid_ThenConnectingSucceeds(
@@ -151,7 +127,7 @@ namespace Google.Solutions.IapDesktop.Application.Test.Windows
                     CancellationToken.None);
 
                 var rdpService = new RemoteDesktopService(this.serviceProvider);
-                rdpService.Connect(
+                var session = rdpService.Connect(
                     testInstance.InstanceReference,
                     "localhost",
                     (ushort)tunnel.LocalPort,
@@ -168,6 +144,18 @@ namespace Google.Solutions.IapDesktop.Application.Test.Windows
 
                 AwaitEvent<RemoteDesktopConnectionSuceededEvent>();
                 Assert.IsNull(this.ExceptionShown);
+
+
+                RemoteDesktopWindowClosedEvent expectedEvent = null;
+
+                this.serviceProvider.GetService<IEventService>()
+                    .BindHandler<RemoteDesktopWindowClosedEvent>(e =>
+                    {
+                        expectedEvent = e;
+                    });
+                session.Close();
+
+                Assert.IsNotNull(expectedEvent);
             }
         }
     }
