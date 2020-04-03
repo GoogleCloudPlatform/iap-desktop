b6a4fc901060ffd30e37925a5f7a855a7389ca85
Johannes Passing <jpassing@google.com>
2020-04-03 18:07:36 +0200

    Unwrap TargetInvocationExceptions
    Change-Id: If745302d1399f6936ff17ab0d53652a33f506df2

---
+++ Google.Solutions.IapDesktop.Application.Test/TestExceptionExtensions.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using System.Reflection;
+
+namespace Google.Solutions.IapDesktop.Application.Test
+{
+    [TestFixture]
+    public class TestExceptionExtensions
+    {
+        [Test]
+        public void WhenRegularException_UnwrapDoesNothing()
+        {
+            var ex = new ApplicationException();
+
+            var unwrapped = ex.Unwrap();
+
+            Assert.AreSame(ex, unwrapped);
+        }
+
+        [Test]
+        public void WhenAggregateException_UnwrapReturnsFirstInnerException()
+        {
+            var inner1 = new ApplicationException();
+            var inner2 = new ApplicationException();
+            var aggregate = new AggregateException(inner1, inner2);
+
+            var unwrapped = aggregate.Unwrap();
+
+            Assert.AreSame(inner1, unwrapped);
+        }
+
+        [Test]
+        public void WhenTargetInvocationException_UnwrapReturnsInnerException()
+        {
+            var inner = new ApplicationException();
+            var target = new TargetInvocationException("", inner);
+
+            var unwrapped = target.Unwrap();
+
+            Assert.AreSame(inner, unwrapped);
+        }
+    }
+}
