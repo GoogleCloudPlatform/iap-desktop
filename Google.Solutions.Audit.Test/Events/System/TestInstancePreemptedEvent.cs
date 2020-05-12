1ea1ea7091a9bf56f05baf33868b9d49448521e0
Johannes Passing <jpassing@google.com>
2020-05-12 16:20:58 +0200

    Add test for preemption
    Change-Id: Ifdc183bd13091af351db13c49df379112cb65436

--- Google.Solutions.Audit.Test/Events/System/TestInstancePreemptedEvent.cs
+++ Google.Solutions.Audit.Test/Events/System/TestInstancePreemptedEvent.cs
@@ -33,7 +33,57 @@ namespace Google.Solutions.Audit.Test.Events.System
         [Test]
         public void WhenSeverityIsInfo_ThenFieldsAreExtracted()
         {
-            Assert.Inconclusive();
+            var json = @"
+                {
+                   'protoPayload': {
+                     '@type': 'type.googleapis.com/google.cloud.audit.AuditLog',
+                     'status': {},
+                     'authenticationInfo': {
+                       'principalEmail': 'system@google.com'
+                     },
+                     'requestMetadata': {
+                       'requestAttributes': {},
+                       'destinationAttributes': {}
+                     },
+                     'serviceName': 'compute.googleapis.com',
+                     'methodName': 'compute.instances.preempted',
+                     'resourceName': 'projects/project-1/zones/us-central1-a/instances/instance-1',
+                     'request': {
+                       '@type': 'type.googleapis.com/compute.instances.preempted'
+                     }
+                   },
+                   'insertId': 'epf7fkd2vsm',
+                   'resource': {
+                     'type': 'gce_instance',
+                     'labels': {
+                       'zone': 'us-central1-a',
+                       'instance_id': '2162224123123123213',
+                       'project_id': 'project-1'
+                     }
+                   },
+                   'timestamp': '2020-05-11T15:56:44.808Z',
+                   'severity': 'INFO',
+                   'logName': 'projects/project-1/logs/cloudaudit.googleapis.com%2Fsystem_event',
+                   'operation': {
+                     'id': 'systemevent-1589212587537-5a56163c0b559-3e94b149-65935fdf',
+                     'producer': 'compute.instances.preempted',
+                     'first': true,
+                     'last': true
+                   },
+                   'receiveTimestamp': '2020-05-11T15:56:45.533622382Z'
+                 }";
+
+            var r = LogRecord.Deserialize(json);
+            Assert.IsTrue(InstancePreemptedEvent.IsInstancePreemptedEvent(r));
+
+            var e = (InstancePreemptedEvent)r.ToEvent();
+
+            Assert.AreEqual(2162224123123123213, e.InstanceId);
+            Assert.AreEqual("INFO", e.Severity);
+            Assert.IsNull(e.Status);
+            Assert.AreEqual(
+                new VmInstanceReference("project-1", "us-central1-a", "instance-1"),
+                e.InstanceReference);
         }
     }
 }
