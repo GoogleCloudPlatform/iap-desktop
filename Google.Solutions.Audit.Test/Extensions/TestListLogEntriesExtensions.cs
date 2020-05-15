d7c0cceaa883a029db19dde8bd9a9aa77ed127b4
Johannes Passing <jpassing@google.com>
2020-05-15 17:36:25 +0200

    Add copyright headers
    Change-Id: Idba13dcd81b1966b23f49142cee35dab0c4c6897

--- Google.Solutions.Audit.Test/Extensions/TestListLogEntriesExtensions.cs
+++ Google.Solutions.Audit.Test/Extensions/TestListLogEntriesExtensions.cs
@@ -1,4 +1,25 @@
-﻿using Google.Apis.Logging.v2;
+﻿//
+// Copyright 2019 Google LLC
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+using Google.Apis.Logging.v2;
 using Google.Apis.Logging.v2.Data;
 using Google.Apis.Services;
 using Google.Solutions.Compute.Test.Env;
@@ -10,7 +31,6 @@ using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Google.Solutions.LogAnalysis.Test.Extensions
@@ -30,7 +50,6 @@ namespace Google.Solutions.LogAnalysis.Test.Extensions
             {
                 HttpClientInitializer = Defaults.GetCredential()
             });
-
 
             var request = new ListLogEntriesRequest()
             {
