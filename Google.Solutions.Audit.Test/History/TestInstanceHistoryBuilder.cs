e733884529a7726a87e12be694b895cdb0370b87
Johannes Passing <jpassing@google.com>
2020-05-15 09:23:04 +0200

    Add InstanceHistoryBuilder
    Change-Id: I076cda83e911b25a009ae0fb4674fcf8184568d2

---
+++ Google.Solutions.Audit.Test/History/TestInstanceHistoryBuilder.cs
@@ -0,0 +1,367 @@
+﻿using Google.Solutions.Compute;
+using Google.Solutions.LogAnalysis.History;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Google.Solutions.LogAnalysis.Test.History
+{
+    [TestFixture]
+    public class TestInstanceHistoryBuilder
+    {
+        private static readonly VmInstanceReference SampleReference = new VmInstanceReference("pro", "zone", "name");
+        private static readonly GlobalResourceReference SampleImage = GlobalResourceReference.FromString("projects/project-1/global/images/image-1");
+
+        [Test]
+        public void WhenInstanceIsDeletedAndNoEventsRegistered_ThenImageIsNull()
+        {
+            var b = InstanceHistoryBuilder.ForDeletedInstance(1, SampleReference);
+            var i = b.Build();
+
+            Assert.AreEqual(1, i.InstanceId);
+            Assert.AreEqual(SampleReference, i.Reference);
+
+            Assert.IsNull(i.Image);
+        }
+
+        //---------------------------------------------------------------------
+        // Tenancy.
+        //---------------------------------------------------------------------
+
+        [Test]
+        public void WhenInstanceDeletedAndNoPlacementOrInsertRegistered_TheTenancyIsUnknown()
+        {
+            var b = InstanceHistoryBuilder.ForDeletedInstance(1, SampleReference);
+            var i = b.Build();
+
+            Assert.AreEqual(1, i.InstanceId);
+
+            Assert.AreEqual(0, i.Placements.Count());
+            Assert.AreEqual(Tenancy.Unknown, i.Tenancy);
+        }
+
+        [Test]
+        public void WhenInstanceDeletedAndInsertRegistered_TheTenancyIsFleet()
+        {
+            var b = InstanceHistoryBuilder.ForDeletedInstance(1, SampleReference);
+            b.OnInsert(new DateTime(2019, 12, 30), SampleImage);
+            var i = b.Build();
+
+            Assert.AreEqual(Tenancy.Fleet, i.Tenancy);
+        }
+
+        [Test]
+        public void WhenInstanceDeletedAndPlacementRegistered_TheTenancyIsSoleTenant()
+        {
+            var b = InstanceHistoryBuilder.ForDeletedInstance(1, SampleReference);
+            b.OnSetPlacement("server-1", new DateTime(2019, 12, 30));
+
+            var i = b.Build();
+
+            Assert.AreEqual(Tenancy.SoleTenant, i.Tenancy);
+        }
+
+        [Test]
+        public void WhenInstanceExistsAndIsSoleTenant_ThenTenancyIsSoleTenant()
+        {
+            var b = InstanceHistoryBuilder.ForExistingInstance(
+                1,
+                SampleReference,
+                SampleImage,
+                DateTime.Now,
+                Tenancy.SoleTenant);
+            Assert.AreEqual(Tenancy.SoleTenant, b.Tenancy);
+        }
+
+        //---------------------------------------------------------------------
+        // Placements for existing instances.
+        //---------------------------------------------------------------------
+
+        [Test]
+        public void WhenRedundantPlacementsRegistered_ThenSecondPlacementIsIgnored()
+        {
+            var b = InstanceHistoryBuilder.ForExistingInstance(
+                1,
+                SampleReference,
+                SampleImage,
+                new DateTime(2019, 12, 31),
+                Tenancy.SoleTenant);
+
+            b.OnSetPlacement("server-1", new DateTime(2019, 12, 30));
+            b.OnSetPlacement("server-1", new DateTime(2019, 12, 29));
+
+            var placements = b.Build().Placements.ToList();
+            Assert.AreEqual(1, placements.Count());
+            Assert.AreEqual(new DateTime(2019, 12, 29), placements[0].From);
+            Assert.AreEqual(new DateTime(2019, 12, 31), placements[0].To);
+        }
+
+        [Test]
+        public void WhenPlacementsWithSameServerIdAfterStopRegistered_ThenPlacementsAreKept()
+        {
+            var b = InstanceHistoryBuilder.ForExistingInstance(
+                1,
+                SampleReference,
+                SampleImage,
+                new DateTime(2019, 12, 31),
+                Tenancy.SoleTenant);
+
+            b.OnSetPlacement("server-1", new DateTime(2019, 12, 30));
+            b.OnStop(new DateTime(2019, 12, 29));
+            b.OnSetPlacement("server-1", new DateTime(2019, 12, 28));
+
+            var placements = b.Build().Placements.ToList();
+            Assert.AreEqual(2, placements.Count());
+            Assert.AreEqual(new DateTime(2019, 12, 28), placements[0].From);
+            Assert.AreEqual(new DateTime(2019, 12, 29), placements[0].To);
+            Assert.AreEqual(new DateTime(2019, 12, 30), placements[1].From);
+            Assert.AreEqual(new DateTime(2019, 12, 31), placements[1].To);
+        }
+
+        [Test]
+        public void WhenPlacementsWithDifferentServerIdsRegistered_ThenPlacementsAreKept()
+        {
+            var b = InstanceHistoryBuilder.ForExistingInstance(
+                1,
+                SampleReference,
+                SampleImage,
+                new DateTime(2019, 12, 31),
+                Tenancy.SoleTenant);
+            b.OnSetPlacement("server-1", new DateTime(2019, 12, 30));
+            b.OnSetPlacement("server-2", new DateTime(2019, 12, 29));
+
+            var placements = b.Build().Placements.ToList();
+            Assert.AreEqual(2, placements.Count());
+            Assert.AreEqual(new DateTime(2019, 12, 29), placements[0].From);
+            Assert.AreEqual(new DateTime(2019, 12, 30), placements[0].To);
+            Assert.AreEqual(new DateTime(2019, 12, 30), placements[1].From);
+            Assert.AreEqual(new DateTime(2019, 12, 31), placements[1].To);
+        }
+
+        [Test]
+        public void WhenInstanceRunningAndSinglePlacementRegistered_ThenInstanceContainsRightPlacements()
+        {
+            var b = InstanceHistoryBuilder.ForExistingInstance(
+                1,
+                SampleReference,
+                SampleImage,
+                new DateTime(2019, 12, 31),
+                Tenancy.SoleTenant);
+            b.OnSetPlacement("server-1", new DateTime(2019, 12, 30));
+
+            var i = b.Build();
+
+            Assert.AreEqual(1, i.InstanceId);
+            Assert.AreEqual(1, i.Placements.Count());
+
+            var placement = i.Placements.First();
+            Assert.AreEqual("server-1", placement.ServerId);
+            Assert.AreEqual(new DateTime(2019, 12, 30), placement.From);
+            Assert.AreEqual(new DateTime(2019, 12, 31), placement.To);
+        }
+
+        [Test]
+        public void WhenInstanceRunningAndMultiplePlacementsRegistered_ThenInstanceContainsRightPlacements()
+        {
+            var b = InstanceHistoryBuilder.ForExistingInstance(
+                1,
+                SampleReference,
+                SampleImage,
+                new DateTime(2019, 12, 31),
+                Tenancy.SoleTenant);
+            b.OnSetPlacement("server-2", new DateTime(2019, 12, 30));
+            b.OnSetPlacement("server-1", new DateTime(2019, 12, 29));
+
+            var i = b.Build();
+
+            var placements = i.Placements.ToList();
+            Assert.AreEqual(2, i.Placements.Count());
+
+            Assert.AreEqual("server-1", placements[0].ServerId);
+            Assert.AreEqual(new DateTime(2019, 12, 29), placements[0].From);
+            Assert.AreEqual(new DateTime(2019, 12, 30), placements[0].To);
+
+            Assert.AreEqual("server-2", placements[1].ServerId);
+            Assert.AreEqual(new DateTime(2019, 12, 30), placements[1].From);
+            Assert.AreEqual(new DateTime(2019, 12, 31), placements[1].To);
+        }
+
+        [Test]
+        public void WhenInstanceRunningAndMultiplePlacementWithStopsInBetweenRegistered_ThenInstanceContainsRightPlacements()
+        {
+            var b = InstanceHistoryBuilder.ForExistingInstance(
+                1,
+                SampleReference,
+                SampleImage,
+                new DateTime(2019, 12, 31),
+                Tenancy.SoleTenant);
+            b.OnSetPlacement("server-2", new DateTime(2019, 12, 30));
+            b.OnStop(new DateTime(2019, 12, 29));
+            b.OnSetPlacement("server-1", new DateTime(2019, 12, 28));
+
+            var i = b.Build();
+
+            var placements = i.Placements.ToList();
+            Assert.AreEqual(2, i.Placements.Count());
+
+            Assert.AreEqual("server-1", placements[0].ServerId);
+            Assert.AreEqual(new DateTime(2019, 12, 28), placements[0].From);
+            Assert.AreEqual(new DateTime(2019, 12, 29), placements[0].To);
+
+            Assert.AreEqual("server-2", placements[1].ServerId);
+            Assert.AreEqual(new DateTime(2019, 12, 30), placements[1].From);
+            Assert.AreEqual(new DateTime(2019, 12, 31), placements[1].To);
+        }
+
+
+        //---------------------------------------------------------------------
+        // Placement events for deleted instances.
+        //---------------------------------------------------------------------
+
+        [Test]
+        public void WhenInstanceDeletedAndPlacementRegistered_ThenInstanceIsDefunct()
+        {
+            var b = InstanceHistoryBuilder.ForDeletedInstance(
+                1,
+                SampleReference);
+            b.OnSetPlacement("server-1", new DateTime(2019, 12, 30));
+
+            Assert.IsTrue(b.IsDefunct);
+        }
+
+        [Test]
+        public void WhenInstanceDeletedAndSinglePlacementRegistered_ThenInstanceContainsRightPlacements()
+        {
+            var b = InstanceHistoryBuilder.ForDeletedInstance(
+                1,
+                SampleReference);
+            b.OnStop(new DateTime(2019, 12, 31));
+            b.OnSetPlacement("server-1", new DateTime(2019, 12, 30));
+
+            Assert.AreEqual(Tenancy.SoleTenant, b.Tenancy);
+            var i = b.Build();
+
+            Assert.AreEqual(1, i.InstanceId);
+            Assert.AreEqual(1, i.Placements.Count());
+
+            var placement = i.Placements.First();
+            Assert.AreEqual("server-1", placement.ServerId);
+            Assert.AreEqual(new DateTime(2019, 12, 30), placement.From);
+            Assert.AreEqual(new DateTime(2019, 12, 31), placement.To);
+        }
+
+        [Test]
+        public void WhenInstanceDeletedAndMultiplePlacementsRegistered_ThenInstanceContainsRightPlacements()
+        {
+            var b = InstanceHistoryBuilder.ForDeletedInstance(
+                1,
+                SampleReference);
+            b.OnStop(new DateTime(2019, 12, 31));
+            b.OnSetPlacement("server-2", new DateTime(2019, 12, 30));
+            b.OnSetPlacement("server-1", new DateTime(2019, 12, 29));
+
+            Assert.AreEqual(Tenancy.SoleTenant, b.Tenancy);
+            var i = b.Build();
+
+            var placements = i.Placements.ToList();
+            Assert.AreEqual(2, i.Placements.Count());
+
+            Assert.AreEqual("server-1", placements[0].ServerId);
+            Assert.AreEqual(new DateTime(2019, 12, 29), placements[0].From);
+            Assert.AreEqual(new DateTime(2019, 12, 30), placements[0].To);
+
+            Assert.AreEqual("server-2", placements[1].ServerId);
+            Assert.AreEqual(new DateTime(2019, 12, 30), placements[1].From);
+            Assert.AreEqual(new DateTime(2019, 12, 31), placements[1].To);
+        }
+
+        [Test]
+        public void WhenInstanceDeletedAndMultiplePlacementWithStopsInBetweenRegistered_ThenInstanceContainsRightPlacements()
+        {
+            var b = InstanceHistoryBuilder.ForDeletedInstance(
+                1,
+                SampleReference);
+            b.OnStop(new DateTime(2019, 12, 31));
+            b.OnSetPlacement("server-2", new DateTime(2019, 12, 30));
+            b.OnStop(new DateTime(2019, 12, 29));
+            b.OnSetPlacement("server-1", new DateTime(2019, 12, 28));
+
+            Assert.AreEqual(Tenancy.SoleTenant, b.Tenancy);
+            var i = b.Build();
+
+            var placements = i.Placements.ToList();
+            Assert.AreEqual(2, i.Placements.Count());
+
+            Assert.AreEqual("server-1", placements[0].ServerId);
+            Assert.AreEqual(new DateTime(2019, 12, 28), placements[0].From);
+            Assert.AreEqual(new DateTime(2019, 12, 29), placements[0].To);
+
+            Assert.AreEqual("server-2", placements[1].ServerId);
+            Assert.AreEqual(new DateTime(2019, 12, 30), placements[1].From);
+            Assert.AreEqual(new DateTime(2019, 12, 31), placements[1].To);
+        }
+
+
+        //---------------------------------------------------------------------
+        // More information needed.
+        //---------------------------------------------------------------------
+
+        [Test]
+        public void WhenInstanceExists_ThenNoMoreInformationNeeded()
+        {
+            var b = InstanceHistoryBuilder.ForExistingInstance(
+                1,
+                SampleReference,
+                SampleImage,
+                DateTime.Now,
+                Tenancy.SoleTenant);
+            Assert.IsFalse(b.IsMoreInformationNeeded);
+        }
+
+        [Test]
+        public void WhenInstancDeletedAndNoPlacementRegistered_ThenMoreInformationNeeded()
+        {
+            var b = InstanceHistoryBuilder.ForDeletedInstance(
+                1,
+                SampleReference);
+            Assert.IsTrue(b.IsMoreInformationNeeded);
+        }
+
+        [Test]
+        public void WhenInstanceDeletedAndPlacementRegisteredButNoInsertRegistered_ThenMoreInformationNeeded()
+        {
+            var b = InstanceHistoryBuilder.ForDeletedInstance(
+                1,
+                SampleReference);
+            b.OnStop(new DateTime(2019, 12, 31));
+            b.OnSetPlacement("server-2", new DateTime(2019, 12, 30));
+            Assert.IsTrue(b.IsMoreInformationNeeded);
+        }
+
+        [Test]
+        public void WhenInstanceDeletedAndPlacementAndInsertRegistered_ThenNoMoreInformationNeeded()
+        {
+            var b = InstanceHistoryBuilder.ForDeletedInstance(
+                1,
+                SampleReference);
+            b.OnStop(new DateTime(2019, 12, 31));
+            b.OnSetPlacement("server-2", new DateTime(2019, 12, 30));
+            b.OnInsert(new DateTime(2019, 12, 29), SampleImage);
+            Assert.IsFalse(b.IsMoreInformationNeeded);
+        }
+
+        [Test]
+        public void WhenInstanceDeletedAndInsertRegistered_ThenNoMoreInformationNeeded()
+        {
+            var b = InstanceHistoryBuilder.ForDeletedInstance(
+                1,
+                SampleReference);
+            b.OnInsert(new DateTime(2019, 12, 29), SampleImage);
+            Assert.IsFalse(b.IsMoreInformationNeeded);
+        }
+
+    }
+}
---
+++ Google.Solutions.Audit/History/InstanceHistoryBuilder.cs
@@ -0,0 +1,230 @@
+﻿//
+// Copyright 2019 Google LLC
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+using Google.Solutions.Compute;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Google.Solutions.LogAnalysis.History
+{
+    /// <summary>
+    /// Reconstructs the history of an instance by analyzing
+    /// events in reverse chronological order.
+    /// </summary>
+    internal class InstanceHistoryBuilder
+    {
+        public long InstanceId { get; }
+        private readonly LinkedList<SoleTenantPlacement> placements = new LinkedList<SoleTenantPlacement>();
+
+        public Tenancy Tenancy { get; private set; }
+        public DateTime? LastStoppedOn { get; private set; }
+
+        public bool IsDefunct { get; private set; } = false;
+
+        public GlobalResourceReference Image { get; set; }
+        public VmInstanceReference Reference { get; set; }
+
+        private DateTime lastEventDate = DateTime.MaxValue;
+
+        private void AddPlacement(SoleTenantPlacement placement)
+        {
+            if (this.placements.Any())
+            {
+                var subsequentPlacement = this.placements.First();
+                if (placement.To == subsequentPlacement.From &&
+                    placement.ServerId == subsequentPlacement.ServerId)
+                {
+                    // Placement are right adjacent -> merge.
+                    placement = new SoleTenantPlacement(
+                        placement.ServerId,
+                        placement.From,
+                        subsequentPlacement.To);
+                    this.placements.RemoveFirst();
+                }
+            }
+
+            this.placements.AddFirst(placement);
+        }
+
+        //---------------------------------------------------------------------
+        // Ctor
+        //---------------------------------------------------------------------
+
+        private InstanceHistoryBuilder(
+            long instanceId,
+            VmInstanceReference reference,
+            GlobalResourceReference image,
+            DateTime? lastStoppedOn,
+            Tenancy tenancy)
+        {
+            Debug.Assert(instanceId != 0);
+
+            this.InstanceId = instanceId;
+            this.LastStoppedOn = lastStoppedOn;
+            this.Reference = reference;
+            this.Image = image;
+            this.Tenancy = tenancy;
+        }
+
+        internal static InstanceHistoryBuilder ForExistingInstance(
+            long instanceId,
+            VmInstanceReference reference,
+            GlobalResourceReference image,
+            DateTime lastSeen,
+            Tenancy tenancy)
+        {
+            return new InstanceHistoryBuilder(
+                instanceId,
+                reference,
+                image,
+                lastSeen,   // Pretend it was stopped when it was last seen.
+                tenancy);
+        }
+
+        internal static InstanceHistoryBuilder ForDeletedInstance(long instanceId, VmInstanceReference reference)
+        {
+            return new InstanceHistoryBuilder(
+                instanceId,
+                reference,
+                null,
+                (DateTime?)null,    // Not clear yet when it was stopped
+                Tenancy.Unknown);
+        }
+
+        public InstanceHistory Build()
+        {
+            return new InstanceHistory(
+                this.InstanceId,
+                this.Reference,
+                this.Image,
+                this.Tenancy,
+                this.placements);
+        }
+
+        public bool IsMoreInformationNeeded
+        {
+            get
+            {
+                if (this.Tenancy == Tenancy.Unknown)
+                {
+                    return true;
+                }
+                else if (this.Tenancy == Tenancy.SoleTenant)
+                {
+                    return this.Image == null;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public void OnInsert(DateTime date, GlobalResourceReference image)
+        {
+            Debug.Assert(date < this.lastEventDate);
+            this.lastEventDate = date;
+
+            // NB. We might get multiple calls for a single instance, each providing some, but
+            // potentially not all information.
+
+            if (this.Tenancy == Tenancy.Unknown)
+            {
+                // We have not seen any OnSetPlacement call - by now, it is therefore 
+                // clear that this is not a sole tenant VM.
+                this.Tenancy = History.Tenancy.Fleet;
+            }
+
+            if (this.Image == null)
+            {
+                this.Image = image;
+            }
+        }
+
+        public void OnStart(DateTime date)
+        {
+            Debug.Assert(date < this.lastEventDate);
+            this.lastEventDate = date;
+
+        }
+
+        public void OnStop(DateTime date)
+        {
+            Debug.Assert(date < this.lastEventDate);
+            this.lastEventDate = date;
+
+            this.LastStoppedOn = date;
+        }
+
+        public void OnSetPlacement(string serverId, DateTime date)
+        {
+            Debug.Assert(date < this.lastEventDate);
+            this.lastEventDate = date;
+
+            // Now we know for sure it is a sole tenant VM.
+            this.Tenancy = Tenancy.SoleTenant;
+
+            DateTime placedUntil;
+            if (this.placements.Count == 0)
+            {
+                if (this.LastStoppedOn.HasValue)
+                {
+                    Debug.Assert(this.LastStoppedOn != null);
+                    Debug.Assert(date <= this.LastStoppedOn);
+
+                    placedUntil = this.LastStoppedOn.Value;
+                }
+                else
+                {
+                    Debug.WriteLine(
+                        $"Instance {this.InstanceId} was placed, but never stopped, " +
+                        "and yet is not running anymore. Flagging as defunct.");
+                    this.IsDefunct = true;
+                    return;
+                }
+            }
+            else
+            {
+                if (this.LastStoppedOn.HasValue)
+                {
+                    Debug.Assert(date <= this.LastStoppedOn);
+                    Debug.Assert(date <= this.placements.First().From);
+
+                    placedUntil = DateTimeUtil.Min(
+                        this.LastStoppedOn.Value,
+                        this.placements.First().From);
+                }
+                else
+                {
+                    Debug.Assert(date <= this.placements.First().From);
+                    placedUntil = this.placements.First().From;
+                }
+            }
+
+            // For a fleet VM, we'd never receive this kind of VM, so this must be 
+            // a sole tenant VM.
+            this.Tenancy = History.Tenancy.SoleTenant;
+            AddPlacement(new SoleTenantPlacement(serverId, date, placedUntil));
+        }
+    }
+}
