53066fc32a8c93f522e82fc56283ccf126792a57
Johannes Passing <jpassing@google.com>
2020-05-23 14:09:53 +0200

    Add BindingExtensions
    Change-Id: I4c301b64c67409098bc23e254eeb4db23a11b46a

---
+++ Google.Solutions.IapDesktop.Application.Test/ObjectModel/TestBindingExtensions.cs
@@ -0,0 +1,180 @@
+﻿//
+// Copyright 2020 Google LLC
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+using Google.Solutions.IapDesktop.Application.ObjectModel;
+using NUnit.Framework;
+using System;
+using System.Windows.Forms;
+
+namespace Google.Solutions.IapDesktop.Application.Test.ObjectModel
+{
+    [TestFixture]
+    public class TestBindingExtensions
+    {
+        //---------------------------------------------------------------------
+        // OnPropertyChange tests.
+        //---------------------------------------------------------------------
+
+        private class Observable : ObservableBase
+        {
+            private string one;
+            private int two;
+
+            public string One
+            {
+                get => this.one;
+                set
+                {
+                    this.one = value;
+                    RaisePropertyChange();
+                }
+            }
+
+            public int Two
+            {
+                get => this.two;
+                set
+                {
+                    this.two = value;
+                    RaisePropertyChange();
+                }
+            }
+        }
+
+        [Test]
+        public void WhenObservedPropertyChanges_ThenOnPropertyChangeTriggersCallback()
+        {
+            var callbacks = 0;
+            var observed = new Observable();
+
+            using (observed.OnPropertyChange(
+                o => o.One,
+                v => { callbacks++; }))
+            {
+                observed.One = "observed";
+                Assert.AreEqual(1, callbacks);
+            }
+
+            observed.One = "not observed";
+            Assert.AreEqual(1, callbacks);
+        }
+
+        [Test]
+        public void WhenNonObservedPropertyChanges_ThenOnPropertyChangeIgnoresThis()
+        {
+            var callbacks = 0;
+            var observed = new Observable();
+
+            using (observed.OnPropertyChange(
+                o => o.One,
+                v => { callbacks++; }))
+            {
+                observed.Two = 2;
+                Assert.AreEqual(0, callbacks);
+            }
+        }
+
+        //---------------------------------------------------------------------
+        // OnControlPropertyChange tests.
+        //---------------------------------------------------------------------
+
+        [Test]
+        public void WhenObservedControlPropertyChanges_ThenOnControlPropertyChangeTriggersCallback()
+        {
+            var callbacks = 0;
+            var observed = new TextBox();
+
+            using (observed.OnControlPropertyChange(
+                o => o.Text,
+                v => { callbacks++; }))
+            {
+                observed.Text = "observed";
+                Assert.AreEqual(1, callbacks);
+            }
+
+            observed.Text = "not observed";
+            Assert.AreEqual(1, callbacks);
+        }
+
+        [Test]
+        public void WhenNonObservedControlPropertyChanges_ThenOnControlPropertyChangeIgnoresThis()
+        {
+            var callbacks = 0;
+            var observed = new TextBox();
+
+            using (observed.OnControlPropertyChange(
+                o => o.Text,
+                v => { callbacks++; }))
+            {
+                observed.TextAlign = HorizontalAlignment.Center;
+                Assert.AreEqual(0, callbacks);
+            }
+        }
+
+        [Test]
+        public void WhenControlHasNoAppropriateEvent_ThenOnControlPropertyChangeThrowsArgumentException()
+        {
+            var observed = new TextBox();
+
+            Assert.Throws<ArgumentException>(() => observed.OnControlPropertyChange(
+                o => o.PasswordChar,
+                _ => { }));
+        }
+
+        //---------------------------------------------------------------------
+        // Bind tests.
+        //---------------------------------------------------------------------
+
+        [Test]
+        public void WhenControlChanges_ThenModelIsUpdated()
+        {
+            var control = new TextBox();
+            var model = new Observable();
+
+            using (control.Bind(
+                t => t.Text,
+                model,
+                m => m.One))
+            {
+                Assert.IsNull(model.One);
+                control.Text = "test";
+                Assert.AreEqual("test", model.One);
+            }
+        }
+
+        [Test]
+        public void WhenModelChanges_ThenControlIsUpdated()
+        {
+            var control = new TextBox();
+            var model = new Observable();
+
+            using (control.Bind(
+                t => t.Text,
+                model,
+                m => m.One))
+            {
+                Assert.AreEqual("", control.Text);
+                model.One = "test";
+                Assert.AreEqual("test", control.Text);
+            }
+        }
+    }
+}
---
+++ Google.Solutions.IapDesktop.Application/ObjectModel/BindingExtensions.cs
@@ -0,0 +1,221 @@
+﻿//
+// Copyright 2019 Google LLC
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace Google.Solutions.IapDesktop.Application.ObjectModel
+{
+    /// <summary>
+    /// Extension methods to implement simple two-way data bindings
+    /// between WinForms controls and model classes.
+    /// </summary>
+    internal static class BindingExtensions
+    {
+        public static IDisposable OnPropertyChange<TObject, TProperty>(
+            this TObject observed,
+            Expression<Func<TObject, TProperty>> modelProperty,
+            Action<TProperty> newValue)
+            where TObject : INotifyPropertyChanged
+        {
+            Debug.Assert(modelProperty.NodeType == ExpressionType.Lambda);
+            if (modelProperty.Body is MemberExpression memberExpression &&
+                memberExpression.Member is PropertyInfo propertyInfo)
+            {
+                return new NotifyPropertyChangedBinding<TObject, TProperty>(
+                    observed,
+                    propertyInfo.Name,
+                    modelProperty.Compile(),
+                    newValue);
+            }
+            else
+            {
+                throw new ArgumentException("Expression does not resolve to a property");
+            }
+        }
+
+        public static IDisposable OnControlPropertyChange<TControl, TProperty>(
+            this TControl observed,
+            Expression<Func<TControl, TProperty>> controlProperty,
+            Action<TProperty> newValue)
+            where TControl : Control
+        {
+            Debug.Assert(controlProperty.NodeType == ExpressionType.Lambda);
+            if (controlProperty.Body is MemberExpression memberExpression &&
+                memberExpression.Member is PropertyInfo propertyInfo)
+            {
+                // Look for a XxxChanged event.
+                var changedEvent = typeof(TControl).GetEvent(propertyInfo.Name + "Changed");
+                if (changedEvent == null)
+                {
+                    throw new ArgumentException(
+                        $"Cannot observe {propertyInfo.Name} because class does not " +
+                        "provide an appropriate event");
+                }
+
+                return new EventHandlerBinding<TControl, TProperty>(
+                    observed,
+                    changedEvent,
+                    controlProperty.Compile(),
+                    newValue);
+            }
+            else
+            {
+                throw new ArgumentException("Expression does not resolve to a property");
+            }
+        }
+
+        private static Action<TProperty> CreateSetter<TObject, TProperty>(
+            TObject obj,
+            Expression<Func<TObject, TProperty>> controlProperty)
+        {
+            Debug.Assert(controlProperty.NodeType == ExpressionType.Lambda);
+            if (controlProperty.Body is MemberExpression memberExpression &&
+                memberExpression.Member is PropertyInfo propertyInfo)
+            {
+                return value =>
+                {
+                    propertyInfo.SetValue(obj, value);
+                };
+            }
+            else
+            {
+                throw new ArgumentException("Expression does not resolve to a property");
+            }
+        }
+
+        public static IDisposable Bind<TControl, TProperty, TModel>(
+            this TControl control,
+            Expression<Func<TControl, TProperty>> controlProperty,
+            TModel model,
+            Expression<Func<TModel, TProperty>> modelProperty)
+            where TModel : INotifyPropertyChanged
+            where TControl : Control
+        {
+            var forwardBinding = control.OnControlPropertyChange(
+                controlProperty,
+                CreateSetter(model, modelProperty));
+
+            var reverseBinding = model.OnPropertyChange(
+                modelProperty,
+                CreateSetter(control, controlProperty));
+
+            return new MultiDisposable(forwardBinding, reverseBinding);
+        }
+
+        private sealed class EventHandlerBinding<TControl, TProperty> : IDisposable
+            where TControl : Control
+        {
+            private readonly TControl observed;
+            private readonly EventInfo eventInfo;
+            private readonly Func<TControl, TProperty> readPropertyFunc;
+            private readonly Action<TProperty> newValueAction;
+
+            private void Observed_PropertyChanged(object sender, EventArgs e)
+            {
+                this.newValueAction(this.readPropertyFunc(this.observed));
+            }
+
+            public EventHandlerBinding(
+                TControl observed,
+                EventInfo eventDescriptor,
+                Func<TControl, TProperty> readPropertyFunc,
+                Action<TProperty> newValueAction)
+            {
+                this.observed = observed;
+                this.eventInfo = eventDescriptor;
+                this.readPropertyFunc = readPropertyFunc;
+                this.newValueAction = newValueAction;
+
+                this.eventInfo.AddEventHandler(
+                    this.observed,
+                    new EventHandler(Observed_PropertyChanged));
+            }
+
+            public void Dispose()
+            {
+                this.eventInfo.RemoveEventHandler(
+                    this.observed,
+                    new EventHandler(Observed_PropertyChanged));
+            }
+        }
+
+        private sealed class NotifyPropertyChangedBinding<TObject, TProperty> : IDisposable
+            where TObject : INotifyPropertyChanged
+        {
+            private readonly TObject observed;
+            private readonly string propertyName;
+
+            private readonly Func<TObject, TProperty> readPropertyFunc;
+            private readonly Action<TProperty> newValueAction;
+
+            private void Observed_PropertyChanged(object sender, PropertyChangedEventArgs e)
+            {
+                if (e.PropertyName == this.propertyName)
+                {
+                    this.newValueAction(this.readPropertyFunc(this.observed));
+                }
+            }
+
+            public NotifyPropertyChangedBinding(
+                TObject observed,
+                string propertyName,
+                Func<TObject, TProperty> readPropertyFunc,
+                Action<TProperty> newValueAction)
+            {
+                this.observed = observed;
+                this.propertyName = propertyName;
+                this.readPropertyFunc = readPropertyFunc;
+                this.newValueAction = newValueAction;
+
+                this.observed.PropertyChanged += Observed_PropertyChanged;
+            }
+
+            public void Dispose()
+            {
+                this.observed.PropertyChanged -= Observed_PropertyChanged;
+            }
+        }
+
+        private sealed class MultiDisposable : IDisposable
+        {
+            private readonly IEnumerable<IDisposable> disposables;
+
+            public MultiDisposable(params IDisposable[] disposables)
+            {
+                this.disposables = disposables;
+            }
+
+            public void Dispose()
+            {
+                foreach (var d in this.disposables)
+                {
+                    d.Dispose();
+                }
+            }
+        }
+    }
+}
