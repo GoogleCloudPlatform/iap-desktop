c7166ea2abc04d36fc339172f2953b725a6e9f64
J. Passing <jpassing@hotmail.com>
2020-06-11 17:05:14 +0200

    Bindable TreeView control (#157)
    Add derived TreeView control that supports simple asynchronous data binding

---
+++ Google.Solutions.IapDesktop.Application.Test/Controls/TestBindableTreeView.cs
@@ -0,0 +1,371 @@
+﻿//
+// Copyright 2020 Google LLC
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+using Google.Solutions.Common.Util;
+using Google.Solutions.IapDesktop.Application.Controls;
+using Google.Solutions.IapDesktop.Application.ObjectModel;
+using NUnit.Framework;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Google.Solutions.IapDesktop.Application.Test.Controls
+{
+    [TestFixture]
+    [Apartment(ApartmentState.STA)]
+    public class TestBindableTreeView : FixtureBase
+    {
+        private class ModelNode : ViewModelBase
+        {
+            private int imageIndex;
+            private string name;
+            private bool expanded = false;
+
+            public bool IsLeaf => false;
+
+            public ObservableCollection<ModelNode> Children = new ObservableCollection<ModelNode>();
+
+            public string Name
+            {
+                get => this.name;
+                set
+                {
+                    this.name = value;
+                    RaisePropertyChange();
+                }
+            }
+
+            public int ImageIndex
+            {
+                get => this.imageIndex;
+                set
+                {
+                    this.imageIndex = value;
+                    RaisePropertyChange();
+                }
+            }
+
+            public bool IsExpanded
+            {
+                get => this.expanded;
+                set
+                {
+                    this.expanded = value;
+                    RaisePropertyChange();
+                }
+            }
+
+            public Task<ObservableCollection<ModelNode>> GetChildren()
+            {
+                return Task.FromResult(this.Children);
+            }
+
+            public Task<ObservableCollection<ModelNode>> Throw()
+            {
+                return Task.FromException<ObservableCollection<ModelNode>>(
+                    new ArgumentException());
+            }
+        }
+
+        private class ModelTreeView : BindableTreeView<ModelNode>
+        {
+        }
+
+        private ModelTreeView tree;
+        private Form form;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.tree = new ModelTreeView();
+
+            this.form = new Form();
+            this.form.Controls.Add(this.tree);
+
+            this.form.Show();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            this.form.Close();
+        }
+
+        private void RunPendingAsyncTasks() => System.Windows.Forms.Application.DoEvents();
+
+        //---------------------------------------------------------------------
+        // Loading.
+        //---------------------------------------------------------------------
+
+        [Test]
+        public void WhenNoOtherPropertiesBound_ThenBindSucceeds()
+        {
+            tree.Bind(new ModelNode());
+
+            Assert.AreEqual(1, this.tree.Nodes.Count);
+        }
+
+        [Test]
+        public void WhenIsExpandedIsTrueInModel_ThenTreeAutoExpands()
+        {
+            var root = new ModelNode()
+            {
+                Name = "root",
+                IsExpanded = true
+            };
+            root.Children.Add(new ModelNode()
+            {
+                Name = "child-1"
+            });
+            root.Children.Add(new ModelNode()
+            {
+                Name = "child-2"
+            });
+
+            tree.BindChildren(m => m.GetChildren());
+            tree.BindIsExpanded(m => m.IsExpanded);
+            tree.Bind(root);
+            RunPendingAsyncTasks();
+
+            Assert.AreEqual(1, this.tree.Nodes.Count);
+            var rootTreeNode = this.tree.Nodes.OfType<ModelTreeView.Node>().First();
+            Assert.AreEqual(2, rootTreeNode.Nodes.Count);
+            Assert.IsTrue(rootTreeNode.IsExpanded);
+        }
+
+        [Test]
+        public void WhenIsExpandedIsFalseInModel_ThenTreeHasHiddenLoadingNode()
+        {
+            var root = new ModelNode()
+            {
+                Name = "root",
+                IsExpanded = false
+            };
+            root.Children.Add(new ModelNode()
+            {
+                Name = "child-1"
+            });
+            root.Children.Add(new ModelNode()
+            {
+                Name = "child-2"
+            });
+
+            tree.BindChildren(m => m.GetChildren());
+            tree.BindIsExpanded(m => m.IsExpanded);
+            tree.Bind(root);
+            RunPendingAsyncTasks();
+
+            Assert.AreEqual(1, this.tree.Nodes.Count);
+            var rootTreeNode = this.tree.Nodes.OfType<ModelTreeView.Node>().First();
+            Assert.AreEqual(1, rootTreeNode.Nodes.Count);   // Loading node, hidden
+            Assert.IsFalse(rootTreeNode.IsExpanded);
+        }
+
+        [Test]
+        public void WhenLoadingChildrenFails_ThenEventIsFiredAndExpandCanBeRetried()
+        {
+            var root = new ModelNode()
+            {
+                Name = "root",
+                IsExpanded = true
+            };
+
+            int eventCount = 0;
+            tree.LoadingChildrenFailed += (sender, args) =>
+            {
+                eventCount++;
+                Assert.IsInstanceOf<ModelNode>(sender);
+                Assert.IsInstanceOf<ArgumentException>(args.Exception.Unwrap());
+            };
+
+            tree.BindChildren(m => m.Throw());
+            tree.BindIsExpanded(m => m.IsExpanded);
+            tree.Bind(root);
+            RunPendingAsyncTasks();
+
+            Assert.AreEqual(1, eventCount);
+
+            var rootTreeNode = this.tree.Nodes.OfType<ModelTreeView.Node>().First();
+            Assert.IsFalse(rootTreeNode.IsExpanded);
+            Assert.IsFalse(root.IsExpanded);
+
+            // Try again.
+            rootTreeNode.Expand();
+            RunPendingAsyncTasks();
+            Assert.AreEqual(2, eventCount);
+
+            // Try again, but this time let loading succeed.
+            tree.BindChildren(m => m.GetChildren());
+            rootTreeNode.Expand();
+            RunPendingAsyncTasks();
+            Assert.AreEqual(2, eventCount);
+            Assert.IsTrue(rootTreeNode.IsExpanded);
+            Assert.IsTrue(root.IsExpanded);
+        }
+
+        //---------------------------------------------------------------------
+        // Binding.
+        //---------------------------------------------------------------------
+
+        [Test]
+        public void WhenModelTextIsUpdated_ThenTreeViewIsUpdated()
+        {
+            var root = new ModelNode()
+            {
+                Name = "root"
+            };
+
+            tree.BindText(m => m.Name);
+            tree.Bind(root);
+
+            Assert.AreEqual(1, this.tree.Nodes.Count);
+            var rootTreeNode = this.tree.Nodes.OfType<ModelTreeView.Node>().First();
+            Assert.AreEqual("root", rootTreeNode.Text);
+            Assert.AreEqual("root", rootTreeNode.Name);
+
+            root.Name = "new";
+            Assert.AreEqual("new", rootTreeNode.Text);
+            Assert.AreEqual("root", rootTreeNode.Name);
+        }
+
+        [Test]
+        public void WhenModelImageIndexIsUpdated_ThenTreeViewIsUpdated()
+        {
+            var root = new ModelNode()
+            {
+                ImageIndex = 1
+            };
+
+            tree.BindImageIndex(m => m.ImageIndex);
+            tree.BindSelectedImageIndex(m => m.ImageIndex);
+            tree.Bind(root);
+
+            Assert.AreEqual(1, this.tree.Nodes.Count);
+            var rootTreeNode = this.tree.Nodes.OfType<ModelTreeView.Node>().First();
+            Assert.AreEqual(1, rootTreeNode.ImageIndex);
+            Assert.AreEqual(1, rootTreeNode.SelectedImageIndex);
+
+            root.ImageIndex = 2;
+            Assert.AreEqual(2, rootTreeNode.ImageIndex);
+            Assert.AreEqual(2, rootTreeNode.SelectedImageIndex);
+        }
+
+        [Test]
+        public void WhenModelAddsChildren_ThenTreeViewIsUpdated()
+        {
+            var root = new ModelNode()
+            {
+                Name = "root",
+                IsExpanded = true
+            };
+            root.Children.Add(new ModelNode()
+            {
+                Name = "child-1"
+            });
+            root.Children.Add(new ModelNode()
+            {
+                Name = "child-2"
+            });
+
+            tree.BindChildren(m => m.GetChildren());
+            tree.BindIsExpanded(m => m.IsExpanded);
+            tree.Bind(root);
+            RunPendingAsyncTasks();
+
+            var rootTreeNode = this.tree.Nodes.OfType<ModelTreeView.Node>().First();
+            Assert.AreEqual(2, rootTreeNode.Nodes.Count);
+
+            root.Children.Add(new ModelNode()
+            {
+                Name = "child-3"
+            });
+
+            Assert.AreEqual(3, rootTreeNode.Nodes.Count);
+        }
+
+        [Test]
+        public void WhenModelRemovesChildren_ThenTreeViewIsUpdated()
+        {
+            var root = new ModelNode()
+            {
+                Name = "root",
+                IsExpanded = true
+            };
+            root.Children.Add(new ModelNode()
+            {
+                Name = "child-1"
+            });
+            root.Children.Add(new ModelNode()
+            {
+                Name = "child-2"
+            });
+
+            tree.BindChildren(m => m.GetChildren());
+            tree.BindIsExpanded(m => m.IsExpanded);
+            tree.Bind(root);
+            RunPendingAsyncTasks();
+
+            var rootTreeNode = this.tree.Nodes.OfType<ModelTreeView.Node>().First();
+            Assert.AreEqual(2, rootTreeNode.Nodes.Count);
+
+            root.Children.RemoveAt(0);
+
+            Assert.AreEqual(1, rootTreeNode.Nodes.Count);
+        }
+
+        [Test]
+        public void WhenModelReplacesChildren_ThenTreeViewIsUpdated()
+        {
+            var root = new ModelNode()
+            {
+                Name = "root",
+                IsExpanded = true
+            };
+            root.Children.Add(new ModelNode()
+            {
+                Name = "child-1"
+            });
+            root.Children.Add(new ModelNode()
+            {
+                Name = "child-2"
+            });
+
+            tree.BindChildren(m => m.GetChildren());
+            tree.BindIsExpanded(m => m.IsExpanded);
+            tree.Bind(root);
+            RunPendingAsyncTasks();
+
+            var rootTreeNode = this.tree.Nodes.OfType<ModelTreeView.Node>().First();
+            Assert.AreEqual(2, rootTreeNode.Nodes.Count);
+
+            root.Children[0] = new ModelNode()
+            {
+                Name = "new"
+            };
+
+            Assert.AreEqual(2, rootTreeNode.Nodes.Count);
+            Assert.AreEqual("new", rootTreeNode.Nodes.OfType<ModelTreeView.Node>().First().Model.Name);
+        }
+    }
+}
---
+++ Google.Solutions.IapDesktop.Application/Controls/BindableTreeView.cs
@@ -0,0 +1,373 @@
+﻿//
+// Copyright 2020 Google LLC
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+using Google.Solutions.IapDesktop.Application.ObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+#pragma warning disable CS1690 // Accessing a member on a field of a marshal-by-reference class may cause a runtime exception
+
+namespace Google.Solutions.IapDesktop.Application.Controls
+{
+    public class BindableTreeView<TModelNode> : TreeView
+        where TModelNode : class, INotifyPropertyChanged
+    {
+        private Expression<Func<TModelNode, bool>> isExpandedExpression = null;
+        private Expression<Func<TModelNode, int>> imageIndexExpression = null;
+        private Expression<Func<TModelNode, int>> selectedImageIndexExpression = null;
+        private Expression<Func<TModelNode, string>> textExpression = null;
+        
+        private Func<TModelNode, bool> isLeafFunc = _ => false;
+        private Action<TModelNode, bool> setExpandedFunc = (n, state) => {};
+        private Func<TModelNode, bool> isExpandedFunc = _ => false;
+        private Func<TModelNode, Task<ObservableCollection<TModelNode>>> getChildrenAsyncFunc
+            = _ => Task.FromResult(new ObservableCollection<TModelNode>());
+
+        private readonly TaskScheduler taskScheduler;
+
+        public event EventHandler SelectedModelNodeChanged;
+        public event EventHandler<ExceptionEventArgs> LoadingChildrenFailed;
+
+        public BindableTreeView()
+        {
+            this.taskScheduler = TaskScheduler.FromCurrentSynchronizationContext();
+
+            this.BeforeExpand += (sender, args) => ((Node)args.Node).OnExpand();
+            this.BeforeCollapse += (sender, args) => ((Node)args.Node).OnCollapse();
+
+            // Consider right-click as a selection.
+            this.NodeMouseClick += (sender, args) =>
+            {
+                if (args.Button == MouseButtons.Right)
+                {
+                    this.SelectedNode = args.Node;
+                }
+            };
+            this.AfterSelect += (sender, args) =>
+            {
+                this.SelectedModelNodeChanged?.Invoke(this, EventArgs.Empty);
+            };
+        }
+
+        public TModelNode SelectedModelNode
+        {
+            get
+            {
+                if (this.SelectedNode is Node node)
+                {
+                    return node.Model;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        //---------------------------------------------------------------------
+        // Data Binding.
+        //---------------------------------------------------------------------
+
+        public void Bind(TModelNode rootNode)
+        {
+            this.Nodes.Clear();
+            this.Nodes.Add(new Node(this, rootNode));
+        }
+
+        public void BindImageIndex(Expression<Func<TModelNode, int>> imageIndexExpression)
+        {
+            this.imageIndexExpression = imageIndexExpression;
+        }
+
+        public void BindIsLeaf(Func<TModelNode, bool> isLeafFunc)
+        {
+            this.isLeafFunc = isLeafFunc;
+        }
+
+        public void BindSelectedImageIndex(Expression<Func<TModelNode, int>> selectedImageIndexExpression)
+        {
+            this.selectedImageIndexExpression = selectedImageIndexExpression;
+        }
+
+        public void BindText(Expression<Func<TModelNode, string>> nameExpression)
+        {
+            this.textExpression = nameExpression;
+        }
+
+        public void BindIsExpanded(Expression<Func<TModelNode, bool>> propertyExpression)
+        {
+            if (propertyExpression.Body is MemberExpression memberExpression &&
+                memberExpression.Member is PropertyInfo propertyInfo)
+            {
+                this.isExpandedExpression = propertyExpression;
+                this.isExpandedFunc = propertyExpression.Compile();
+                this.setExpandedFunc = (node, value) => propertyInfo.SetValue(node, value);
+            }
+            else
+            {
+                throw new ArgumentException("Expression does not resolve to a property");
+            }
+        }
+
+        public void BindChildren(
+            Func<TModelNode, Task<ObservableCollection<TModelNode>>> getChildrenAsyncFunc)
+        {
+            this.getChildrenAsyncFunc = getChildrenAsyncFunc;
+        }
+
+        //---------------------------------------------------------------------
+        // BindableTreeNode
+        //---------------------------------------------------------------------
+
+        internal class Node : TreeNode 
+        {
+            private readonly BindableTreeView<TModelNode> treeView;
+            public TModelNode Model { get; }
+            private bool lazyLoadTriggered = false;
+
+            public Node(BindableTreeView<TModelNode> treeView, TModelNode modelNode)
+            {
+                this.treeView = treeView;
+                this.Model = modelNode;
+
+
+                // Bind properties to keep TreeNode in sync with view model.
+                // Note that binding is one-way (view model -> view) as TreeNodes
+                // are not proper controls and do not provide the necessary events.
+                if (this.treeView.textExpression != null)
+                {
+                    this.Name = this.Text = this.treeView.textExpression.Compile()(this.Model);
+                    this.Model.OnPropertyChange(
+                        this.treeView.textExpression,
+                        text => this.Text = text);
+                }
+
+                if (this.treeView.imageIndexExpression != null)
+                {
+                    this.ImageIndex = this.treeView.imageIndexExpression.Compile()(this.Model);
+                    this.Model.OnPropertyChange(
+                        this.treeView.imageIndexExpression,
+                        iconIndex => this.ImageIndex = iconIndex);
+                }
+
+                if (this.treeView.selectedImageIndexExpression != null)
+                {
+                    this.SelectedImageIndex = this.treeView.selectedImageIndexExpression.Compile()(this.Model);
+                    this.Model.OnPropertyChange(
+                        this.treeView.selectedImageIndexExpression,
+                        iconIndex => this.SelectedImageIndex = iconIndex);
+                }
+
+                if (this.treeView.isExpandedExpression != null)
+                {
+                    this.Model.OnPropertyChange(
+                        this.treeView.isExpandedExpression,
+                        expanded =>
+                        {
+                            if (expanded)
+                            {
+                                this.Expand();
+                            }
+                            else
+                            {
+                                this.Collapse();
+                            }
+                        });
+                }
+
+                if (this.treeView.isLeafFunc(this.Model))
+                {
+                    // This node does not have children.
+                }
+                else
+                {
+                    // This node might have children. Add a dummy node
+                    // to ensure that the '+' control is being displayed.
+                    this.Nodes.Add(new LoadingTreeNode());
+
+                    if (this.treeView.isExpandedFunc(this.Model))
+                    {
+                        // Eagerly load children.
+                        this.Expand();
+                        LazyLoadChildren();
+                    }
+                    else
+                    {
+                        // Lazy load children. Nothing to do for now.
+                    }
+                }
+            }
+
+            internal void OnExpand()
+            {
+                if (!this.treeView.isExpandedFunc(this.Model))
+                {
+                    this.treeView.setExpandedFunc(this.Model, true);
+                }
+
+                LazyLoadChildren();
+            }
+
+            internal void OnCollapse()
+            {
+                if (this.treeView.isExpandedFunc(this.Model))
+                {
+                    this.treeView.setExpandedFunc(this.Model, false);
+                }
+            }
+
+            public void LazyLoadChildren()
+            {
+                if (this.lazyLoadTriggered)
+                {
+                    return;
+                }
+
+                this.lazyLoadTriggered = true;
+                this.treeView.getChildrenAsyncFunc(this.Model)
+                    .ContinueWith(t =>
+                    {
+                        try
+                        {
+                            var children = t.Result;
+
+                            // Clear any dummy node if present.
+                            this.Nodes.Clear();
+
+                            // Add nodes.
+                            AddTreeNodesForModelNodes(children);
+
+                            // Observe for changes.
+                            children.CollectionChanged += ModelChildrenChanged;
+                        }
+                        catch (Exception e)
+                        {
+                            // Reset state so that the action can be retried.
+                            this.Collapse();
+                            this.treeView.setExpandedFunc(this.Model, false);
+                            this.lazyLoadTriggered = false;
+
+                            // Report error.
+                            this.treeView.LoadingChildrenFailed?.Invoke(
+                                this.Model,
+                                new ExceptionEventArgs(e));
+                        }
+                    },
+                    CancellationToken.None,
+                    TaskContinuationOptions.None,
+
+                    // Continue on UI thread. 
+                    // Note that there's a bug in the CLR that can cause
+                    // TaskScheduler.FromCurrentSynchronizationContext() to become null.
+                    // Therefore, use a task scheduler object captured previously.
+                    // Cf. https://stackoverflow.com/questions/4659257/
+                    this.treeView.taskScheduler);
+            }
+
+            private void AddTreeNodesForModelNodes(IEnumerable<TModelNode> children)
+            {
+                foreach (var child in children)
+                {
+                    this.Nodes.Add(new Node(this.treeView, child));
+                }
+            }
+
+            private Node FindTreeNodeByModelNode(TModelNode modelNode)
+            {
+                return this.Nodes
+                    .OfType<Node>()
+                    .FirstOrDefault(n => n.Model.Equals(modelNode));
+            }
+
+            private void ModelChildrenChanged(object sender, NotifyCollectionChangedEventArgs e)
+            {
+                switch (e.Action)
+                {
+                    case NotifyCollectionChangedAction.Add:
+                        AddTreeNodesForModelNodes(e.NewItems.OfType<TModelNode>());
+                        break;
+
+                    case NotifyCollectionChangedAction.Remove:
+                        foreach (var oldModelItem in e.OldItems.OfType<TModelNode>())
+                        {
+                            var oldTreeNode = FindTreeNodeByModelNode(oldModelItem);
+                            if (oldTreeNode != null)
+                            {
+                                this.Nodes.Remove(oldTreeNode);
+                            }
+                        }
+                        break;
+
+                    case NotifyCollectionChangedAction.Replace:
+                        if (e.OldItems.Count == e.NewItems.Count)
+                        {
+                            var count = e.OldItems.Count;
+                            for (var i = 0; i < count; i++)
+                            {
+                                var oldModelItem = (TModelNode)e.OldItems[i];
+                                var newModelItem = (TModelNode)e.NewItems[i];
+
+                                var treeNode = FindTreeNodeByModelNode(oldModelItem);
+                                if (treeNode != null)
+                                {
+                                    this.Nodes.Remove(treeNode);
+                                    this.Nodes.Insert(
+                                        e.NewStartingIndex, 
+                                        new Node(this.treeView, newModelItem));
+                                }
+                            }
+                        }
+                        break;
+
+                    case NotifyCollectionChangedAction.Move:
+                        // Not supported.
+                        break;
+
+                    case NotifyCollectionChangedAction.Reset:
+                        // Reload everything.
+                        this.Nodes.Clear();
+                        AddTreeNodesForModelNodes((ObservableCollection<TModelNode>)sender);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private class LoadingTreeNode : TreeNode
+        {
+            public LoadingTreeNode()
+                : base("Loading...")
+            {
+            }
+        }
+    }
+}
