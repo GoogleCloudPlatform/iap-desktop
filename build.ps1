39ebf7c9a0da94e7230f715ec9a8b89659bc1007
Johannes Passing <jpassing@google.com>
2020-06-16 12:33:05 +0200

    Use TLS 1.2 for all downloads
    Change-Id: I62e1fb49c97edfbf4fb0ad7609b862811ee57a0a

--- build.ps1
+++ build.ps1
@@ -30,6 +30,9 @@ $VsixInstaller = (Resolve-Path ([IO.Path]::Combine(${Env:ProgramFiles(x86)}, 'Mi
 $NugetDownloadUrl = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
 $RdcManDownloadUrl = "https://download.microsoft.com/download/A/F/0/AF0071F3-B198-4A35-AA90-C68D103BDCCF/rdcman.msi"
 
+# Use TLS 1.2 for all downloads.
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
 Write-Host "========================================================"
 Write-Host "=== Preparing build                                 ==="
 Write-Host "========================================================"
