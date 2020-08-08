6ff426dcd2126fe0b34a4770faf9ad59f8463ad4
Johannes Passing <jpassing@google.com>
2020-08-08 07:44:07 +0200

    Change singleton creation logic to allow references betwen singletons

--- sources/Google.Solutions.IapDesktop.Application/ObjectModel/ServiceRegistry.cs
+++ sources/Google.Solutions.IapDesktop.Application/ObjectModel/ServiceRegistry.cs
@@ -21,6 +21,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Reflection;
 using System.Runtime.Serialization;
 
@@ -36,7 +37,7 @@ namespace Google.Solutions.IapDesktop.Application.ObjectModel
     public class ServiceRegistry : IServiceProvider
     {
         private readonly ServiceRegistry parent;
-        private readonly IDictionary<Type, object> singletons = new Dictionary<Type, object>();
+        private readonly IDictionary<Type, SingletonStub> singletons = new Dictionary<Type, SingletonStub>();
         private readonly IDictionary<Type, Func<object>> transients = new Dictionary<Type, Func<object>>();
 
         public ServiceRegistry()
@@ -82,17 +83,17 @@ namespace Google.Solutions.IapDesktop.Application.ObjectModel
 
         public void AddSingleton<TService>(TService singleton)
         {
-            this.singletons[typeof(TService)] = singleton;
+            this.singletons[typeof(TService)] = new SingletonStub(singleton);
         }
 
         public void AddSingleton<TService>()
         {
-            this.singletons[typeof(TService)] = CreateInstance<TService>();
+            this.singletons[typeof(TService)] = new SingletonStub(CreateInstance<TService>());
         }
 
         public void AddSingleton<TService, TServiceClass>()
         {
-            this.singletons[typeof(TService)] = CreateInstance<TServiceClass>();
+            this.singletons[typeof(TService)] = new SingletonStub(CreateInstance<TServiceClass>());
         }
 
         public void AddTransient<TService>()
@@ -107,9 +108,9 @@ namespace Google.Solutions.IapDesktop.Application.ObjectModel
 
         public object GetService(Type serviceType)
         {
-            if (this.singletons.TryGetValue(serviceType, out object singleton))
+            if (this.singletons.TryGetValue(serviceType, out SingletonStub singletonStub))
             {
-                return singleton;
+                return singletonStub.Object;
             }
             else if (this.transients.TryGetValue(serviceType, out Func<object> transientFactory))
             {
@@ -144,14 +145,66 @@ namespace Google.Solutions.IapDesktop.Application.ObjectModel
             // Register singletons. Because transients are already registered,
             // it is ok if singletons instantiate transients in their constructor.
             //
+            // As singletons might depend on another, we cannot instantiate them
+            // in a single pass. Instead, register stubs in the first pass,
+            // then force their instantiation in a second pass.
+            //
+
+            //
+            // (1) Register stubs, but do not create instances yet.
+            //
             foreach (var type in assembly.GetTypes())
             {
                 if (type.GetCustomAttribute<ServiceAttribute>() is ServiceAttribute attribute &&
                     attribute.Lifetime == ServiceLifetime.Singleton)
                 {
-                    var instance = CreateInstance(type);
+                    this.singletons[attribute.ServiceInterface ?? type] 
+                        = new SingletonStub(() => CreateInstance(type));
+                }
+            }
 
-                    this.singletons[attribute.ServiceInterface ?? type] = instance;
+            //
+            // (2) Trigger stubs to create instances.
+            //
+            foreach (var stub in this.singletons.Values)
+            {
+                var instance = stub.Object;
+                Debug.Assert(instance != null);
+            }
+        }
+
+        private class SingletonStub
+        {
+            private object value;
+            private readonly Func<object> factoryFunc;
+
+            public SingletonStub(object value)
+            {
+                this.value = value;
+            }
+
+            public SingletonStub(Func<object> factoryFunc)
+            {
+                this.factoryFunc = factoryFunc;
+            }
+
+            public object Object
+            {
+                get
+                {
+                    //
+                    // NB. This does not need to be thread-safe as the
+                    // initialization happens synchronously,
+                    // see CreateInstance().
+                    //
+                    if (this.value == null)
+                    {
+                        Debug.Assert(this.factoryFunc != null);
+                        this.value = this.factoryFunc();
+                        Debug.Assert(this.value != null);
+                    }
+
+                    return this.value;
                 }
             }
         }
