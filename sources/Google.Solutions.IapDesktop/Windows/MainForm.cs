db1e6007fd64da2e22e69d0b1266183eeb0fe482
Johannes Passing <jpassing@google.com>
2020-10-23 07:40:52 +0200

    Add license headers

--- sources/Google.Solutions.IapDesktop/Windows/MainForm.cs
+++ sources/Google.Solutions.IapDesktop/Windows/MainForm.cs
@@ -25,7 +25,6 @@ using Google.Solutions.Common.Util;
 using Google.Solutions.IapDesktop.Application.ObjectModel;
 using Google.Solutions.IapDesktop.Application.Services.Adapters;
 using Google.Solutions.IapDesktop.Application.Services.Integration;
-using Google.Solutions.IapDesktop.Application.Services.Persistence;
 using Google.Solutions.IapDesktop.Application.Views;
 using Google.Solutions.IapDesktop.Application.Views.Diagnostics;
 using Google.Solutions.IapDesktop.Application.Views.ProjectExplorer;
