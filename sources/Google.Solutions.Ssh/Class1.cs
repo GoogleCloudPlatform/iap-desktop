862c42de253c2636a7b709b83694bc82dcbbbc3d
Johannes Passing <jpassing@google.com>
2020-11-15 08:57:05 +0100

    Add ssh project

---
+++ sources/Google.Solutions.Ssh/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Google.Solutions.Ssh
+{
+    public class Class1
+    {
+    }
+}
