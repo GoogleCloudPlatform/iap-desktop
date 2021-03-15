bd693a022a1fc3c2bbbdf358321617bda18f631a
Johannes Passing <jpassing@hotmail.com>
2021-03-15 14:21:00 +0100

    Add TypographicQuotes helper class

---
+++ sources/Google.Solutions.IapDesktop.Application.Test/Util/TestTypographicQuotes.cs
@@ -0,0 +1,58 @@
+﻿//
+// Copyright 2020 Google LLC
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+using Google.Solutions.IapDesktop.Application.Util;
+using NUnit.Framework;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Google.Solutions.IapDesktop.Application.Test.Util
+{
+    [TestFixture]
+    public class TestTypographicQuotes : ApplicationFixtureBase
+    {
+        [Test]
+        public void WhenStringContainsNoTypograhicQuotes_ThenToAsciiQuotesReturnsSameString()
+        {
+            var s = "This isn't \"typographic\" but `plain´ ASCII";
+            Assert.AreEqual(s, TypographicQuotes.ToAsciiQuotes(s));
+        }
+
+        [Test]
+        public void WhenStringContainsTypograhicDoubleQuotes_ThenToAsciiQuotesReturnsSanitizedString()
+        {
+            var s = "These are \u201CEnglish\u201d, \u201eGerman\u201c, and \u00bbFrench\u00ab double quotes";
+            Assert.AreEqual(
+                "These are \"English\", \"German\", and \"French\" double quotes",
+            TypographicQuotes.ToAsciiQuotes(s));
+        }
+
+        [Test]
+        public void WhenStringContainsTypograhicSingleQuotes_ThenToAsciiQuotesReturnsSanitizedString()
+        {
+            var s = "These are \u2018English\u2019, \u201aGerman\u2018, and \u203aFrench\u2039 single quotes";
+            Assert.AreEqual(
+                "These are \'English\', \'German\', and \'French\' single quotes",
+                TypographicQuotes.ToAsciiQuotes(s));
+        }
+    }
+}
---
+++ sources/Google.Solutions.IapDesktop.Application/Util/TypographicQuotes.cs
@@ -0,0 +1,66 @@
+﻿//
+// Copyright 2020 Google LLC
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+using Google.Apis.Util;
+using System.Collections.Generic;
+
+namespace Google.Solutions.IapDesktop.Application.Util
+{
+    public static class TypographicQuotes
+    {
+        private static readonly IDictionary<char, char> conversionTable = new Dictionary<char, char>()
+        {
+            { '\u00AB', '\"' }, // left-pointing double angle quotation mark
+            { '\u00BB', '\"' }, // right-pointing double angle quotation mark
+            { '\u2018', '\'' }, // left single quotation mark
+            { '\u2019', '\'' }, // right single quotation mark
+            { '\u201A', '\'' }, // single low-9 quotation mark
+            { '\u201B', '\'' }, // single high-reversed-9 quotation mark
+            { '\u201C', '\"' }, // left double quotation mark
+            { '\u201D', '\"' }, // right double quotation mark
+            { '\u201E', '\"' }, // double low-9 quotation mark
+            { '\u201F', '\"' }, // double high-reversed-9 quotation mark
+            { '\u2039', '\'' }, // single left-pointing angle quotation mark
+            { '\u203A', '\'' }, // single right-pointing angle quotation mark
+            { '\u2E42', '\'' }, // double low-reversed-9 quotation mark
+        };
+
+        public static string ToAsciiQuotes(string s)
+        {
+            Utilities.ThrowIfNull(s, nameof(s));
+
+            var sanitized = new char[s.Length];
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (conversionTable.TryGetValue(s[i], out char replacement))
+                {
+                    sanitized[i] = replacement;
+                }
+                else
+                {
+                    sanitized[i] = s[i];
+                }
+            }
+
+            return new string(sanitized);
+        }
+    }
+}
