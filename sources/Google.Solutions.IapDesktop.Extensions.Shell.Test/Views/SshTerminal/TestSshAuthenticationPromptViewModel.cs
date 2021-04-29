995ff17073bcd0e6aad9c41cab8fc6d102709aba
Johannes Passing <jpassing@hotmail.com>
2021-04-29 15:42:45 +0200

    Add tests for view model

---
+++ sources/Google.Solutions.IapDesktop.Extensions.Shell.Test/Views/SshTerminal/TestSshAuthenticationPromptViewModel.cs
@@ -0,0 +1,72 @@
+﻿//
+// Copyright 2021 Google LLC
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+using Google.Solutions.Common.Test;
+using Google.Solutions.IapDesktop.Application.Test;
+using Google.Solutions.IapDesktop.Extensions.Shell.Views.SshTerminal;
+using NUnit.Framework;
+
+namespace Google.Solutions.IapDesktop.Extensions.Shell.Test.Views.SshTerminal
+{
+    [TestFixture]
+    public class TestSshAuthenticationPromptViewModel : ApplicationFixtureBase
+    {
+        [Test]
+        public void WhenTitleSet_ThenNotificationIsRaised()
+        {
+            var viewModel = new SshAuthenticationPromptViewModel();
+            AssertEx.RaisesPropertyChangedNotification(
+                viewModel,
+                () => viewModel.Title = "test",
+                v => v.Title);
+        }
+
+        [Test]
+        public void WhenDescriptionSet_ThenNotificationIsRaised()
+        {
+            var viewModel = new SshAuthenticationPromptViewModel();
+            AssertEx.RaisesPropertyChangedNotification(
+                viewModel,
+                () => viewModel.Description = "test",
+                v => v.Description);
+        }
+        
+        [Test]
+        public void WhenInputSet_ThenNotificationIsRaised()
+        {
+            var viewModel = new SshAuthenticationPromptViewModel();
+            AssertEx.RaisesPropertyChangedNotification(
+                viewModel,
+                () => viewModel.Input = "test",
+                v => v.Input);
+        }
+
+        [Test]
+        public void WhenIsPasswordMaskedSet_ThenNotificationIsRaised()
+        {
+            var viewModel = new SshAuthenticationPromptViewModel();
+            AssertEx.RaisesPropertyChangedNotification(
+                viewModel,
+                () => viewModel.IsPasswordMasked = true,
+                v => v.IsPasswordMasked);
+        }
+    }
+}
