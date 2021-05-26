1ee68c1e9c2ab7e077a9b5a7a393e113f7efbf73
Johannes Passing <jpassing@hotmail.com>
2021-05-26 16:09:56 +0200

    Cleanup

--- sources/Google.Solutions.IapDesktop.Extensions.Shell/Controls/VirtualTerminalKeyTranslation.cs
+++ sources/Google.Solutions.IapDesktop.Extensions.Shell/Controls/VirtualTerminalKeyTranslation.cs
@@ -66,6 +66,9 @@ namespace Google.Solutions.IapDesktop.Extensions.Shell.Controls
             }
         }
 
+        private static string Esc = "\u001b";
+        private static string Ss3 = Esc + "O";
+
         /// <summary>
         /// Standard Xterm key translations.
         /// </summary>
@@ -75,67 +78,67 @@ namespace Google.Solutions.IapDesktop.Extensions.Shell.Controls
                 //
                 // Function keys.
                 //
-                { Keys.F1,       new StandardMapping { Normal = "\u001b[11~", Shift = "\u001b[23~", Control = "\u001b[11~", Alt = "\u001b\u001b[11~" } },
-                { Keys.F2,       new StandardMapping { Normal = "\u001b[12~", Shift = "\u001b[24~", Control = "\u001b[12~", Alt = "\u001b\u001b[12~" } },
-                { Keys.F3,       new StandardMapping { Normal = "\u001b[13~", Shift = "\u001b[25~", Control = "\u001b[13~", Alt = "\u001b\u001b[13~" } },
-                { Keys.F4,       new StandardMapping { Normal = "\u001b[14~", Shift = "\u001b[26~", Control = "\u001b[14~", Alt = "\u001b\u001b[14~" } },
-                { Keys.F5,       new StandardMapping { Normal = "\u001b[15~", Shift = "\u001b[28~", Control = "\u001b[15~", Alt = "\u001b\u001b[15~" } },
-                { Keys.F6,       new StandardMapping { Normal = "\u001b[17~", Shift = "\u001b[29~", Control = "\u001b[17~", Alt = "\u001b\u001b[17~" } },
-                { Keys.F7,       new StandardMapping { Normal = "\u001b[18~", Shift = "\u001b[31~", Control = "\u001b[18~", Alt = "\u001b\u001b[18~" } },
-                { Keys.F8,       new StandardMapping { Normal = "\u001b[19~", Shift = "\u001b[32~", Control = "\u001b[19~", Alt = "\u001b\u001b[19~" } },
-                { Keys.F9,       new StandardMapping { Normal = "\u001b[20~", Shift = "\u001b[33~", Control = "\u001b[20~", Alt = "\u001b\u001b[20~" } },
-                { Keys.F10,      new StandardMapping { Normal = "\u001b[21~", Shift = "\u001b[24~", Control = "\u001b[21~", Alt = "\u001b\u001b[21~" } },
-                { Keys.F11,      new StandardMapping { Normal = "\u001b[23~", Shift = "\u001b[23~", Control = "\u001b[23~", Alt = "\u001b\u001b[23~" } },
-                { Keys.F12,      new StandardMapping { Normal = "\u001b[24~", Shift = "\u001b[24~", Control = "\u001b[24~", Alt = "\u001b\u001b[24~" } },
+                { Keys.F1,       new StandardMapping { Normal = Esc + "[11~", Shift = Esc + "[23~", Control = Esc + "[11~", Alt = Esc + Esc + "[11~" } },
+                { Keys.F2,       new StandardMapping { Normal = Esc + "[12~", Shift = Esc + "[24~", Control = Esc + "[12~", Alt = Esc + Esc + "[12~" } },
+                { Keys.F3,       new StandardMapping { Normal = Esc + "[13~", Shift = Esc + "[25~", Control = Esc + "[13~", Alt = Esc + Esc + "[13~" } },
+                { Keys.F4,       new StandardMapping { Normal = Esc + "[14~", Shift = Esc + "[26~", Control = Esc + "[14~", Alt = Esc + Esc + "[14~" } },
+                { Keys.F5,       new StandardMapping { Normal = Esc + "[15~", Shift = Esc + "[28~", Control = Esc + "[15~", Alt = Esc + Esc + "[15~" } },
+                { Keys.F6,       new StandardMapping { Normal = Esc + "[17~", Shift = Esc + "[29~", Control = Esc + "[17~", Alt = Esc + Esc + "[17~" } },
+                { Keys.F7,       new StandardMapping { Normal = Esc + "[18~", Shift = Esc + "[31~", Control = Esc + "[18~", Alt = Esc + Esc + "[18~" } },
+                { Keys.F8,       new StandardMapping { Normal = Esc + "[19~", Shift = Esc + "[32~", Control = Esc + "[19~", Alt = Esc + Esc + "[19~" } },
+                { Keys.F9,       new StandardMapping { Normal = Esc + "[20~", Shift = Esc + "[33~", Control = Esc + "[20~", Alt = Esc + Esc + "[20~" } },
+                { Keys.F10,      new StandardMapping { Normal = Esc + "[21~", Shift = Esc + "[24~", Control = Esc + "[21~", Alt = Esc + Esc + "[21~" } },
+                { Keys.F11,      new StandardMapping { Normal = Esc + "[23~", Shift = Esc + "[23~", Control = Esc + "[23~", Alt = Esc + Esc + "[23~" } },
+                { Keys.F12,      new StandardMapping { Normal = Esc + "[24~", Shift = Esc + "[24~", Control = Esc + "[24~", Alt = Esc + Esc + "[24~" } },
 
                 //
                 // Arrow keys.
                 //
-                { Keys.Up,       new StandardMapping { Normal = "\u001b[A",   Shift = "\u001bOA", Control = "\u001bOA",     Alt = "\u001b\u001b[A" } },
-                { Keys.Down,     new StandardMapping { Normal = "\u001b[B",   Shift = "\u001bOB", Control = "\u001bOB",     Alt = "\u001b\u001b[B" } },
-                { Keys.Right,    new StandardMapping { Normal = "\u001b[C",   Shift = "\u001bOC", Control = "\u001bOC",     Alt = "\u001b\u001b[C" } },
-                { Keys.Left,     new StandardMapping { Normal = "\u001b[D",   Shift = "\u001bOD", Control = "\u001bOD",     Alt = "\u001b\u001b[D" } },
-                { Keys.Home,     new StandardMapping { Normal = "\u001b[1~",  Shift = "\u001b[1~",                          Alt = "\u001b\u001b[1~" } },
-                { Keys.Insert,   new StandardMapping { Normal = "\u001b[2~",                                                Alt = "\u001b\u001b[2~" } },
-                { Keys.Delete,   new StandardMapping { Normal = "\u001b[3~",  Shift = "\u001b[3~",                          Alt = "\u001b\u001b[3~" } },
-                { Keys.End,      new StandardMapping { Normal = "\u001b[4~",  Shift = "\u001b[4~",                          Alt = "\u001b\u001b[4~" } },
-                { Keys.PageUp,   new StandardMapping { Normal = "\u001b[5~",  Shift = "\u001b[5~",                          Alt = "\u001b\u001b[5~" } },
-                { Keys.PageDown, new StandardMapping { Normal = "\u001b[6~",  Shift = "\u001b[6~",                          Alt = "\u001b\u001b[6~" } },
+                { Keys.Up,       new StandardMapping { Normal = Esc + "[A",   Shift = Esc + "OA", Control = Esc + "OA",     Alt = Esc + Esc + "[A" } },
+                { Keys.Down,     new StandardMapping { Normal = Esc + "[B",   Shift = Esc + "OB", Control = Esc + "OB",     Alt = Esc + Esc + "[B" } },
+                { Keys.Right,    new StandardMapping { Normal = Esc + "[C",   Shift = Esc + "OC", Control = Esc + "OC",     Alt = Esc + Esc + "[C" } },
+                { Keys.Left,     new StandardMapping { Normal = Esc + "[D",   Shift = Esc + "OD", Control = Esc + "OD",     Alt = Esc + Esc + "[D" } },
+                { Keys.Home,     new StandardMapping { Normal = Esc + "[1~",  Shift = Esc + "[1~",                          Alt = Esc + Esc + "[1~" } },
+                { Keys.Insert,   new StandardMapping { Normal = Esc + "[2~",                                                Alt = Esc + Esc + "[2~" } },
+                { Keys.Delete,   new StandardMapping { Normal = Esc + "[3~",  Shift = Esc + "[3~",                          Alt = Esc + Esc + "[3~" } },
+                { Keys.End,      new StandardMapping { Normal = Esc + "[4~",  Shift = Esc + "[4~",                          Alt = Esc + Esc + "[4~" } },
+                { Keys.PageUp,   new StandardMapping { Normal = Esc + "[5~",  Shift = Esc + "[5~",                          Alt = Esc + Esc + "[5~" } },
+                { Keys.PageDown, new StandardMapping { Normal = Esc + "[6~",  Shift = Esc + "[6~",                          Alt = Esc + Esc + "[6~" } },
 
                 //
                 // Main keyboard.
                 //
                 { Keys.Back,    new StandardMapping { Normal = "\u007F",      Shift = "\b",           Control = "\u007F" } },
-                { Keys.Tab,     new StandardMapping { Normal = "\t",          Shift = "\u001b[Z",     } },
-                { Keys.Return,  new StandardMapping { Normal = "\r",          Shift = "\r",           Control = "\r",       Alt = "\u001b\r" } },
-                { Keys.Escape,  new StandardMapping { Normal = "\u001b\u001b",Shift = "\u001b\u001b", Control = "\u001b\u001b" } },
-                { Keys.Space,   new StandardMapping { Control = "\u0000",                                                   Alt = "\u001b " } },
-                { Keys.A,       new StandardMapping { Control = "\u0001",     Alt = "\u001ba" } },
-                { Keys.B,       new StandardMapping { Control = "\u0002",     Alt = "\u001bb" } },
-                { Keys.C,       new StandardMapping { Control = "\u0003",     Alt = "\u001bc" } },
-                { Keys.D,       new StandardMapping { Control = "\u0004",     Alt = "\u001bd" } },
-                { Keys.E,       new StandardMapping { Control = "\u0005",     Alt = "\u001be" } },
-                { Keys.F,       new StandardMapping { Control = "\u0006",     Alt = "\u001bf" } },
-                { Keys.G,       new StandardMapping { Control = "\u0007",     Alt = "\u001bg" } },
-                { Keys.H,       new StandardMapping { Control = "\u0008",     Alt = "\u001bh" } },
-                { Keys.I,       new StandardMapping { Control = "\u0009",     Alt = "\u001bi" } },
-                { Keys.J,       new StandardMapping { Control = "\u000a",     Alt = "\u001bj" } },
-                { Keys.K,       new StandardMapping { Control = "\u000b",     Alt = "\u001bk" } },
-                { Keys.L,       new StandardMapping { Control = "\u000c",     Alt = "\u001bl" } },
-                { Keys.M,       new StandardMapping { Control = "\u000d",     Alt = "\u001bm" } },
-                { Keys.N,       new StandardMapping { Control = "\u000e",     Alt = "\u001bn" } },
-                { Keys.O,       new StandardMapping { Control = "\u000f",     Alt = "\u001bo" } },
-                { Keys.P,       new StandardMapping { Control = "\u0010",     Alt = "\u001bp" } },
-                { Keys.Q,       new StandardMapping { Control = "\u0011",     Alt = "\u001bq" } },
-                { Keys.R,       new StandardMapping { Control = "\u0012",     Alt = "\u001br" } },
-                { Keys.S,       new StandardMapping { Control = "\u0013",     Alt = "\u001bs" } },
-                { Keys.T,       new StandardMapping { Control = "\u0014",     Alt = "\u001bt" } },
-                { Keys.U,       new StandardMapping { Control = "\u0015",     Alt = "\u001bu" } },
-                { Keys.V,       new StandardMapping { Control = "\u0016",     Alt = "\u001bv" } },
-                { Keys.W,       new StandardMapping { Control = "\u0017",     Alt = "\u001bw" } },
-                { Keys.X,       new StandardMapping { Control = "\u0018",     Alt = "\u001bx" } },
-                { Keys.Y,       new StandardMapping { Control = "\u0019",     Alt = "\u001by" } },
-                { Keys.Z,       new StandardMapping { Control = "\u001a",     Alt = "\u001bz" } },
+                { Keys.Tab,     new StandardMapping { Normal = "\t",          Shift = Esc + "[Z",     } },
+                { Keys.Return,  new StandardMapping { Normal = "\r",          Shift = "\r",           Control = "\r",       Alt = Esc + "\r" } },
+                { Keys.Escape,  new StandardMapping { Normal = Esc + Esc,     Shift = Esc + Esc,      Control = Esc + Esc } },
+                { Keys.Space,   new StandardMapping { Control = "\u0000",                                                   Alt = Esc + " " } },
+                { Keys.A,       new StandardMapping { Control = "\u0001",     Alt = Esc + "a" } },
+                { Keys.B,       new StandardMapping { Control = "\u0002",     Alt = Esc + "b" } },
+                { Keys.C,       new StandardMapping { Control = "\u0003",     Alt = Esc + "c" } },
+                { Keys.D,       new StandardMapping { Control = "\u0004",     Alt = Esc + "d" } },
+                { Keys.E,       new StandardMapping { Control = "\u0005",     Alt = Esc + "e" } },
+                { Keys.F,       new StandardMapping { Control = "\u0006",     Alt = Esc + "f" } },
+                { Keys.G,       new StandardMapping { Control = "\u0007",     Alt = Esc + "g" } },
+                { Keys.H,       new StandardMapping { Control = "\u0008",     Alt = Esc + "h" } },
+                { Keys.I,       new StandardMapping { Control = "\u0009",     Alt = Esc + "i" } },
+                { Keys.J,       new StandardMapping { Control = "\u000a",     Alt = Esc + "j" } },
+                { Keys.K,       new StandardMapping { Control = "\u000b",     Alt = Esc + "k" } },
+                { Keys.L,       new StandardMapping { Control = "\u000c",     Alt = Esc + "l" } },
+                { Keys.M,       new StandardMapping { Control = "\u000d",     Alt = Esc + "m" } },
+                { Keys.N,       new StandardMapping { Control = "\u000e",     Alt = Esc + "n" } },
+                { Keys.O,       new StandardMapping { Control = "\u000f",     Alt = Esc + "o" } },
+                { Keys.P,       new StandardMapping { Control = "\u0010",     Alt = Esc + "p" } },
+                { Keys.Q,       new StandardMapping { Control = "\u0011",     Alt = Esc + "q" } },
+                { Keys.R,       new StandardMapping { Control = "\u0012",     Alt = Esc + "r" } },
+                { Keys.S,       new StandardMapping { Control = "\u0013",     Alt = Esc + "s" } },
+                { Keys.T,       new StandardMapping { Control = "\u0014",     Alt = Esc + "t" } },
+                { Keys.U,       new StandardMapping { Control = "\u0015",     Alt = Esc + "u" } },
+                { Keys.V,       new StandardMapping { Control = "\u0016",     Alt = Esc + "v" } },
+                { Keys.W,       new StandardMapping { Control = "\u0017",     Alt = Esc + "w" } },
+                { Keys.X,       new StandardMapping { Control = "\u0018",     Alt = Esc + "x" } },
+                { Keys.Y,       new StandardMapping { Control = "\u0019",     Alt = Esc + "y" } },
+                { Keys.Z,       new StandardMapping { Control = "\u001a",     Alt = Esc + "z" } },
             };
 
         /// <summary>
@@ -144,10 +147,16 @@ namespace Google.Solutions.IapDesktop.Extensions.Shell.Controls
         private static readonly Dictionary<Keys, StandardMapping> ApplicationCursorKeysModeTranslations
             = new Dictionary<Keys, StandardMapping>
             {
-                { Keys.Up,       new StandardMapping { Normal = "\u001bOA" } },
-                { Keys.Down,     new StandardMapping { Normal = "\u001bOB" } },
-                { Keys.Right,    new StandardMapping { Normal = "\u001bOC" } },
-                { Keys.Left,     new StandardMapping { Normal = "\u001bOD" } },
+                //
+                // NB. To manually enter this mode, run
+                //     echo -e "\e[?1h"
+                // To disable:
+                //     echo -e "\e[?1l"
+                //
+                { Keys.Up,       new StandardMapping { Normal = Ss3 + "A" } },
+                { Keys.Down,     new StandardMapping { Normal = Ss3 + "B" } },
+                { Keys.Right,    new StandardMapping { Normal = Ss3 + "C" } },
+                { Keys.Left,     new StandardMapping { Normal = Ss3 + "D" } },
             };
 
         public static string ForKey(
