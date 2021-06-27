4506f7f74c886b7a120c7c2cb0d0a8c51889e71d
Johannes Passing <jpassing@hotmail.com>
2021-06-27 14:10:58 +0200

    Undo AssemblyInfo changes

--- sources/Google.Solutions.IapDesktop.Extensions.Activity/Properties/AssemblyInfo.cs
+++ sources/Google.Solutions.IapDesktop.Extensions.Activity/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("Google LLC")]
 
-[assembly: AssemblyVersion("2.19.1")]
-[assembly: AssemblyFileVersion("2.19.1")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
 
 [assembly: InternalsVisibleTo("Google.Solutions.IapDesktop.Extensions.Activity.Test")]
