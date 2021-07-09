a0f1cb76ba3da43fa59386f2aae18759ff44b8a9
Johannes Passing <jpassing@hotmail.com>
2021-07-09 12:39:10 +0200

    Add additional documentation links to Help menu

--- sources/Google.Solutions.IapDesktop/Properties/Resources.Designer.cs
+++ sources/Google.Solutions.IapDesktop/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace Google.Solutions.IapDesktop.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Documentation_16 {
+            get {
+                object obj = ResourceManager.GetObject("Documentation_16", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap project1 {
             get {
                 object obj = ResourceManager.GetObject("project1", resourceCulture);
