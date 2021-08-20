f088f0884c9c5ad5df2e76a29d1614b1112d8dd1
Johannes Passing <jpassing@hotmail.com>
2021-08-20 13:10:14 +0200

    b/193758410 Client certificate selection (#551)
    Allow selecting a custom client certificate in place of the default
    EV-provisioned certificate. The filter uses a syntax derived from
    Chrome's AutoSelectCertificateForUrls filter syntax.

---
+++ sources/Google.Solutions.IapDesktop.Application.Test/Services/SecureConnect/TestChromeCertificateSelector.cs
@@ -0,0 +1,344 @@
+﻿//
+// Copyright 2021 Google LLC
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+using Google.Solutions.IapDesktop.Application.Services.SecureConnect;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Google.Solutions.IapDesktop.Application.Test.Services.SecureConnect
+{
+    [TestFixture]
+    public class TestChromeCertificateSelector : ApplicationFixtureBase
+    {
+        private static readonly X500DistinguishedName ComplexIssuerDn = 
+            new X500DistinguishedName("C=US,S=CA,L=MTV,O=Acme,OU=Sales,CN=Issuer");
+        private static readonly X500DistinguishedName ComplexSubjectDn =
+            new X500DistinguishedName("C=US,S=CA,L=MTV,O=Acme,OU=Sales,CN=Subject");
+
+        private static readonly X500DistinguishedName SimpleIssuerDn =
+            new X500DistinguishedName("CN=Issuer");
+        private static readonly X500DistinguishedName SimpleSubjectDn =
+            new X500DistinguishedName("CN=Subject");
+
+        //---------------------------------------------------------------------
+        // TryParse.
+        //---------------------------------------------------------------------
+
+        [Test]
+        public void WhenJsonMalformed_ThenTryParseReturnsFalse()
+        {
+            Assert.IsFalse(ChromeCertificateSelector.TryParse(
+                "{asd'", 
+                out var _));
+        }
+
+        //---------------------------------------------------------------------
+        // URL match.
+        //---------------------------------------------------------------------
+
+        [Test]
+        public void WhenUrlMismatches_ThenIsMatchReturnsFalse()
+        {
+            var selector = ChromeCertificateSelector.Parse(
+                @"{
+                    'pattern': 'https://*.google.com/', 
+                    'filter':{}
+                }");
+
+            Assert.IsNotNull(selector.Pattern);
+            Assert.IsNull(selector.Filter.Issuer);
+            Assert.IsNull(selector.Filter.Subject);
+
+            Assert.IsFalse(selector.IsMatch(
+                new Uri("https://www.google.de"),
+                SimpleIssuerDn,
+                SimpleSubjectDn,
+                null));
+        }
+
+        [Test]
+        public void WhenUrlAndFilterEmpty_ThenIsMatchReturnsTrue()
+        {
+            var selector = ChromeCertificateSelector.Parse(
+                @"{
+                }");
+
+            Assert.IsNotNull(selector.Pattern);
+            Assert.IsNull(selector.Filter.Issuer);
+            Assert.IsNull(selector.Filter.Subject);
+
+            Assert.IsTrue(selector.IsMatch(
+                new Uri("https://www.google.com"),
+                SimpleIssuerDn,
+                SimpleSubjectDn,
+                null));
+        }
+
+        [Test]
+        public void WhenUrlMatchesAndFilterEmpty_ThenIsMatchReturnsTrue()
+        {
+            var selector = ChromeCertificateSelector.Parse(
+                @"{
+                    'pattern': 'https://*.google.com/', 
+                    'filter':{}
+                }");
+
+            Assert.IsNotNull(selector.Pattern);
+            Assert.IsNull(selector.Filter.Issuer);
+            Assert.IsNull(selector.Filter.Subject);
+
+            Assert.IsTrue(selector.IsMatch(
+                new Uri("https://www.google.com"),
+                SimpleIssuerDn,
+                SimpleSubjectDn,
+                null));
+        }
+
+        //---------------------------------------------------------------------
+        // Issuer match.
+        //---------------------------------------------------------------------
+
+        [Test]
+        public void WhenUrlAndIssuerMatchesBySingleField_ThenIsMatchReturnsTrue()
+        {
+            var selector = ChromeCertificateSelector.Parse(
+                @"{
+                    'pattern': 'https://*.google.com/', 
+                    'filter':{
+                        'ISSUER': {
+                            'CN': 'Issuer'
+                        }
+                    }
+                }");
+
+            Assert.IsNotNull(selector.Pattern);
+            Assert.IsNotNull(selector.Filter.Issuer);
+            Assert.IsNull(selector.Filter.Subject);
+
+            Assert.IsTrue(selector.IsMatch(
+                new Uri("https://www.google.com"),
+                ComplexIssuerDn,
+                ComplexSubjectDn,
+                null));
+
+            Assert.IsTrue(selector.IsMatch(
+                new Uri("https://www.google.com"),
+                SimpleIssuerDn,
+                ComplexSubjectDn,
+                null));
+        }
+
+        [Test]
+        public void WhenUrlAndIssuerMatchByAllFields_ThenIsMatchReturnsTrue()
+        {
+            var selector = ChromeCertificateSelector.Parse(
+                @"{
+                    'pattern': 'https://*.google.com/', 
+                    'filter':{
+                        'ISSUER': {
+                            'CN': 'Issuer',
+                            'C': 'US',
+                            'S': 'CA',
+                            'L': 'MTV',
+                            'O': 'Acme',
+                            'OU': 'Sales'
+                        }
+                    }
+                }");
+
+            Assert.IsNotNull(selector.Pattern);
+            Assert.IsNotNull(selector.Filter.Issuer);
+            Assert.IsNull(selector.Filter.Subject);
+
+            Assert.IsTrue(selector.IsMatch(
+                new Uri("https://www.google.com"),
+                ComplexIssuerDn,
+                SimpleSubjectDn,
+                null));
+
+            Assert.IsFalse(selector.IsMatch(
+                new Uri("https://www.google.com"),
+                SimpleIssuerDn,
+                SimpleSubjectDn,
+                null));
+        }
+
+        [Test]
+        public void WhenUrlAndIssuerMatchButSubjectMismatches_ThenIsMatchReturnsFalse()
+        {
+            var selector = ChromeCertificateSelector.Parse(
+                @"{
+                    'pattern': 'https://*.google.com/', 
+                    'filter':{
+                        'ISSUER': {
+                            'CN': 'Issuer'
+                        },
+                        'SUBJECT': {
+                            'CN': 'Xxx'
+                        }
+                    }
+                }");
+
+            Assert.IsNotNull(selector.Pattern);
+            Assert.IsNotNull(selector.Filter.Issuer);
+            Assert.IsNotNull(selector.Filter.Subject);
+
+            Assert.IsFalse(selector.IsMatch(
+                new Uri("https://www.google.com"),
+                ComplexIssuerDn,
+                ComplexSubjectDn,
+                null));
+        }
+
+        //---------------------------------------------------------------------
+        // Subject match.
+        //---------------------------------------------------------------------
+
+        [Test]
+        public void WhenUrlAndSubjectMatchesBySingleField_ThenIsMatchReturnsTrue()
+        {
+            var selector = ChromeCertificateSelector.Parse(
+                @"{
+                    'pattern': 'https://*.google.com/', 
+                    'filter':{
+                        'SUBJECT': {
+                            'CN': 'Subject'
+                        }
+                    }
+                }");
+
+            Assert.IsNotNull(selector.Pattern);
+            Assert.IsNull(selector.Filter.Issuer);
+            Assert.IsNotNull(selector.Filter.Subject);
+
+            Assert.IsTrue(selector.IsMatch(
+                new Uri("https://www.google.com"),
+                SimpleIssuerDn,
+                SimpleSubjectDn,
+                null));
+
+            Assert.IsTrue(selector.IsMatch(
+                new Uri("https://www.google.com"),
+                SimpleIssuerDn,
+                ComplexSubjectDn,
+                null));
+        }
+
+        [Test]
+        public void WhenUrlAndSubjectMatchByAllFields_ThenIsMatchReturnsTrue()
+        {
+            var selector = ChromeCertificateSelector.Parse(
+                @"{
+                    'pattern': 'https://*.google.com/', 
+                    'filter':{
+                        'SUBJECT': {
+                            'CN': 'Subject',
+                            'C': 'US',
+                            'S': 'CA',
+                            'L': 'MTV',
+                            'O': 'Acme',
+                            'OU': 'Sales'
+                        }
+                    }
+                }");
+
+            Assert.IsNotNull(selector.Pattern);
+            Assert.IsNull(selector.Filter.Issuer);
+            Assert.IsNotNull(selector.Filter.Subject);
+
+            Assert.IsTrue(selector.IsMatch(
+                new Uri("https://www.google.com"),
+                SimpleIssuerDn,
+                ComplexSubjectDn,
+                null));
+
+            Assert.IsFalse(selector.IsMatch(
+                new Uri("https://www.google.com"),
+                SimpleIssuerDn,
+                SimpleSubjectDn,
+                null));
+        }
+
+        //---------------------------------------------------------------------
+        // Thumbprint match.
+        //---------------------------------------------------------------------
+
+        [Test]
+        public void WhenThumbprintMatches_ThenIsMatchReturnsTrue()
+        {
+            var selector = ChromeCertificateSelector.Parse(
+                @"{
+                    'pattern': 'https://*.google.com/', 
+                    'filter':{
+                        'THUMBPRINT': 'abcd'
+                    }
+                }");
+
+            Assert.IsNotNull(selector.Pattern);
+            Assert.IsNull(selector.Filter.Issuer);
+            Assert.IsNull(selector.Filter.Subject);
+            Assert.IsNotNull(selector.Filter.Thumbprint);
+
+            Assert.IsTrue(selector.IsMatch(
+                new Uri("https://www.google.com"),
+                SimpleIssuerDn,
+                SimpleSubjectDn,
+                "ABCD"));
+
+            Assert.IsFalse(selector.IsMatch(
+                new Uri("https://www.google.com"),
+                SimpleIssuerDn,
+                SimpleSubjectDn,
+                "0123"));
+        }
+
+        [Test]
+        public void WhenThumbprintMatchesButSubjectDoesnt_ThenIsMatchReturnsFalse()
+        {
+            var selector = ChromeCertificateSelector.Parse(
+                @"{
+                    'pattern': 'https://*.google.com/', 
+                    'filter':{
+                        'THUMBPRINT': 'abcd',
+                        'SUBJECT': {
+                            'CN': 'Somethingelse'
+                        }
+                    }
+                }");
+
+            Assert.IsNotNull(selector.Pattern);
+            Assert.IsNull(selector.Filter.Issuer);
+            Assert.IsNotNull(selector.Filter.Subject);
+            Assert.IsNotNull(selector.Filter.Thumbprint);
+
+            Assert.IsFalse(selector.IsMatch(
+                new Uri("https://www.google.com"),
+                SimpleIssuerDn,
+                ComplexSubjectDn,
+                "abcd"));
+        }
+    }
+}
---
+++ sources/Google.Solutions.IapDesktop.Application/Services/SecureConnect/ChromeCertificateSelector.cs
@@ -0,0 +1,171 @@
+﻿//
+// Copyright 2021 Google LLC
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+using Google.Apis.Util;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Google.Solutions.IapDesktop.Application.Services.SecureConnect
+{
+    /// <summary>
+    /// Certificate selector as defined in
+    /// https://chromeenterprise.google/policies/?policy=AutoSelectCertificateForUrls.
+    /// 
+    /// In addition to the standard SUBJECT and ISSUER filters, this implementation
+    /// supports a THUMBPRINT filter that lets you select a specific certificate.
+    /// </summary>
+    internal class ChromeCertificateSelector
+    {
+        public ChromeMatchPattern Pattern { get; }
+        public CertificateFilter Filter { get; }
+
+        [JsonConstructor]
+        public ChromeCertificateSelector(
+            [JsonProperty("pattern")] string pattern,
+            [JsonProperty("filter")] CertificateFilter filter)
+        {
+            this.Pattern = ChromeMatchPattern.Parse(pattern ?? ChromeMatchPattern.AllUrls);
+            this.Filter = filter ?? new CertificateFilter();
+        }
+
+        public static ChromeCertificateSelector Parse(string json)
+        {
+            Utilities.ThrowIfNullOrEmpty(json, nameof(json));
+            return JsonConvert.DeserializeObject<ChromeCertificateSelector>(json);
+        }
+
+        public static bool TryParse(string json, out ChromeCertificateSelector selector)
+        {
+            try
+            {
+                selector = Parse(json);
+                return true;
+            }
+            catch
+            {
+                selector = null;
+                return false;
+            }
+        }
+
+        public bool IsMatch(
+            Uri uri,
+            X500DistinguishedName issuer,
+            X500DistinguishedName subject,
+            string thumbprint)
+        {
+            return
+                this.Pattern.IsMatch(uri) &&
+                (this.Filter.Issuer == null || this.Filter.Issuer.IsMatch(issuer)) &&
+                (this.Filter.Subject == null || this.Filter.Subject.IsMatch(subject)) &&
+                (this.Filter.Thumbprint == null || this.Filter.Thumbprint.Equals(
+                        thumbprint, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public bool IsMatch(
+            Uri uri,
+            X509Certificate2 certificate)
+            => IsMatch(
+                uri, 
+                certificate.IssuerName, 
+                certificate.SubjectName, 
+                certificate.Thumbprint);
+
+        //---------------------------------------------------------------------
+        // Inner classes for deserialization.
+        //---------------------------------------------------------------------
+
+        public class CertificateFilter
+        {
+            [JsonConstructor]
+            public CertificateFilter(
+                [JsonProperty("ISSUER")] DistinguishedNameFilter issuer,
+                [JsonProperty("SUBJECT")] DistinguishedNameFilter subject,
+                [JsonProperty("THUMBPRINT")] string thumbprint)
+            {
+                this.Issuer = issuer;
+                this.Subject = subject;
+                this.Thumbprint = thumbprint;
+            }
+
+            public CertificateFilter() : this(null, null, null)
+            {
+            }
+
+            [JsonProperty("ISSUER")]
+            public DistinguishedNameFilter Issuer { get; }
+            
+            [JsonProperty("SUBJECT")]
+            public DistinguishedNameFilter Subject { get; }
+
+            [JsonProperty("THUMBPRINT")]
+            public string Thumbprint { get; }
+        }
+
+        public class DistinguishedNameFilter
+        {
+            [JsonConstructor]
+            public DistinguishedNameFilter(
+                [JsonProperty("CN")] string commonName,
+                [JsonProperty("L")] string location,
+                [JsonProperty("O")] string organization,
+                [JsonProperty("OU")] string orgUnit)
+            {
+                this.CommonName = commonName;
+                this.Location = location;
+                this.Organization = organization;
+                this.OrgUnit = orgUnit;
+            }
+
+            [JsonProperty("CN")]
+            public string CommonName { get; }
+
+            [JsonProperty("L")]
+            public string Location { get; }
+
+            [JsonProperty("O")]
+            public string Organization { get; }
+
+            [JsonProperty("OU")]
+            public string OrgUnit { get; }
+
+            public bool IsMatch(X500DistinguishedName dn)
+            {
+                var components = dn
+                    .Format(true)
+                    .Split('\n')
+                    .Select(s => s.Trim())
+                    .ToList();
+
+                return
+                    (this.CommonName == null || components.Contains($"CN={this.CommonName}")) &&
+                    (this.Location == null || components.Contains($"L={this.Location}")) &&
+                    (this.Organization == null || components.Contains($"O={this.Organization}")) &&
+                    (this.OrgUnit == null || components.Contains($"OU={this.OrgUnit}"));
+            }
+        }
+    }
+}
