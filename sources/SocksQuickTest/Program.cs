a94937e6b4c1d428c6ed00d5f4e8cd13c54c2ffd
Johannes Passing <jpassing@hotmail.com>
2022-01-21 10:29:57 +0100

    Add test program

--- sources/SocksQuickTest/Program.cs
+++ sources/SocksQuickTest/Program.cs
@@ -1,7 +1,12 @@
-﻿using Google.Solutions.IapTunneling.Iap;
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Solutions.Common.Net;
+using Google.Solutions.IapTunneling.Iap;
+using Google.Solutions.IapTunneling.Net;
 using Google.Solutions.IapTunneling.Socks5;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -12,13 +17,13 @@ namespace SocksQuickTest
 {
     class Program : ISshRelayEndpointResolver
     {
-        static void Main(string[] args)
+        private const ushort SocksPort = 1080;
+        private readonly UserAgent agent = new UserAgent("SocksTest", new Version(1, 0));
+        private readonly ICredential credential;
+
+        public Program(ICredential credential)
         {
-            var listener = new Socks5Listener(
-                new Program(),
-                new AllowAllRelayPolicy(),
-                1080);
-            listener.ListenAsync(CancellationToken.None).Wait();
+            this.credential = credential;
         }
 
         public Task<ISshRelayEndpoint> ResolveEndpointAsync(
@@ -26,7 +31,20 @@ namespace SocksQuickTest
             ushort destinationPort,
             CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            if (InternalDns.TryParseZonalDns(destinationDomain, out var locator))
+            {
+                return Task.FromResult<ISshRelayEndpoint>(new IapTunnelingEndpoint(
+                    this.credential,
+                    locator,
+                    destinationPort,
+                    IapTunnelingEndpoint.DefaultNetworkInterface,
+                    this.agent));
+            }
+            else
+            {
+                throw new ArgumentException(
+                    $"{destinationPort} is not a valid internal DNS name");
+            }
         }
 
         public Task<ISshRelayEndpoint> ResolveEndpointAsync(
@@ -35,6 +53,42 @@ namespace SocksQuickTest
             CancellationToken cancellationToken)
         {
             throw new NotImplementedException();
+        }
+
+        public async Task StartSocksServerAsync(CancellationToken cancellationToken)
+        {
+            var listener = new Socks5Listener(
+                this,
+                new AllowAllRelayPolicy(),
+                SocksPort);
+            await listener
+                .ListenAsync(cancellationToken)
+                .ConfigureAwait(false);
+        }
+
+        static void Main(string[] args)
+        {
+            var chrome = Process.Start(new ProcessStartInfo()
+            {
+                UseShellExecute = true,
+                FileName = "chrome",
+                Arguments = $"--user-data-dir={Path.GetTempPath()} " +
+                            $"--proxy-server=\"socks5://127.0.0.1:{SocksPort}\" " +
+                            "--guest --host-resolver-rules=\"MAP * ~NOTFOUND\""
+            });
+
+            new Program(GoogleCredential.GetApplicationDefault())
+                .StartSocksServerAsync(CancellationToken.None)
+                .ContinueWith(t =>
+                {
+                    if (t.IsFaulted)
+                    {
+                        Console.WriteLine(t.Exception.ToString());
+                        Environment.Exit(1);
+                    }
+                });
+
+            chrome.WaitForExit();
         }
     }
 }
