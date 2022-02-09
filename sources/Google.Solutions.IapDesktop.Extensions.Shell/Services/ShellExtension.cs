5d40c5c35449e9fec346d796701168c3dda006c7
Johannes Passing <jpassing@hotmail.com>
2022-02-09 16:43:47 +0100

    b/216743374 Change shortcut for leaving full-screen RDP
    Changing to Ctrl + Alt + F11, which
    
    * works on notebook keyboards (which lack a Break key)
    * is more consistent with the F11 shortcut to enter full-screen

--- sources/Google.Solutions.IapDesktop.Extensions.Shell/Services/ShellExtension.cs
+++ sources/Google.Solutions.IapDesktop.Extensions.Shell/Services/ShellExtension.cs
@@ -371,7 +371,7 @@ namespace Google.Solutions.IapDesktop.Extensions.Shell.Services
                     _ => DoWithActiveRemoteDesktopSession(session => session.TrySetFullscreen(FullScreenMode.SingleScreen)))
                 {
                     Image = Resources.Fullscreen_16,
-                    ShortcutKeys = Keys.F11
+                    ShortcutKeys =  DocumentWindow.EnterFullScreenHotKey
                 });
             desktopMenu.AddCommand(
                 new Command<IMainForm>(
