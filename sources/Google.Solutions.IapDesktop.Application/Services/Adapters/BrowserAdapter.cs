2be959fc9762ef22135abf95aaada09e8c0366e9
Johannes Passing <jpassing@hotmail.com>
2022-03-23 16:29:18 +0100

    WIP: BrowserAdapter, no tests

---
+++ sources/Google.Solutions.IapDesktop.Application/Services/Adapters/BrowserAdapter.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Google.Solutions.IapDesktop.Application.Services.Adapters
+{
+    public enum BrowserPreference
+    {
+        /// <summary>
+        /// Use system default browser.
+        /// </summary>
+        Default,
+
+        /// <summary>
+        /// Use Chrome if available.
+        /// </summary>
+        Chrome
+    }
+
+    public interface IBrowserAdapter
+    {
+        /// <summary>
+        /// Open browser and navigate to an address.
+        /// </summary>
+        void Navigate(Uri address, BrowserPreference preference);
+    }
+
+    public class BrowserAdapter : IBrowserAdapter
+    {
+        private const string AppPath = @"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths";
+
+        public static string ChromeExecutablePath
+        {
+            get 
+            {
+                using (var hive = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Default))
+                using (var chromeAppPath = hive.OpenSubKey($@"{AppPath}\chrome.exe", false))
+                {
+                    return (string)chromeAppPath?.GetValue(null);
+                }
+            }
+        }
+
+        public static bool IsChromeInstalled => ChromeExecutablePath != null;
+
+        //---------------------------------------------------------------------
+        // IBrowserAdapter.
+        //---------------------------------------------------------------------
+
+        public void Navigate(Uri address, BrowserPreference preference)
+        {
+            if (preference == BrowserPreference.Chrome && IsChromeInstalled)
+            {
+                //
+                // Launch Chrome.
+                //
+            }
+            else
+            {
+                //
+                // Use system-default browser.
+                //
+                using (Process.Start(new ProcessStartInfo()
+                {
+                    UseShellExecute = true,
+                    Verb = "open",
+                    FileName = address.ToString()
+                }))
+                { };
+            }
+        }
+    }
+}
