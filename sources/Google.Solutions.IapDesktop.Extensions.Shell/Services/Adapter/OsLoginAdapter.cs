0f8b8188c6bc6b9251edaf473628b6f4850a0ad3
Johannes Passing <jpassing@hotmail.com>
2022-03-31 12:31:12 +0200

    Handle failing key uploads caused by policy

--- sources/Google.Solutions.IapDesktop.Extensions.Shell/Services/Adapter/OsLoginAdapter.cs
+++ sources/Google.Solutions.IapDesktop.Extensions.Shell/Services/Adapter/OsLoginAdapter.cs
@@ -35,6 +35,7 @@ using Google.Solutions.Ssh;
 using Google.Solutions.Ssh.Auth;
 using System;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
