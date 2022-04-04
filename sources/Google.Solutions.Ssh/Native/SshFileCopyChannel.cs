873cca82de1aa5b518a15c73d9213d76ee27a883
Johannes Passing <jpassing@hotmail.com>
2022-04-04 17:11:49 +0200

    Rename base class

--- sources/Google.Solutions.Ssh/Native/SshFileCopyChannel.cs
+++ sources/Google.Solutions.Ssh/Native/SshFileCopyChannel.cs
@@ -30,9 +30,9 @@ namespace Google.Solutions.Ssh.Native
     /// <summary>
     /// SCP file uploda/download channel.
     /// </summary>
-    public abstract class SshFileCopyChannel : SshChannelBase // TODO: rename to Base
+    public abstract class SshFileCopyChannelBase : SshChannelBase
     {
-        internal SshFileCopyChannel(
+        internal SshFileCopyChannelBase(
             SshSession session,
             SshChannelHandle channelHandle)
             : base(session, channelHandle)
@@ -44,7 +44,7 @@ namespace Google.Solutions.Ssh.Native
     /// <summary>
     /// SCP upload channel.
     /// </summary>
-    public class SshFileUploadChannel : SshFileCopyChannel
+    public class SshFileUploadChannel : SshFileCopyChannelBase
     {
         internal SshFileUploadChannel(
             SshSession session,
@@ -65,7 +65,7 @@ namespace Google.Solutions.Ssh.Native
     /// <summary>
     /// SCP download channel.
     /// </summary>
-    public class SshFileDownloadChannel : SshFileCopyChannel
+    public class SshFileDownloadChannel : SshFileCopyChannelBase
     {
         private readonly LIBSSH2_STAT fileStat;
 
