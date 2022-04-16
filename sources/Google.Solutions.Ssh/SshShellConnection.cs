ebeb5be851bf099657daa0c60235ad1b36929341
Johannes Passing <jpassing@hotmail.com>
2022-04-16 14:13:01 +0200

    Remove intermeditate base class, SshConnectionBase

--- sources/Google.Solutions.Ssh/SshShellConnection.cs
+++ sources/Google.Solutions.Ssh/SshShellConnection.cs
@@ -19,9 +19,12 @@
 // under the License.
 //
 
+using Google.Apis.Util;
 using Google.Solutions.Ssh.Auth;
 using Google.Solutions.Ssh.Native;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Globalization;
 using System.Net;
 using System.Text;
@@ -29,15 +32,20 @@ using System.Threading.Tasks;
 
 namespace Google.Solutions.Ssh
 {
-    public class SshShellConnection : SshConnectionBase
+    public class SshShellConnection : SshWorkerThread
     {
         public const string DefaultTerminal = "xterm";
         public static readonly TerminalSize DefaultTerminalSize = new TerminalSize(80, 24);
-
         private static readonly Encoding DefaultEncoding = Encoding.UTF8;
 
-        private readonly ITextTerminal terminal;
+        private readonly IRawTerminal terminal;
         private readonly TerminalSize initialSize;
+
+        private readonly Queue<SendOperation> sendQueue = new Queue<SendOperation>();
+        private readonly TaskCompletionSource<int> connectionCompleted
+            = new TaskCompletionSource<int>();
+
+        private readonly byte[] receiveBuffer = new byte[64 * 1024];
 
         //---------------------------------------------------------------------
         // Ctor.
@@ -50,10 +58,11 @@ namespace Google.Solutions.Ssh
             TerminalSize initialSize)
             : base(
                   endpoint,
-                  authenticator,
-                  terminal.ToRawTerminal(DefaultEncoding))
+                  authenticator)
         {
-            this.terminal = terminal;
+            this.terminal = terminal
+                .ThrowIfNull(nameof(terminal))
+                .ToRawTerminal(DefaultEncoding);
             this.initialSize = initialSize;
         }
 
@@ -89,13 +98,142 @@ namespace Google.Solutions.Ssh
                 environmentVariables);
         }
 
+        protected override void Receive(SshChannelBase channel) //TODO: Rename to OnReadyToSend/Receive
+        {
+            //
+            // NB. receiveFunc() can throw an exception, in which case
+            // we do not complete the current packet. If the exception
+            // is bad, we'll receive a OnReceiveError callback later.
+            //
+            // NB. This method is always called on the same thread, so it's ok
+            // to reuse the same buffer.
+            //
+
+            var bytesReceived = channel.Read(this.receiveBuffer);
+            this.terminal.OnDataReceived(this.receiveBuffer, 0, bytesReceived);
+
+            //
+            // In non-blocking mode, we're not always receive a final
+            // zero-length read.
+            //
+            if (bytesReceived > 0 && channel.IsEndOfStream)
+            {
+                this.terminal.OnDataReceived(Array.Empty<byte>(), 0, 0);
+            }
+        }
+
+        protected override void OnReceiveError(Exception exception)
+        {
+            this.terminal.OnError(exception);
+        }
+
+        protected override void Send(SshChannelBase channel)
+        {
+            lock (this.sendQueue)
+            {
+                Debug.Assert(this.sendQueue.Count > 0);
+
+                var packet = this.sendQueue.Peek();
+
+                //
+                // NB. The operation can throw an exception. It is
+                // imporant that we let this exception escape because
+                // it might be simply an EAGAIN situation. If the
+                // exception is bad, we'll receive an OnSendError 
+                // callback later.
+                //
+                packet.Operation(channel);
+
+                //
+                // Receive succeeded - complete packet.
+                //
+                this.sendQueue.Dequeue();
+                packet.CompletionSource.SetResult(0);
+
+                if (this.sendQueue.Count == 0)
+                {
+                    //
+                    // Do not ask us for more data, we do not have any
+                    // right now.
+                    //
+                    NotifyReadyToSend(false);
+                }
+            }
+        }
+
+        protected override void OnSendError(Exception exception)
+        {
+            lock (this.sendQueue)
+            {
+                Debug.Assert(this.sendQueue.Count > 0);
+                this.sendQueue.Dequeue().CompletionSource.SetException(exception);
+            }
+        }
+
+        protected override void OnConnected()
+        {
+            // Complete task, but force continuation to run on different thread.
+            Task.Run(() => this.connectionCompleted.SetResult(0));
+        }
+
+        protected override void OnConnectionError(Exception exception)
+        {
+            if (!this.connectionCompleted.Task.IsCompleted)
+            {
+                // TODO: improve this
+                // Complete task, but force continuation to run on different thread.
+                Task.Run(() => this.connectionCompleted.SetException(exception));
+            }
+        }
+
         //---------------------------------------------------------------------
         // I/O.
         //---------------------------------------------------------------------
 
+        protected Task SendAsync(Action<SshChannelBase> operation)
+        {
+            if (!this.IsConnected)
+            {
+                throw new SshException("Connection is closed");
+            }
+
+            lock (this.sendQueue)
+            {
+                var packet = new SendOperation(operation);
+                this.sendQueue.Enqueue(packet);
+
+                // 
+                // Nofify that we have data and expect a Send()
+                // callback.
+                //
+                NotifyReadyToSend(true);
+
+                //
+                // Return a task - it'll be completed once we've
+                // actually sent the data.
+                //
+                return packet.CompletionSource.Task;
+            }
+        }
+
+        //---------------------------------------------------------------------
+        // Publics.
+        //---------------------------------------------------------------------
+
+        public Task ConnectAsync()
+        {
+            Connect();
+            return connectionCompleted.Task;
+        }
+
+        public Task SendAsync(byte[] buffer)
+        {
+            return SendAsync(channel => channel.Write(buffer));
+        }
+
         public Task SendAsync(string data)
         {
-            return base.SendAsync(DefaultEncoding.GetBytes(data));
+            return SendAsync(DefaultEncoding.GetBytes(data));
         }
 
         public Task ResizeTerminalAsync(TerminalSize size)
@@ -104,6 +242,22 @@ namespace Google.Solutions.Ssh
                 channel => ((SshShellChannel)channel).ResizePseudoTerminal(
                     size.Columns,
                     size.Rows));
+        }
+
+        //---------------------------------------------------------------------
+        // Inner classes.
+        //---------------------------------------------------------------------
+
+        protected internal class SendOperation
+        {
+            internal readonly TaskCompletionSource<uint> CompletionSource;
+            internal readonly Action<SshChannelBase> Operation;
+
+            internal SendOperation(Action<SshChannelBase> operation)
+            {
+                this.Operation = operation;
+                this.CompletionSource = new TaskCompletionSource<uint>();
+            }
         }
     }
 }
