b579c8b852c1c0c165239690c7fd5c2a85b35589
Johannes Passing <jpassing@hotmail.com>
2022-06-18 10:51:05 +1000

    Show badge icon indicating profile

---
+++ sources/Google.Solutions.IapDesktop.Application/Controls/BadgeIcon.cs
@@ -0,0 +1,114 @@
+﻿//
+// Copyright 2022 Google LLC
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Google.Solutions.IapDesktop.Application.Controls
+{
+    /// <summary>
+    /// 16x16 icon that can be used as overlay.
+    /// </summary>
+    public sealed class BadgeIcon : IDisposable
+    {
+        public Color BackColor { get; }
+        public IntPtr Handle { get; }
+
+        private BadgeIcon(IntPtr handle, Color backColor)
+        {
+            this.Handle = handle;
+            this.BackColor = backColor;
+        }
+
+        private static Color ColorFromText(string text)
+        {
+            ushort hue = (ushort)(text.GetHashCode() % 255);
+            ushort lightness = 128;
+            ushort saturation = 255;
+
+            return ColorTranslator.FromWin32(
+                NativeMethods.ColorHLSToRGB(hue, lightness, saturation));
+        }
+
+        public static BadgeIcon ForTextInitial(string text)
+        {
+            if (text == null || text.Length < 1)
+            {
+                throw new ArgumentException(nameof(text));
+            }
+
+            //
+            // Generate a color based on the input text.
+            //
+            var backColor = ColorFromText(text);
+
+            using (var bitmap = new Bitmap(16, 16, System.Drawing.Imaging.PixelFormat.Format32bppArgb))
+            using (var graphics = Graphics.FromImage(bitmap))
+            using (var format = StringFormat.GenericTypographic)
+            using (var font = new Font(FontFamily.GenericSansSerif, 6, FontStyle.Bold, GraphicsUnit.Point))
+            using (var brush = new SolidBrush(backColor))
+            {
+                format.Alignment = StringAlignment.Center;
+                format.LineAlignment = StringAlignment.Center;
+
+                //
+                // Draw text box at lower right corner.
+                //
+                var initial = text.Substring(0, 1).ToUpper();
+                var textSize = graphics.MeasureString(initial, font, new PointF(0, 0), format);
+                var textSizeWithMargin = textSize + new SizeF(5.0f, 2.0f);
+                var textBox = new RectangleF(
+                    new PointF(16, 16) - textSizeWithMargin,
+                    textSizeWithMargin);
+
+                graphics.FillRectangle(brush, textBox);
+                graphics.DrawString(
+                    initial,
+                    font,
+                    Brushes.White,
+                    textBox,
+                    format);
+
+                return new BadgeIcon(bitmap.GetHicon(), backColor);
+            }
+        }
+
+        public void Dispose()
+        {
+            NativeMethods.DestroyIcon(this.Handle);
+        }
+
+        private static class NativeMethods
+        {
+            [System.Runtime.InteropServices.DllImport("user32.dll", CharSet = CharSet.Auto)]
+            public extern static bool DestroyIcon(IntPtr handle);
+
+            [System.Runtime.InteropServices.DllImport("shlwapi.dll")]
+            public static extern int ColorHLSToRGB(ushort h, ushort l, ushort s);
+        }
+
+    }
+}
