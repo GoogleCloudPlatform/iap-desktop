9ba016881dbc8510e3bddad7a9ef86e5650f763f
Johannes Passing <jpassing@hotmail.com>
2022-07-25 11:27:33 +1000

    Add dialog

---
+++ sources/Google.Solutions.IapDesktop/Windows/ScopeErrorDialog.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace Google.Solutions.IapDesktop.Windows
+{
+    partial class ScopeErrorDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ScopeErrorDialog));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.headlineLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.okBurtton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.headlineLabel);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(419, 450);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(342, 65);
+            this.label1.TabIndex = 18;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // headlineLabel
+            // 
+            this.headlineLabel.AutoSize = true;
+            this.headlineLabel.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.headlineLabel.Location = new System.Drawing.Point(14, 15);
+            this.headlineLabel.Name = "headlineLabel";
+            this.headlineLabel.Size = new System.Drawing.Size(201, 30);
+            this.headlineLabel.TabIndex = 17;
+            this.headlineLabel.Text = "Authorization failed";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(19, 138);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(349, 237);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // okBurtton
+            // 
+            this.okBurtton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.okBurtton.Location = new System.Drawing.Point(305, 459);
+            this.okBurtton.Name = "okBurtton";
+            this.okBurtton.Size = new System.Drawing.Size(75, 23);
+            this.okBurtton.TabIndex = 2;
+            this.okBurtton.Text = "OK";
+            this.okBurtton.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 382);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(329, 26);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Without access to your Google Cloud data, IAP Desktop can\'t list or \r\naccess any " +
+    "of your VM instances.";
+            // 
+            // ScopeErrorDialog
+            // 
+            this.AcceptButton = this.okBurtton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.okBurtton;
+            this.ClientSize = new System.Drawing.Size(386, 491);
+            this.ControlBox = false;
+            this.Controls.Add(this.okBurtton);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "ScopeErrorDialog";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Authorization failed";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label headlineLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button okBurtton;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ sources/Google.Solutions.IapDesktop/Windows/ScopeErrorDialog.cs
@@ -0,0 +1,22 @@
+﻿using Google.Solutions.IapDesktop.Application.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Google.Solutions.IapDesktop.Windows
+{
+    public partial class ScopeErrorDialog : Form
+    {
+        public ScopeErrorDialog()
+        {
+            InitializeComponent();
+            this.headlineLabel.ForeColor = ThemeColors.HighlightBlue;
+        }
+    }
+}
