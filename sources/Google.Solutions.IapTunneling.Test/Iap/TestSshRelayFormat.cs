882c699a4bd2e957e985824df980154d9fc2f897
Johannes Passing <jpassing@hotmail.com>
2022-09-09 17:08:38 +1000

    Add SshRelayFormat (but not used yet)

---
+++ sources/Google.Solutions.IapTunneling.Test/Iap/TestSshRelayFormat.cs
@@ -0,0 +1,320 @@
+﻿//
+// Copyright 2022 Google LLC
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+using Google.Solutions.IapTunneling.Iap;
+using NUnit.Framework;
+using System;
+
+namespace Google.Solutions.IapTunneling.Test.Iap
+{
+    [TestFixture]
+    public class TestSshRelayFormat : IapFixtureBase
+    {
+        //---------------------------------------------------------------------
+        // DecodeTag.
+        //---------------------------------------------------------------------
+
+        [Test]
+        public void WhenMessageComplete_ThenDecodeTagReturnsTag()
+        {
+            var message = new byte[] { 0, 4, 99, 99, 99, 99, 99 };
+
+            var bytesRead = SshRelayFormat.DecodeTag(message, out var tag);
+
+            Assert.AreEqual(2, bytesRead);
+            Assert.AreEqual(MessageTag.DATA, tag);
+        }
+
+        [Test]
+        public void WhenMessageTruncated_ThenDecodeTagThrowsException()
+        {
+            var message = new byte[] { 0 };
+
+            Assert.Throws<ArgumentException>(
+                () => SshRelayFormat.DecodeTag(message, out var tag));
+        }
+
+        //---------------------------------------------------------------------
+        // DecodeConnectSuccessSid.
+        //---------------------------------------------------------------------
+
+        [Test]
+        public void WhenMessageComplete_ThenDecodeConnectSuccessSidReturnsSid()
+        {
+            var message = new byte[] {
+                0, 1, 
+                0, 0, 0, 3,
+                (byte)'S', (byte)'i', (byte)'d'
+            };
+
+            var bytesRead = SshRelayFormat.DecodeConnectSuccessSid(message, out var sid);
+
+            Assert.AreEqual(9, bytesRead);
+            Assert.AreEqual("Sid", sid);
+        }
+
+        [Test]
+        public void WhenMessageTruncated_ThenDecodeConnectSuccessSidThrowsException()
+        {
+            var message = new byte[] {
+                0, 1,
+                0, 0, 0, 4,
+                (byte)'S', (byte)'i', (byte)'d'
+            };
+
+            Assert.Throws<ArgumentException>(
+                () => SshRelayFormat.DecodeConnectSuccessSid(message, out var sid));
+        }
+
+        //---------------------------------------------------------------------
+        // DecodeReconnectAck.
+        //---------------------------------------------------------------------
+
+        [Test]
+        public void WhenMessageComplete_ThenDecodeReconnectAckReturnsAck()
+        {
+            var message = new byte[] {
+                0, 2,
+                0, 0, 0, 0, 0, 0, 0, 3
+            };
+
+            var bytesRead = SshRelayFormat.DecodeReconnectAck(message, out var ack);
+
+            Assert.AreEqual(10, bytesRead);
+            Assert.AreEqual(3, ack);
+        }
+
+        [Test]
+        public void WhenMessageTruncated_ThenDecodeReconnectAckThrowsException()
+        {
+            var message = new byte[] {
+                0, 1,
+                1, 2, 3, 4, 5, 6, 7
+            };
+
+            Assert.Throws<ArgumentException>(
+                () => SshRelayFormat.DecodeReconnectAck(message, out var ack));
+        }
+
+        //---------------------------------------------------------------------
+        // DecodeReconnectAck.
+        //---------------------------------------------------------------------
+
+        [Test]
+        public void WhenMessageComplete_ThenDecodeAckReturnsAck()
+        {
+            var message = new byte[] {
+                0, 7,
+                0, 0, 0, 0, 0, 0, 0, 3
+            };
+
+            var bytesRead = SshRelayFormat.DecodeAck(message, out var ack);
+
+            Assert.AreEqual(10, bytesRead);
+            Assert.AreEqual(3, ack);
+        }
+
+        [Test]
+        public void WhenMessageTruncated_ThenDecodeAckThrowsException()
+        {
+            var message = new byte[] {
+                0, 7,
+                1, 2, 3, 4, 5, 6, 7
+            };
+
+            Assert.Throws<ArgumentException>(
+                () => SshRelayFormat.DecodeAck(message, out var ack));
+        }
+
+        //---------------------------------------------------------------------
+        // DecodeData.
+        //---------------------------------------------------------------------
+
+        [Test]
+        public void WhenMessageComplete_ThenDecodeDataReturnsData()
+        {
+            var message = new byte[] {
+                0, 4,
+                0, 0, 0, 1, 
+                (byte)'D'
+            };
+
+            var data = new byte[1];
+            var bytesRead = SshRelayFormat.DecodeData(message, data, 0, (uint)data.Length);
+
+            Assert.AreEqual(7, bytesRead);
+            Assert.AreEqual('D', data[0]);
+        }
+
+        [Test]
+        public void WhenTargetIndexNotNull_ThenDecodeDataReturnsData()
+        {
+            var message = new byte[] {
+                0, 4,
+                0, 0, 0, 1,
+                (byte)'D'
+            };
+
+            var data = new byte[2];
+            var bytesRead = SshRelayFormat.DecodeData(message, data, 1, 1);
+
+            Assert.AreEqual(7, bytesRead);
+            Assert.AreEqual('D', data[1]);
+        }
+
+        [Test]
+        public void WhenBufferTooSmall_ThenDecodeDataThrowsException()
+        {
+            var message = new byte[] {
+                0, 4,
+                0, 0, 0, 2,
+                (byte)'D'
+            };
+
+            var data = new byte[1];
+
+            Assert.Throws<ArgumentException>(
+                () => SshRelayFormat.DecodeData(message, data, 1, (uint)data.Length));
+        }
+
+        [Test]
+        public void WhenBufferTooSmallForData_ThenDecodeDataThrowsException()
+        {
+            var message = new byte[] {
+                0, 4,
+                0, 0, 0, 2,
+                (byte)'D', (byte) 'a'
+            };
+
+            var data = new byte[1];
+
+            Assert.Throws<IndexOutOfRangeException>(
+                () => SshRelayFormat.DecodeData(message, data, 0, (uint)data.Length));
+        }
+
+        //---------------------------------------------------------------------
+        // EncodeAck.
+        //---------------------------------------------------------------------
+
+        [Test]
+        public void WhenBufferSufficient_ThenEncodeAckSucceeds()
+        {
+            var message = new byte[10];
+
+            var bytesWritten = SshRelayFormat.EncodeAck(message, 77);
+
+            Assert.AreEqual(10, bytesWritten);
+            CollectionAssert.AreEquivalent(
+                new byte[]
+                {
+                    0, 7,
+                    0, 0, 0, 0, 0, 0, 0, 77
+                },
+                message);
+        }
+
+        [Test]
+        public void WhenBufferTooSmall_ThenEncodeAckThrowsException()
+        {
+            var message = new byte[9];
+
+            Assert.Throws<ArgumentException>(
+                () => SshRelayFormat.EncodeAck(message, 77));
+        }
+
+        //---------------------------------------------------------------------
+        // EncodeData.
+        //---------------------------------------------------------------------
+
+        [Test]
+        public void WhenDataIsEmpty_ThenEncodeDataThrowsException()
+        {
+            var message = new byte[SshRelayFormat.MaxDataPayloadLength];
+            var data = new byte[0];
+
+            Assert.Throws<ArgumentException>(
+                () => SshRelayFormat.EncodeData(message, data, 0, (uint)data.Length));
+        }
+
+        [Test]
+        public void WhenDataTooLarge_ThenEncodeDataThrowsException()
+        {
+            var message = new byte[SshRelayFormat.MaxDataPayloadLength + 1];
+            var data = new byte[SshRelayFormat.MaxDataPayloadLength + 1];
+
+            Assert.Throws<ArgumentException>(
+                () => SshRelayFormat.EncodeData(message, data, 0, (uint)data.Length));
+        }
+
+        [Test]
+        public void WhenBufferTooSmall_ThenEncodeDataThrowsException()
+        {
+            var message = new byte[SshRelayFormat.MaxMessageSize - 1];
+            var data = new byte[SshRelayFormat.MaxDataPayloadLength];
+
+            Assert.Throws<ArgumentException>(
+                () => SshRelayFormat.EncodeData(message, data, 0, (uint)data.Length));
+        }
+
+        [Test]
+        public void WhenDataIsMaxSize_ThenEncodeDataSucceeds()
+        {
+            var message = new byte[SshRelayFormat.MaxMessageSize];
+            var data = new byte[SshRelayFormat.MaxDataPayloadLength];
+            data[0] = (byte)'A';
+            data[SshRelayFormat.MaxDataPayloadLength - 1] = (byte)'Z';
+
+            var bytesWritten = SshRelayFormat.EncodeData(
+                message, 
+                data,
+                0,
+                SshRelayFormat.MaxDataPayloadLength);
+
+            Assert.AreEqual(SshRelayFormat.MaxMessageSize, bytesWritten);
+            Assert.AreEqual(SshRelayFormat.MaxMessageSize, bytesWritten);
+            Assert.AreEqual((byte)'Z', message[SshRelayFormat.MaxMessageSize - 1]);
+        }
+
+        [Test]
+        public void WhenIndexNotZero_ThenEncodeDataSucceeds()
+        {
+            var message = new byte[7];
+            var data = new byte[SshRelayFormat.MaxDataPayloadLength + 1];
+            data[SshRelayFormat.MaxDataPayloadLength] = (byte)'D';
+
+            var bytesWritten = SshRelayFormat.EncodeData(
+                message,
+                data,
+                SshRelayFormat.MaxDataPayloadLength,
+                1);
+
+            Assert.AreEqual(7, bytesWritten);
+            CollectionAssert.AreEquivalent(
+                new byte[]
+                {
+                    0, 4,
+                    0, 0, 0, 1,
+                    (byte)'D'
+                },
+                message);
+        }
+    }
+}
---
+++ sources/Google.Solutions.IapTunneling/Iap/SshRelayFormat.cs
@@ -0,0 +1,225 @@
+﻿//
+// Copyright 2022 Google LLC
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace Google.Solutions.IapTunneling.Iap
+{
+    /// <summary>
+    /// Encode/decode SSH Relay messages.
+    /// </summary>
+    internal static class SshRelayFormat
+    {
+        //
+        // The protocol defines 16K as the max.
+        //
+        public const uint MaxArrayLength = 16 * 1024;
+
+        public const uint MaxDataPayloadLength = MaxArrayLength;
+
+        //
+        // DATA messages are the largest.
+        //
+        public const uint MaxMessageSize = 6 + MaxArrayLength;
+
+        private static void ThrowIfBufferSmallerThan(
+            byte[] messageBuffer,
+            uint minSize)
+        {
+            if (messageBuffer == null || messageBuffer.Length < minSize)
+            {
+                throw new ArgumentException("Message is truncated");
+            }
+        }
+
+        //---------------------------------------------------------------------
+        // Reading.
+        //---------------------------------------------------------------------
+
+        public static uint DecodeTag(
+            byte[] messageBuffer,
+            out MessageTag tag)
+        {
+            //
+            // 00-01 (len=2): Tag
+            //
+
+            ThrowIfBufferSmallerThan(messageBuffer, sizeof(UInt16));
+
+            tag = (MessageTag)BigEndian.DecodeUInt16(messageBuffer, 0);
+            return sizeof(MessageTag);
+        }
+
+        public static uint DecodeConnectSuccessSid(
+            byte[] messageBuffer,
+            out string sid)
+        {
+            //
+            // 00-01 (len=2): Tag
+            // 02-05 (len=4): Array length
+            // 06-*         : SID
+            //
+
+            ThrowIfBufferSmallerThan(messageBuffer, sizeof(UInt16) + sizeof(UInt32));
+            Debug.Assert((MessageTag)BigEndian.DecodeUInt16(messageBuffer, 0) == MessageTag.CONNECT_SUCCESS_SID);
+
+            var arrayLength = BigEndian.DecodeUInt32(messageBuffer, 2);
+            Debug.Assert(arrayLength <= MaxArrayLength);
+
+            ThrowIfBufferSmallerThan(messageBuffer, sizeof(UInt16) + sizeof(UInt32) + arrayLength);
+
+            sid = new ASCIIEncoding().GetString(
+                messageBuffer,
+                6,
+                (int)arrayLength);
+
+            return sizeof(UInt16) + sizeof(UInt32) + arrayLength;
+        }
+
+        public static uint DecodeReconnectAck(
+            byte[] messageBuffer,
+            out ulong ack)
+        {
+            //
+            // 00-01 (len=2): Tag
+            // 02-0A (len=8): ACK
+            //
+
+            ThrowIfBufferSmallerThan(messageBuffer, sizeof(UInt16) + sizeof(UInt64));
+            Debug.Assert((MessageTag)BigEndian.DecodeUInt16(messageBuffer, 0) == MessageTag.RECONNECT_SUCCESS_ACK);
+
+            ack = BigEndian.DecodeUInt64(messageBuffer, 2);
+
+            return sizeof(UInt16) + sizeof(UInt64);
+        }
+
+        public static uint DecodeAck(
+            byte[] messageBuffer,
+            out ulong ack)
+        {
+            //
+            // 00-01 (len=2): Tag
+            // 02-0A (len=8): ACK
+            //
+
+            ThrowIfBufferSmallerThan(messageBuffer, sizeof(UInt16) + sizeof(UInt64));
+            Debug.Assert((MessageTag)BigEndian.DecodeUInt16(messageBuffer, 0) == MessageTag.ACK);
+
+            ack = BigEndian.DecodeUInt64(messageBuffer, 2);
+
+            return sizeof(UInt16) + sizeof(UInt64);
+        }
+
+        public static uint DecodeData(
+            byte[] messageBuffer,
+            byte[] data,
+            uint dataOffset,
+            uint dataLength)
+        {
+            //
+            // 00-01 (len=2): Tag
+            // 02-05 (len=4): Array length
+            // 06-*         : Data
+            //
+
+            ThrowIfBufferSmallerThan(messageBuffer, sizeof(UInt16) + sizeof(UInt32) + 1);
+            Debug.Assert((MessageTag)BigEndian.DecodeUInt16(messageBuffer, 0) == MessageTag.DATA);
+
+            var arrayLength = BigEndian.DecodeUInt32(messageBuffer, 2);
+            Debug.Assert(arrayLength <= MaxArrayLength);
+            Debug.Assert(arrayLength > 0);
+
+            ThrowIfBufferSmallerThan(messageBuffer, sizeof(UInt16) + sizeof(UInt32) + arrayLength);
+
+            if (arrayLength > dataLength || dataLength + dataOffset > data.Length)
+            {
+                throw new IndexOutOfRangeException("Read buffer is too small");
+            }
+
+            Array.Copy(
+                messageBuffer,
+                6,
+                data,
+                dataOffset,
+                (int)arrayLength);
+
+            return sizeof(UInt16) + sizeof(UInt32) + arrayLength;
+        }
+
+        //---------------------------------------------------------------------
+        // Writing.
+        //---------------------------------------------------------------------
+
+        public static uint EncodeAck(
+            byte[] messageBuffer,
+            ulong ack)
+        {
+            //
+            // 00-01 (len=2): Tag
+            // 02-0A (len=8): ACK
+            //
+
+            ThrowIfBufferSmallerThan(messageBuffer, sizeof(UInt16) + sizeof(UInt64));
+
+            BigEndian.EncodeUInt16((ushort)MessageTag.ACK, messageBuffer, 0);
+            BigEndian.EncodeUInt64(ack, messageBuffer, 2);
+
+            return sizeof(UInt16) + sizeof(UInt64);
+        }
+
+        public static uint EncodeData(
+            byte[] messageBuffer,
+            byte[] data,
+            uint dataOffset,
+            uint dataLength)
+        {
+            //
+            // 00-01 (len=2): Tag
+            // 02-05 (len=4): Array length
+            // 06-*         : Data
+            //
+
+            if (dataLength > MaxArrayLength)
+            {
+                throw new ArgumentException($"At most {MaxArrayLength} bytes can be sent at once");
+            }
+            else if (dataLength == 0)
+            {
+                throw new ArgumentException($"At least 1 byte must be sent at once");
+            }
+
+            ThrowIfBufferSmallerThan(messageBuffer, sizeof(UInt16) + sizeof(UInt32) + dataLength);
+
+            BigEndian.EncodeUInt16((ushort)MessageTag.DATA, messageBuffer, 0);
+            BigEndian.EncodeUInt32(dataLength, messageBuffer, 2);
+            Array.Copy(
+                data,
+                (int)dataOffset,
+                messageBuffer,
+                6,
+                dataLength);
+
+            return sizeof(UInt16) + sizeof(UInt32) + dataLength;
+        }
+    }
+}
