a85ecff04c88a84616185243c3bd8a73f3a024c7
Johannes Passing <jpassing@hotmail.com>
2022-09-15 18:39:30 +1000

    Add test rig for WebSocketStream (WIP)

---
+++ sources/Google.Solutions.IapTunneling.Test/Net/TestWebSocketStream.cs
@@ -0,0 +1,84 @@
+﻿//
+// Copyright 2022 Google LLC
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+using Google.Solutions.IapTunneling.Net;
+using Google.Solutions.Testing.Common;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Google.Solutions.IapTunneling.Test.Net
+{
+    [TestFixture]
+    public class TestWebSocketStream : IapFixtureBase
+    {
+        private WebSocketServer server;
+
+        [SetUp]
+        public void StartServer()
+        {
+            this.server = new WebSocketServer();
+        }
+
+        [TearDown]
+        public void StopServer()
+        {
+            this.server.Dispose();
+        }
+
+        [Test]
+        public async Task WhenConnectionClosedImmediately_ThenReadThrowsWebSocketStreamClosedByServerException()
+        {
+            using (var client = new ClientWebSocket())
+            {
+                var connectTask = client.ConnectAsync(
+                        this.server.Endpoint,
+                        CancellationToken.None);
+
+                using (var server = await this.server.AcceptConnectionAsync())
+                {
+                    await connectTask.ConfigureAwait(false);
+
+                    using (var clientStream = new WebSocketStream(
+                        client,
+                        64))
+                    {
+                        await server
+                            .CloseOutputAsync(WebSocketCloseStatus.InternalServerError)
+                            .ConfigureAwait(false);
+
+                        var buffer = new byte[64];
+
+                        ExceptionAssert.ThrowsAggregateException<WebSocketStreamClosedByServerException>(
+                            () => clientStream
+                                .ReadAsync(buffer, 0, buffer.Length, CancellationToken.None)
+                                .Wait());
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ sources/Google.Solutions.IapTunneling.Test/Net/WebSocketServer.cs
@@ -0,0 +1,124 @@
+﻿using Google.Solutions.IapTunneling.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Google.Solutions.IapTunneling.Test.Net
+{
+    internal sealed class WebSocketServer : IDisposable
+    {
+        private readonly HttpListener listener;
+
+        public Uri Endpoint { get; }
+
+        public WebSocketServer()
+        {
+            this.listener = new HttpListener();
+
+            var port = PortFinder.FindFreeLocalPort();
+            this.Endpoint = new Uri($"ws://localhost:{port}/");
+
+            listener.Prefixes.Add($"http://localhost:{port}/");
+            listener.Start();
+        }
+
+        public async Task<WebSocketConnection> AcceptConnectionAsync()
+        {
+            var context = await listener
+                .GetContextAsync()
+                .ConfigureAwait(false);
+
+            if (!context.Request.IsWebSocketRequest)
+            {
+                context.Response.StatusCode = 400;
+                throw new InvalidOperationException(
+                    "HTTP request is not a WebSocket request");
+            }
+            else
+            {
+                return new WebSocketConnection(await context
+                    .AcceptWebSocketAsync(null)
+                    .ConfigureAwait(false));
+            }
+        }
+
+        public void Dispose()
+        {
+            this.listener.Stop();
+        }
+    }
+
+    
+
+    internal sealed class WebSocketConnection : IDisposable
+    { 
+        public HttpListenerWebSocketContext Contect { get; }
+
+        private void ThrowIfNotConnected()
+        {
+            if (this.Contect == null)
+            {
+                throw new InvalidOperationException("WebSocket not connected");
+            }
+        }
+
+        public WebSocketConnection(HttpListenerWebSocketContext webSocket)
+        {
+            this.Contect = webSocket;
+        }
+
+        public async Task SendBinaryFrameAsync(byte[] data)
+        {
+            ThrowIfNotConnected();
+
+            await this.Contect.WebSocket.SendAsync(
+                    new ArraySegment<byte>(data),
+                    WebSocketMessageType.Binary,
+                    true,
+                    CancellationToken.None)
+                .ConfigureAwait(false);
+        }
+
+        public async Task ReceiveBinaryFrameAsync(byte[] buffer)
+        {
+            ThrowIfNotConnected();
+
+            await this.Contect.WebSocket.ReceiveAsync(
+                    new ArraySegment<byte>(buffer),
+                    CancellationToken.None)
+                .ConfigureAwait(false);
+        }
+
+        public async Task CloseOutputAsync(WebSocketCloseStatus status)
+        {
+            ThrowIfNotConnected();
+
+            await this.Contect.WebSocket.CloseOutputAsync(
+                    status,
+                    status.ToString(),
+                    CancellationToken.None)
+                .ConfigureAwait(false);
+        }
+
+        public async Task CloseAsync(WebSocketCloseStatus status)
+        {
+            ThrowIfNotConnected();
+
+            await this.Contect.WebSocket.CloseAsync(
+                    status,
+                    status.ToString(),
+                    CancellationToken.None)
+                .ConfigureAwait(false);
+        }
+
+        public void Dispose()
+        {
+            this.Contect.WebSocket.Dispose();
+        }
+    }
+}
