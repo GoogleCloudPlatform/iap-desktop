dea8d3916630407b910e1bc8068cee0c3263b88c
Johannes Passing <jpassing@hotmail.com>
2022-10-07 15:17:28 +1100

    b/249915031 Update to OpenSSL 1.1.1p
    Add a custom port for OpenSSL 1.1.1 as the upstream
    port has switched to OpenSSL 3.

--- dependencies/sources/libssh2/vcpkg-ports/openssl/portfile.cmake
+++ dependencies/sources/libssh2/vcpkg-ports/openssl/portfile.cmake
@@ -2,12 +2,12 @@ if(EXISTS "${CURRENT_INSTALLED_DIR}/include/openssl/ssl.h")
     message(FATAL_ERROR "Can't build openssl if libressl/boringssl is installed. Please remove libressl/boringssl, and try install openssl again if you need it.")
 endif()
 
-set(OPENSSL_VERSION 1.1.1m)
+set(OPENSSL_VERSION 1.1.1p)
 vcpkg_download_distfile(
     ARCHIVE
     URLS "https://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz" "https://www.openssl.org/source/old/1.1.1/openssl-${OPENSSL_VERSION}.tar.gz"
     FILENAME "openssl-${OPENSSL_VERSION}.tar.gz"
-    SHA512 ba0ef99b321546c13385966e4a607734df38b96f6ed45c4c67063a5f8d1482986855279797a6920d9f86c2ec31ce3e104dcc62c37328caacdd78aec59aa66156
+    SHA512 203470b1cd37bdbfabfec5ef37fc97c991d9943f070c988316f6396b09dae7cea16ac884bd8646dbf7dd1ed40ebde6bdfa5700beee2d714d07c97cc70b4e48d9
 )
 
 vcpkg_find_acquire_program(PERL)
