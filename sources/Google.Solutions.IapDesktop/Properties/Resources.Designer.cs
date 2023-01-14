fc5ff67f7dd589a24f58b69709a220bde9c01d89
Johannes Passing <jpassing@hotmail.com>
2023-01-14 18:10:53 +1100

    Update Event Log icon

--- sources/Google.Solutions.IapDesktop/Properties/Resources.Designer.cs
+++ sources/Google.Solutions.IapDesktop/Properties/Resources.Designer.cs
@@ -103,16 +103,6 @@ namespace Google.Solutions.IapDesktop.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap project1 {
-            get {
-                object obj = ResourceManager.GetObject("project1", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap Splash {
             get {
                 object obj = ResourceManager.GetObject("Splash", resourceCulture);
