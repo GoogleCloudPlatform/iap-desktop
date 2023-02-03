abfbe5a0418ed6eea7b141e455e74ba5b5de869e
Johannes Passing <jpassing@hotmail.com>
2023-02-03 14:48:08 +1100

    Delete ThemeColors

---
+++ sources/Google.Solutions.IapDesktop.Extensions.Shell/Views/Credentials/NewCredentialsDialog.cs
@@ -0,0 +1,125 @@
+﻿//
+// Copyright 2020 Google LLC
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+using Google.Solutions.Common.Locator;
+using Google.Solutions.IapDesktop.Application.ObjectModel;
+using Google.Solutions.IapDesktop.Application.Services.Authorization;
+using Google.Solutions.IapDesktop.Application.Services.Integration;
+using Google.Solutions.IapDesktop.Application.Services.Windows;
+using Google.Solutions.IapDesktop.Extensions.Shell.Services.ConnectionSettings;
+using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Google.Solutions.IapDesktop.Extensions.Shell.Views.Credentials
+{
+    public interface INewCredentialsDialog
+    {
+        Task ShowDialogAsync(
+            IWin32Window owner,
+            InstanceLocator instanceRef,
+            ConnectionSettingsBase settings,
+            bool silent);
+
+        Task<bool> IsGrantedPermissionToGenerateCredentials(
+            InstanceLocator instanceRef);
+    }
+
+    [Service(typeof(INewCredentialsDialog))]
+    public class NewCredentialsDialog : INewCredentialsDialog
+    {
+        private readonly IServiceProvider serviceProvider;
+
+        public NewCredentialsDialog(IServiceProvider serviceProvider) // TODO: Inject Service<>
+        {
+            this.serviceProvider = serviceProvider;
+        }
+
+        public async Task ShowDialogAsync(
+            IWin32Window owner,
+            InstanceLocator instanceLocator,
+            ConnectionSettingsBase settings,
+            bool silent)
+        {
+            var username = string.IsNullOrEmpty(settings.RdpUsername.StringValue)
+                ? this.serviceProvider
+                    .GetService<IAuthorizationSource>()
+                    .Authorization
+                    .SuggestWindowsUsername()
+                : settings.RdpUsername.StringValue;
+
+            if (!silent)
+            {
+                //
+                // Prompt user to customize the defaults.
+                //
+
+                var dialogResult = this.serviceProvider
+                    .GetService<IGenerateCredentialsDialog>()
+                    .ShowDialog(owner,
+                    username);
+
+                if (dialogResult.Result == DialogResult.OK)
+                {
+                    username = dialogResult.Username;
+                }
+                else
+                {
+                    throw new OperationCanceledException();
+                }
+            }
+
+            var credentials = await this.serviceProvider.GetService<IJobService>().RunInBackground(
+                new JobDescription("Generating Windows logon credentials..."),
+                token => this.serviceProvider
+                    .GetService<IWindowsCredentialService>()
+                    .CreateWindowsCredentialsAsync(
+                        instanceLocator,
+                        username,
+                        UserFlags.AddToAdministrators,
+                        token))
+                .ConfigureAwait(true);
+
+            if (!silent)
+            {
+                this.serviceProvider.GetService<IShowCredentialsDialog>().ShowDialog(
+                    owner,
+                    credentials.UserName,
+                    credentials.Password);
+            }
+
+            // Save credentials.
+            settings.RdpUsername.StringValue = credentials.UserName;
+            settings.RdpPassword.ClearTextValue = credentials.Password;
+
+            // NB. The computer might be joined to a domain, therefore force a local logon.
+            settings.RdpDomain.StringValue = ".";
+        }
+
+        public async Task<bool> IsGrantedPermissionToGenerateCredentials(InstanceLocator instance)
+        {
+            return await this.serviceProvider
+                .GetService<IWindowsCredentialService>()
+                .IsGrantedPermissionToCreateWindowsCredentialsAsync(instance)
+                .ConfigureAwait(false);
+        }
+    }
+}
---
+++ sources/Google.Solutions.IapDesktop.Extensions.Shell/Views/Credentials/NewCredentialsView.cs
@@ -0,0 +1,103 @@
+﻿//
+// Copyright 2019 Google LLC
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+using Google.Solutions.Common.Diagnostics;
+using Google.Solutions.IapDesktop.Application.ObjectModel;
+using Google.Solutions.IapDesktop.Application.Theme;
+using Google.Solutions.IapDesktop.Application.Views;
+using Google.Solutions.Mvvm.Binding;
+using System.Windows.Forms;
+
+namespace Google.Solutions.IapDesktop.Extensions.Shell.Views.Credentials
+{
+    public interface IGenerateCredentialsDialog
+    {
+        GenerateCredentialsDialogResult ShowDialog(
+            IWin32Window owner,
+            string suggestedUsername);
+    }
+
+    [Service(typeof(IGenerateCredentialsDialog))]
+    [SkipCodeCoverage("View code")]
+    public partial class NewCredentialsView : Form, IGenerateCredentialsDialog
+    {
+        private readonly NewCredentialsViewModel viewModel
+            = new NewCredentialsViewModel();
+
+        public NewCredentialsView(IThemeService themeService)
+        {
+            InitializeComponent();
+
+            themeService.DialogTheme.ApplyTo(this);
+
+            this.usernameText.BindProperty(
+                c => c.Text,
+                this.viewModel,
+                m => m.Username,
+                this.components);
+            this.usernameReservedLabel.BindReadonlyProperty(
+                c => c.Visible,
+                this.viewModel,
+                m => m.IsUsernameReserved,
+                this.components);
+
+            // Bind buttons.
+            this.okButton.BindReadonlyProperty(
+                c => c.Enabled,
+                this.viewModel,
+                m => m.IsOkButtonEnabled,
+                this.components);
+        }
+
+        private void usernameText_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Cancel any keypresses of disallowed characters.
+            e.Handled = !this.viewModel.IsAllowedCharacterForUsername(e.KeyChar);
+        }
+
+        public GenerateCredentialsDialogResult ShowDialog(
+            IWin32Window owner,
+            string suggestedUsername)
+        {
+            this.viewModel.Username = suggestedUsername;
+
+            var result = ShowDialog(owner);
+
+            return new GenerateCredentialsDialogResult(
+                result,
+                this.viewModel.Username);
+        }
+    }
+
+    public class GenerateCredentialsDialogResult
+    {
+        public DialogResult Result { get; }
+        public string Username { get; }
+
+        public GenerateCredentialsDialogResult(
+            DialogResult result,
+            string username)
+        {
+            this.Result = result;
+            this.Username = username;
+        }
+    }
+}
