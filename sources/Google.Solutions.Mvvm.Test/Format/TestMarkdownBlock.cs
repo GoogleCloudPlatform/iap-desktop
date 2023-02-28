93b5f7d80528a7a0b47fa2ffdc65d6cc51411216
Johannes Passing <jpassing@hotmail.com>
2023-02-28 16:23:32 +1100

    Basic block parser

---
+++ sources/Google.Solutions.Mvvm.Test/Format/TestMarkdownBlock.cs
@@ -0,0 +1,63 @@
+﻿//
+// Copyright 2023 Google LLC
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+using Google.Solutions.Mvvm.Format;
+using NUnit.Framework;
+using System.Linq;
+
+namespace Google.Solutions.Mvvm.Test.Format
+{
+    [TestFixture]
+    public class TestMarkdownBlock
+    {
+        //---------------------------------------------------------------------
+        // Document.
+        //---------------------------------------------------------------------
+
+        [Test]
+        public void EmptyDocument()
+        {
+            var doc = MarkdownBlock.Parse("");
+            Assert.IsNotNull(doc);
+        }
+
+        //---------------------------------------------------------------------
+        // Heading.
+        //---------------------------------------------------------------------
+
+        [Test]
+        public void Headings()
+        {
+            var doc = MarkdownBlock.Parse(
+                "\n" +
+                "# heading1    \n" +
+                "##      \theading 2\n" +
+                "### heading3\n" +
+                "#### heading4 \t\n" +
+                "##### heading5\n" +
+                "\n" +
+                "####### heading6");
+            Assert.IsNotNull(doc);
+            Assert.AreEqual("", doc.ToString());
+
+        }
+    }
+}
---
+++ sources/Google.Solutions.Mvvm/Format/MarkdownNode.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Google.Solutions.Mvvm.Format
+{
+    internal static class MarkdownBlock
+    {
+        private static readonly char[] NonBreakingWhitespace = new char[] { ' ', '\t' };
+        private static readonly char[] UnorderedListBullets = new char[] { '*', '-', '+' };
+
+        internal abstract class Block
+        {
+            private Block next;
+            private Block firstChild;
+            private Block lastChild;
+
+            public IEnumerable<Block> Children
+            {
+                get
+                {
+                    if (this.firstChild == null)
+                    {
+                        yield break;
+                    }
+                    else
+                    {
+                        for (var block = this.firstChild; block != null;  block = block.next)
+                        {
+                            yield return block;
+                        }
+                    }
+                }
+            }
+
+            private void AppendBlock(Block block)
+            {
+                if (this.firstChild == null)
+                {
+                    Debug.Assert(this.lastChild == null);
+                    this.firstChild = block;
+                    this.lastChild = this.firstChild;
+                }
+                else
+                {
+                    this.lastChild.next = block;
+                    this.lastChild = block;
+                }
+            }
+
+            internal virtual Block CreateBlock(string line)
+            {
+                if (UnorderedListItemBlock.IsUnorderedListItemBlock(line))
+                {
+                    return new UnorderedListItemBlock(line);
+                }
+                else
+                {
+                    return new TextBlock(line);
+                }
+            }
+
+            internal virtual bool TryConsume(string line)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    //
+                    // An empty line always ends a block, but does
+                    // not start a new one yet.
+                    //
+                    return false;
+                }
+                else if (this.lastChild != null &&
+                    this.lastChild.TryConsume(line))
+                {
+                    //
+                    // Continuation of last block.
+                    //
+                    return true;
+                }
+                else
+                {
+                    //
+                    // Last block is closed, append a new block.
+                    //
+                    AppendBlock(CreateBlock(line));
+                    return true;
+                }
+            }
+
+            public abstract string Value { get; }
+
+            public override string ToString()
+            {
+                var buffer = new StringBuilder();
+                
+                void Visit(Block block, int level)
+                {
+                    buffer.Append(new string(' ', level));
+                    buffer.Append(block.Value);
+                    buffer.Append('\n');
+
+                    foreach (var child in block.Children)
+                    {
+                        Visit(child, level + 1);
+                    }
+                }
+
+                Visit(this, 0);
+
+                return buffer.ToString(); ;
+            }
+        }
+
+        internal class DocumentBlock : Block
+        {
+            internal override Block CreateBlock(string line)
+            {
+                if (HeadingBlock.IsHeadingBlock(line))
+                {
+                    return new HeadingBlock(line);
+                }
+                else
+                {
+                    return base.CreateBlock(line);
+                }
+            }
+
+            public override string Value => "[Document]";
+        }
+
+        internal class HeadingBlock : Block
+        {
+            public int Level { get; }
+            public string Text { get; }
+
+            public static bool IsHeadingBlock(string line)
+            {
+                var index = line.IndexOfAny(NonBreakingWhitespace);
+                return index > 0 && line.Substring(0, index).All(c => c == '#');
+            }
+
+            public HeadingBlock(string line)
+            {
+                Debug.Assert(IsHeadingBlock(line));
+
+                var index = line.IndexOfAny(NonBreakingWhitespace);
+                this.Level = line.Substring(0, index).Count();
+                this.Text = line.Substring(index).Trim();
+            }
+
+            internal override bool TryConsume(string line)
+            {
+                //
+                // Headings are always single-line.
+                //
+                return false;
+            }
+
+            public override string Value => $"[Heading level={this.Level}] {this.Text}";
+        }
+
+        internal class TextBlock : Block
+        {
+            public string Text { get; private set; }
+
+            public TextBlock(string text)
+            {
+                this.Text = text;
+            }
+
+            internal override bool TryConsume(string line)
+            {
+                Debug.Assert(!string.IsNullOrWhiteSpace(line));
+                this.Text += line;
+                return true;
+            }
+
+            public override string Value => "[Text] " + this.Text;
+        }
+
+        internal class UnorderedListItemBlock : Block
+        {
+            public char Bullet { get;}
+            public string Indent { get; }
+
+            public static bool IsUnorderedListItemBlock(string line)
+            {
+                return line.Length > 3 && 
+                    UnorderedListBullets.Contains(line[0]) && 
+                    NonBreakingWhitespace.Contains(line[1]);
+            }
+
+
+            public UnorderedListItemBlock(string line)
+            {
+                Debug.Assert(IsUnorderedListItemBlock(line));
+
+                this.Bullet = line[0];
+
+                var indent = 1;
+                while (line[indent] == ' ')
+                {
+                    indent++;
+                }
+
+                this.Indent = new string(' ', indent);
+            }
+
+            internal override bool TryConsume(string line)
+            {
+                if (!line.StartsWith(this.Indent))
+                {
+                    //
+                    // Line doesn't have the minimum amount of indentation,
+                    // so it can't be a continuation.
+                    //
+                    // NB. We don't support lazy continations.
+                    //
+                    return false;
+                }
+                else
+                {
+                    return base.TryConsume(line.Substring(this.Indent.Length));
+                }
+            }
+
+            public override string Value => $"[UnorderedList bullet={this.Bullet}]";
+        }
+
+        public static DocumentBlock Parse(TextReader reader)
+        {
+            var document = new DocumentBlock();
+            bool allWhitespace = true;
+            while (true)
+            {
+                var line = reader.ReadLine();
+                if (line == null)
+                {
+                    break;
+                }
+                else if (allWhitespace && string.IsNullOrWhiteSpace(line))
+                {
+                    // Ignore leading blank lines.
+                }
+                else
+                {
+                    allWhitespace = false;
+                    document.TryConsume(line);
+                }
+            }
+
+            return document;
+        }
+
+        public static DocumentBlock Parse(string markdown)
+        {
+            using (var reader = new StringReader(markdown))
+            {
+                return Parse(reader);
+            }
+        }
+    }
+}
