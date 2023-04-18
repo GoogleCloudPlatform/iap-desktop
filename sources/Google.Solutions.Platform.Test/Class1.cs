2a938e1db260448a36f9bf5c20a395f8450d88aa
Johannes Passing <jpassing@hotmail.com>
2023-04-18 12:23:08 +1000

    Add project

---
+++ sources/Google.Solutions.Platform.Test/Class1.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Google.Solutions.Platform.Test
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test()
+        {
+        }
+    }
+}
