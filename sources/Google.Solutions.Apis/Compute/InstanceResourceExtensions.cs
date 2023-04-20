bca04e09ac6183c3f4621a1c066eb57c79549662
Johannes Passing <jpassing@hotmail.com>
2023-04-20 10:48:09 +1000

    Move SerialPortStream to Apis project

---
+++ sources/Google.Solutions.Apis/Compute/InstanceResourceExtensions.cs
@@ -0,0 +1,103 @@
+﻿//
+// Copyright 2019 Google LLC
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+using Google.Apis.Compute.v1;
+using Google.Solutions.Apis.Locator;
+using Google.Solutions.Common.Diagnostics;
+using Google.Solutions.Common.Text;
+using Google.Solutions.Common.Util;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Google.Solutions.Apis.Compute
+{
+    public static class InstanceResourceExtensions
+    {
+        public static IAsyncReader<string> OpenSerialPort(
+            this InstancesResource instancesResource,
+            InstanceLocator instance,
+            ushort port)
+        {
+            return new SerialPortStream(
+                instancesResource.ExpectNotNull(nameof(instancesResource)),
+                instance.ExpectNotNull(nameof(instance)),
+                port);
+        }
+
+        //---------------------------------------------------------------------
+        // Inner classes.
+        //---------------------------------------------------------------------
+
+        private class SerialPortStream : IAsyncReader<string>
+        {
+            private readonly InstancesResource instancesResource;
+            private readonly InstanceLocator instance;
+            private readonly ushort port;
+
+            //
+            // Offset of next character to be read.
+            //
+            private long nextOffset = 0;
+
+            public SerialPortStream(
+                InstancesResource instancesResource,
+                InstanceLocator instanceRef,
+                ushort port)
+            {
+                this.instancesResource = instancesResource;
+                this.port = port;
+                this.instance = instanceRef;
+            }
+
+            public void Dispose()
+            {
+            }
+
+            public async Task<string> ReadAsync(CancellationToken token)
+            {
+                using (ApiTraceSources.Google.TraceMethod().WithParameters(this.nextOffset))
+                {
+                    var request = this.instancesResource.GetSerialPortOutput(
+                        this.instance.ProjectId,
+                        this.instance.Zone,
+                        this.instance.Name);
+                    request.Port = this.port;
+                    request.Start = this.nextOffset;
+                    var output = await request.ExecuteAsync(token).ConfigureAwait(false);
+
+                    ApiTraceSources.Google.TraceVerbose(
+                        "Read {0} chars from serial port [start={1}, next={2}]",
+                        output.Contents == null ? 0 : output.Contents.Length,
+                        output.Start.Value,
+                        output.Next.Value);
+
+                    //
+                    // If there is no new data, then output.Next == this.nextOffset
+                    // and output.Contents is an empty string. We never return null
+                    // because this is an end-less stream.
+                    //
+                    this.nextOffset = output.Next.Value;
+                    return output.Contents;
+                }
+            }
+        }
+    }
+}
