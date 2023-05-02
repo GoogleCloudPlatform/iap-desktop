a390c91d76bd75ac36b3697fefb1e052b2f0f1b9
Johannes Passing <jpassing@hotmail.com>
2023-05-02 16:53:41 +1000

    b/275473315 Fix tab coloring, creating sessions by URL (#966)
    * Stay on window thread when creating session
    * Apply tab color to SSH sessions

--- sources/Google.Solutions.IapDesktop.Extensions.Shell/Views/Session/SessionBroker.cs
+++ sources/Google.Solutions.IapDesktop.Extensions.Shell/Views/Session/SessionBroker.cs
@@ -31,6 +31,7 @@ using System;
 using System.Diagnostics;
 using System.Net;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using WeifenLuo.WinFormsUI.Docking;
 
 namespace Google.Solutions.IapDesktop.Extensions.Shell.Views.Session
@@ -114,6 +115,8 @@ namespace Google.Solutions.IapDesktop.Extensions.Shell.Views.Session
             RdpSessionParameters parameters,
             RdpCredential credential)
         {
+            Debug.Assert(this.mainForm.IsWindowThread());
+
             var window = this.toolWindowHost.GetToolWindow<RemoteDesktopView, RemoteDesktopViewModel>();
 
             window.ViewModel.Instance = transport.Instance;
@@ -151,6 +154,8 @@ namespace Google.Solutions.IapDesktop.Extensions.Shell.Views.Session
             SshSessionParameters parameters,
             SshCredential credential)
         {
+            Debug.Assert(this.mainForm.IsWindowThread());
+
             var window = this.toolWindowHost.GetToolWindow<SshTerminalView, SshTerminalViewModel>();
 
             window.ViewModel.Instance = transport.Instance;
@@ -160,6 +165,15 @@ namespace Google.Solutions.IapDesktop.Extensions.Shell.Views.Session
             window.ViewModel.ConnectionTimeout = parameters.ConnectionTimeout;
 
             var session = window.Bind();
+
+            //
+            // Apply accent color if the session deviates from the norm.
+            //
+            if (parameters.TransportType == Transport.TransportType.Vpc)
+            {
+                session.DockHandler.TabAccentColor = AccentColorForNonIapSessions;
+            }
+
             window.Show();
 
             await session.ConnectAsync()
