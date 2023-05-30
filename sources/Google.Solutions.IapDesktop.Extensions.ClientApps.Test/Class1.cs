64e73654c3d33bed51e72d0f060d9e9be266b3ac
Johannes Passing <jpassing@hotmail.com>
2023-05-30 10:55:08 +1000

    Add ClientApps project

---
+++ sources/Google.Solutions.IapDesktop.Extensions.ClientApps.Test/Class1.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Google.Solutions.IapDesktop.Extensions.ClientApps.Test
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.Fail("test");
+        }
+    }
+}
