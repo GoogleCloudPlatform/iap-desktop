a2940a82e25ec51a618dab7b4303578d10e7c0c6
Johannes Passing <jpassing@hotmail.com>
2023-06-16 15:54:56 +1000

    b/287556296 Refactor NetworkCredentialExtensions (#1022)
    * Move to Platform project
    * Add extension methods for handling UPN and NetBios usernames

---
+++ sources/Google.Solutions.Platform.Test/Net/TestNetworkCredentialExtensions.cs
@@ -0,0 +1,225 @@
+﻿//
+// Copyright 2020 Google LLC
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+using Google.Solutions.Platform.Net;
+using NUnit.Framework;
+using System.Net;
+using System.Security.Policy;
+
+namespace Google.Solutions.Platform.Test.Net
+{
+    [TestFixture]
+    public class TestNetworkCredentialExtensions
+    {
+        //---------------------------------------------------------------------
+        // Normalize.
+        //---------------------------------------------------------------------
+
+        [Test]
+        public void WhenUserInUpnFormat_ThenNormalizeReturnsCredential()
+        {
+            var normalized = new NetworkCredential(
+                "user@example.org",
+                "pwd",
+                string.Empty).Normalize();
+
+            Assert.AreEqual("user@example.org", normalized.UserName);
+            Assert.AreEqual(string.Empty, normalized.Domain);
+        }
+
+        [Test]
+        public void WhenUserInNetBiosFormat_ThenNormalizeReturnsCredential()
+        {
+            var normalized = new NetworkCredential(
+                "EXAMPLE\\user",
+                "pwd",
+                string.Empty).Normalize();
+
+            Assert.AreEqual("EXAMPLE\\user", normalized.UserName);
+            Assert.AreEqual(string.Empty, normalized.Domain);
+        }
+
+        [Test]
+        public void WhenDomainSpecifiedSeparately_ThenNormalizeReturnsCredential()
+        {
+            var normalized = new NetworkCredential(
+                "user",
+                "pwd",
+                "EXAMPLE").Normalize();
+
+            Assert.AreEqual("EXAMPLE\\user", normalized.UserName);
+            Assert.AreEqual(string.Empty, normalized.Domain);
+        }
+
+        [Test]
+        public void WhenDomainMissing_ThenNormalizeReturnsCredential()
+        {
+            var normalized = new NetworkCredential(
+                "user",
+                "pwd",
+                null).Normalize();
+
+            Assert.AreEqual("localhost\\user", normalized.UserName);
+            Assert.AreEqual(string.Empty, normalized.Domain);
+        }
+
+        //---------------------------------------------------------------------
+        // IsUpnFormat.
+        //---------------------------------------------------------------------
+
+        [Test]
+        public void IsUpnFormat()
+        {
+            var cred1 = new NetworkCredential()
+            {
+                UserName = "user@domain"
+            };
+
+            Assert.IsTrue(cred1.IsUpnFormat());
+            Assert.IsFalse(cred1.IsNetBiosFormat());
+        }
+
+        //---------------------------------------------------------------------
+        // IsNetBiosFormat.
+        //---------------------------------------------------------------------
+
+        [Test]
+        public void IsNetBiosFormat()
+        {
+            var cred1 = new NetworkCredential()
+            {
+                UserName = "domain\\user",
+            };
+            var cred2 = new NetworkCredential()
+            {
+                UserName = "user",
+                Domain = "domain"
+            };
+            var cred3 = new NetworkCredential()
+            {
+                UserName = "user",
+            };
+
+            Assert.IsTrue(cred1.IsNetBiosFormat());
+            Assert.IsTrue(cred2.IsNetBiosFormat());
+            Assert.IsTrue(cred3.IsNetBiosFormat());
+
+            Assert.IsFalse(cred1.IsUpnFormat());
+            Assert.IsFalse(cred2.IsUpnFormat());
+            Assert.IsFalse(cred3.IsUpnFormat());
+        }
+
+        //---------------------------------------------------------------------
+        // GetDomainComponent.
+        //---------------------------------------------------------------------
+
+        [Test]
+        public void GetDomainComponent()
+        {
+            //
+            // UPN format: username@domain.
+            //
+
+            Assert.AreEqual("domain", new NetworkCredential()
+            {
+                UserName = "user@domain",
+            }.GetDomainComponent());
+            Assert.AreEqual("", new NetworkCredential()
+            {
+                UserName = "user@",
+            }.GetDomainComponent());
+
+            //
+            // NetBIOS format: domain\username.
+            //
+            Assert.AreEqual("domain", new NetworkCredential()
+            {
+                UserName = "domain\\user",
+            }.GetDomainComponent());
+            Assert.AreEqual("", new NetworkCredential()
+            {
+                UserName = "\\user",
+            }.GetDomainComponent());
+
+            //
+            // Decomposed format.
+            //
+            Assert.AreEqual("domain", new NetworkCredential()
+            {
+                UserName = "user",
+                Domain = "domain"
+            }.GetDomainComponent());
+
+            Assert.AreEqual("", new NetworkCredential()
+            {
+                UserName = "user",
+            }.GetDomainComponent());
+        }
+
+
+        //---------------------------------------------------------------------
+        // GetUserComponent.
+        //---------------------------------------------------------------------
+
+        [Test]
+        public void GetUserComponent()
+        {
+            //
+            // UPN format: username@domain.
+            //
+
+            Assert.AreEqual("user", new NetworkCredential()
+            {
+                UserName = "user@domain",
+            }.GetUserComponent());
+            Assert.AreEqual("user", new NetworkCredential()
+            {
+                UserName = "user@",
+            }.GetUserComponent());
+
+            //
+            // NetBIOS format: domain\username.
+            //
+            Assert.AreEqual("user", new NetworkCredential()
+            {
+                UserName = "domain\\user",
+            }.GetUserComponent());
+            Assert.AreEqual("", new NetworkCredential()
+            {
+                UserName = "user\\",
+            }.GetUserComponent());
+
+            //
+            // Decomposed format.
+            //
+            Assert.AreEqual("user", new NetworkCredential()
+            {
+                UserName = "user",
+                Domain = "domain"
+            }.GetUserComponent());
+
+            Assert.AreEqual("user", new NetworkCredential()
+            {
+                UserName = "user",
+            }.GetUserComponent());
+        }
+    }
+}
---
+++ sources/Google.Solutions.Platform/Net/NetworkCredentialExtensions.cs
@@ -0,0 +1,157 @@
+﻿//
+// Copyright 2022 Google LLC
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+
+namespace Google.Solutions.Platform.Net
+{
+    public static class NetworkCredentialExtensions
+    {
+        /// <summary>
+        /// Normalize username to user@domain or DOMAIN\user format.
+        /// </summary>
+        public static NetworkCredential Normalize(
+            this NetworkCredential credential)
+        {
+            if (string.IsNullOrEmpty(credential.UserName))
+            {
+                throw new ArgumentException("username");
+            }
+            else if (credential.UserName.Contains('@'))
+            {
+                //
+                // UPN format: username@domain.
+                //
+                // Leave as is, but clear domain field.
+                //
+                Debug.Assert(string.IsNullOrEmpty(credential.Domain));
+                return new NetworkCredential(
+                    credential.UserName,
+                    credential.Password);
+            }
+            else if (credential.UserName.Contains('\\'))
+            {
+                //
+                // NetBIOS format: domain\username.
+                //
+                // Leave as is, but clear domain field.
+                //
+                Debug.Assert(string.IsNullOrEmpty(credential.Domain));
+                return new NetworkCredential(
+                    credential.UserName,
+                    credential.Password);
+            }
+            else if (!string.IsNullOrEmpty(credential.Domain))
+            {
+                //
+                // Convert to NetBIOS format: domain\username.
+                //
+                return new NetworkCredential(
+                    $@"{credential.Domain}\{credential.UserName}",
+                    credential.Password);
+            }
+            else
+            {
+                //
+                // Convert to NetBIOS format: domain\username.
+                //
+                return new NetworkCredential(
+                    $@"localhost\{credential.UserName}",
+                    credential.Password);
+            }
+        }
+
+        /// <summary>
+        /// Determine if the credential uses user@domain notation.
+        /// </summary>
+        public static bool IsUpnFormat(this NetworkCredential credential)
+        {
+            return credential.UserName != null && credential.UserName.Contains("@");
+        }
+
+        /// <summary>
+        /// Determine if the credential uses DOMAIN\user notation.
+        /// </summary>
+        public static bool IsNetBiosFormat(this NetworkCredential credential)
+        {
+            return !IsUpnFormat(credential);
+        }
+
+        /// <summary>
+        /// Return the DNS or NetBios domain.
+        /// </summary>
+        public static string GetDomainComponent(this NetworkCredential credential)
+        {
+            if (credential.UserName.IndexOf('@') is var atIndex && atIndex > 0)
+            {
+                //
+                // UPN format: username@domain.
+                //
+                return credential.UserName.Substring(atIndex + 1);
+            }
+            else if (credential.UserName.IndexOf('\\') is var backslashIndex && backslashIndex > 0)
+            {
+                //
+                // NetBIOS format: domain\username.
+                //
+                return credential.UserName.Substring(0, backslashIndex);
+            }
+            else
+            {
+                //
+                // Decomposed format.
+                //
+                return credential.Domain;
+            }
+        }
+
+        /// <summary>
+        /// Return the user component of the UPN or NetBios username.
+        /// </summary>
+        public static string GetUserComponent(this NetworkCredential credential)
+        {
+            if (credential.UserName.IndexOf('@') is var atIndex && atIndex > 0)
+            {
+                //
+                // UPN format: username@domain.
+                //
+                return credential.UserName.Substring(0, atIndex);
+            }
+            else if (credential.UserName.IndexOf('\\') is var backslashIndex && backslashIndex > 0)
+            {
+                //
+                // NetBIOS format: domain\username.
+                //
+                return credential.UserName.Substring(backslashIndex + 1);
+            }
+            else
+            {
+                //
+                // Decomposed format.
+                //
+                return credential.UserName;
+            }
+        }
+    }
+}
