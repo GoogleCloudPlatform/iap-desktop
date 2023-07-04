a37442d6dfb1c3dff357b569224bd93c2b7bf653
Johannes Passing <jpassing@hotmail.com>
2023-07-05 09:27:20 +1000

    b/289861037 Change placeholder syntax (#1044)
    Use {x} instead of $x$ for placeholders in configuration files

--- sources/Google.Solutions.IapDesktop.Core/ClientModel/Protocol/AppProtocolClient.cs
+++ sources/Google.Solutions.IapDesktop.Core/ClientModel/Protocol/AppProtocolClient.cs
@@ -72,9 +72,9 @@ namespace Google.Solutions.IapDesktop.Core.ClientModel.Protocol
         /// Optional: Arguments to be passed. Arguments can contain the
         /// following placeholders:
         /// 
-        ///   $port$:     the local port to connect to
-        ///   $host$:     the locat IP address to connect to
-        ///   $username$: the username to authenticate with (can be empty)
+        ///   {port}:     the local port to connect to
+        ///   {host}:     the locat IP address to connect to
+        ///   {username}: the username to authenticate with (can be empty)
         ///   
         /// </summary>
         internal string ArgumentsTemplate { get; }
@@ -121,13 +121,13 @@ namespace Google.Solutions.IapDesktop.Core.ClientModel.Protocol
                 }
 
                 //
-                // NB. We use $ instead of % here to prevent
+                // NB. We use {x} instead of %x% here to prevent
                 // clashes with envirnment variables.
                 //
                 arguments = arguments
-                    .Replace("$port$", transport.Endpoint.Port.ToString())
-                    .Replace("$host$", transport.Endpoint.Address.ToString())
-                    .Replace("$username$", parameters.PreferredUsername ?? string.Empty);
+                    .Replace("{port}", transport.Endpoint.Port.ToString())
+                    .Replace("{host}", transport.Endpoint.Address.ToString())
+                    .Replace("{username}", parameters.PreferredUsername ?? string.Empty);
             }
 
             return arguments;
@@ -141,7 +141,7 @@ namespace Google.Solutions.IapDesktop.Core.ClientModel.Protocol
         public bool IsUsernameRequired
         {
             get => this.ArgumentsTemplate != null && 
-                this.ArgumentsTemplate.Contains("$username$");
+                this.ArgumentsTemplate.Contains("{username}");
         }
     }
 }
