c237ab4022a605621395a017ba76e2e293e05eb4
Johannes Passing <jpassing@hotmail.com>
2023-07-12 13:10:44 +1000

    Add UpdatePolicy

---
+++ sources/Google.Solutions.IapDesktop.Application.Test/Host/TestUpdatePolicy.cs
@@ -0,0 +1,226 @@
+﻿//
+// Copyright 2023 Google LLC
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+using Google.Apis.Util;
+using Google.Solutions.Apis.Auth;
+using Google.Solutions.IapDesktop.Application.Host;
+using Google.Solutions.IapDesktop.Application.Host.Adapters;
+using Google.Solutions.IapDesktop.Application.Windows.Dialog;
+using Moq;
+using NUnit.Framework;
+using System;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+namespace Google.Solutions.IapDesktop.Application.Test.Host
+{
+    [TestFixture]
+    public class TestUpdatePolicy
+    {
+        private Mock<IAuthorization> CreateAuthorization(string email)
+        {
+            var authorization = new Mock<IAuthorization>();
+            authorization.SetupGet(a => a.Email).Returns(email);
+            return authorization;
+        }
+
+        private static IClock CreateClock(DateTime dateTime)
+        {
+            var clock = new Mock<IClock>();
+            clock.SetupGet(c => c.UtcNow).Returns(dateTime);
+            return clock.Object;
+        }
+
+        //---------------------------------------------------------------------
+        // GetReleaseTrack.
+        //---------------------------------------------------------------------
+
+        [Test]
+        public void WhenDescriptionContainsNoTag_ThenGetReleaseTrackReturnsNormal(
+            [Values(null, "", "some description")] string description)
+        {
+            var authorization = CreateAuthorization("bob@example.com");
+            var policy = new UpdatePolicy(
+                authorization.Object,
+                CreateClock(DateTime.Now));
+
+            var release = new Mock<IGitHubRelease>();
+            release.SetupGet(r => r.Description).Returns(description);
+
+            Assert.AreEqual(ReleaseTrack.Normal, policy.GetReleaseTrack(release.Object));
+        }
+
+        [Test]
+        public void WhenDescriptionContainsCriticalTag_ThenGetReleaseTrackReturnsCritical()
+        {
+            var authorization = CreateAuthorization("bob@example.com");
+            var policy = new UpdatePolicy(
+                authorization.Object,
+                CreateClock(DateTime.Now));
+
+            var description = "This release is [track:critical]!!1!";
+
+            var release = new Mock<IGitHubRelease>();
+            release.SetupGet(r => r.Description).Returns(description);
+
+            Assert.AreEqual(ReleaseTrack.Critical, policy.GetReleaseTrack(release.Object));
+        }
+
+        [Test]
+        public void WhenDescriptionContainsRapidTag_ThenGetReleaseTrackReturnsRapid()
+        {
+            var authorization = CreateAuthorization("bob@example.com");
+            var policy = new UpdatePolicy(
+                authorization.Object,
+                CreateClock(DateTime.Now));
+
+            var description = "This release is on the [track:rapid] track!!1!";
+
+            var release = new Mock<IGitHubRelease>();
+            release.SetupGet(r => r.Description).Returns(description);
+
+            Assert.AreEqual(ReleaseTrack.Rapid, policy.GetReleaseTrack(release.Object));
+        }
+
+        [Test]
+        public void WhenDescriptionContainsOptionalTag_ThenGetReleaseTrackReturnsOptional()
+        {
+            var authorization = CreateAuthorization("bob@example.com");
+            var policy = new UpdatePolicy(
+                authorization.Object,
+                CreateClock(DateTime.Now));
+
+            var description = "This release is [track:optional]";
+
+            var release = new Mock<IGitHubRelease>();
+            release.SetupGet(r => r.Description).Returns(description);
+
+            Assert.AreEqual(ReleaseTrack.Optional, policy.GetReleaseTrack(release.Object));
+        }
+
+        //---------------------------------------------------------------------
+        // FollowedTracks.
+        //---------------------------------------------------------------------
+
+        [Test]
+        public void WhenUserFromListedDomains_ThenFollowedTracksIncludesRapid(
+            [Values(
+                "_@gmail.com",
+                "_@GOOGLE.com",
+                "_@x.joonix.Net",
+                "_@x.altostrat.COM")] string email)
+        {
+
+            var authorization = CreateAuthorization(email);
+            var policy = new UpdatePolicy(
+                authorization.Object,
+                CreateClock(DateTime.Now));
+
+            Assert.AreEqual(
+                ReleaseTrack.Critical | ReleaseTrack.Normal | ReleaseTrack.Rapid,
+                policy.FollowedTracks);
+        }
+
+        [Test]
+        public void WhenUserFromOtherDomains_ThenFollowedTracksExcludesRapid(
+            [Values(
+                "_@x.gmail.com",
+                "_@example.com")] string email)
+        {
+            var authorization = CreateAuthorization(email);
+            var policy = new UpdatePolicy(
+                authorization.Object,
+                CreateClock(DateTime.Now));
+
+            Assert.AreEqual(
+                ReleaseTrack.Critical | ReleaseTrack.Normal,
+                policy.FollowedTracks);
+        }
+
+        //---------------------------------------------------------------------
+        // IsUpdateAdvised.
+        //---------------------------------------------------------------------
+
+        [Test]
+        public void WhenReleaseOnFollowedReleaseTrack_ThenIsUpdateAdvisedReturnsTrue()
+        {
+            var rapidPolicy = new UpdatePolicy(
+                CreateAuthorization("_@x.joonix.Net").Object,
+                CreateClock(DateTime.Now));
+            var normalPolicy = new UpdatePolicy(
+                CreateAuthorization("_@example.net").Object,
+                CreateClock(DateTime.Now));
+
+            var normalRelease = new Mock<IGitHubRelease>();
+            var criticalRelease = new Mock<IGitHubRelease>();
+            var optionalRelease = new Mock<IGitHubRelease>();
+            var rapidRelease = new Mock<IGitHubRelease>();
+
+            normalRelease.SetupGet(r => r.Description).Returns("");
+            criticalRelease.SetupGet(r => r.Description).Returns("[track:critical]");
+            optionalRelease.SetupGet(r => r.Description).Returns("[track:optional]");
+            rapidRelease.SetupGet(r => r.Description).Returns("[track:rapid]");
+
+            Assert.IsTrue(normalPolicy.IsUpdateAdvised(normalRelease.Object));
+            Assert.IsTrue(normalPolicy.IsUpdateAdvised(criticalRelease.Object));
+            Assert.IsFalse(normalPolicy.IsUpdateAdvised(rapidRelease.Object));
+            Assert.IsFalse(normalPolicy.IsUpdateAdvised(optionalRelease.Object));
+
+            Assert.IsTrue(rapidPolicy.IsUpdateAdvised(normalRelease.Object));
+            Assert.IsTrue(rapidPolicy.IsUpdateAdvised(criticalRelease.Object));
+            Assert.IsTrue(rapidPolicy.IsUpdateAdvised(rapidRelease.Object));
+            Assert.IsFalse(rapidPolicy.IsUpdateAdvised(optionalRelease.Object));
+        }
+
+        //---------------------------------------------------------------------
+        // IsUpdateCheckDue.
+        //---------------------------------------------------------------------
+
+        [Test]
+        public void WhenNotEnoughDaysElapsed_ThenIsUpdateCheckDueReturnsFalse()
+        {
+            var now = new DateTime(2022, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+            var authorization = CreateAuthorization("bob@example.com");
+            var policy = new UpdatePolicy(
+                authorization.Object,
+                CreateClock(now));
+
+            Assert.IsFalse(policy.IsUpdateCheckDue(now));
+            Assert.IsFalse(policy.IsUpdateCheckDue(now.AddYears(1)));
+            Assert.IsFalse(policy.IsUpdateCheckDue(now.AddDays(-UpdatePolicy.DaysBetweenUpdateChecks).AddMinutes(1)));
+            Assert.IsFalse(policy.IsUpdateCheckDue(now.AddDays(-UpdatePolicy.DaysBetweenUpdateChecks + 1)));
+        }
+
+        [Test]
+        public void WhenTooManyDaysElapsed_ThenIsUpdateCheckDueReturnsTrue()
+        {
+            var now = new DateTime(2022, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+            var authorization = CreateAuthorization("bob@example.com");
+            var policy = new UpdatePolicy(
+                authorization.Object,
+                CreateClock(now));
+
+            Assert.IsTrue(policy.IsUpdateCheckDue(now.AddDays(-UpdatePolicy.DaysBetweenUpdateChecks)));
+            Assert.IsTrue(policy.IsUpdateCheckDue(now.AddDays(-UpdatePolicy.DaysBetweenUpdateChecks - 1)));
+        }
+    }
+}
---
+++ sources/Google.Solutions.IapDesktop.Application/Host/UpdatePolicy.cs
@@ -0,0 +1,129 @@
+﻿using Google.Apis.Util;
+using Google.Solutions.Apis.Auth;
+using Google.Solutions.Common.Util;
+using Google.Solutions.IapDesktop.Application.Host.Adapters;
+using System;
+
+
+namespace Google.Solutions.IapDesktop.Application.Host
+{
+    /// <summary>
+    /// Policy that determines how often to check for updates,
+    /// and which updates to apply.
+    /// </summary>
+    internal class UpdatePolicy
+    {
+        /// <summary>
+        /// Determines how often update checks are performed. 
+        /// A higher number implies a slower pace of updates.
+        /// </summary>
+        public const int DaysBetweenUpdateChecks = 10;
+
+        private readonly IClock clock;
+
+        public UpdatePolicy(
+            IAuthorization authorization,
+            IClock clock)
+        {
+            authorization.ExpectNotNull(nameof(authorization));
+
+            this.clock = clock.ExpectNotNull(nameof(clock));
+
+            //
+            // Only install critical and normal updates by default.
+            //
+            this.FollowedTracks = ReleaseTrack.Critical | ReleaseTrack.Normal;
+
+            if (authorization.Email.EndsWith("@gmail.com", StringComparison.OrdinalIgnoreCase) ||
+                authorization.Email.EndsWith("@google.com", StringComparison.OrdinalIgnoreCase) ||
+                authorization.Email.EndsWith(".joonix.net", StringComparison.OrdinalIgnoreCase) ||
+                authorization.Email.EndsWith(".altostrat.com", StringComparison.OrdinalIgnoreCase))
+            {
+                //
+                // Opt-in these domains to the rapid track.
+                //
+
+                this.FollowedTracks |= ReleaseTrack.Rapid;
+            }
+        }
+
+        /// <summary>
+        /// Release track followed by this user/machine.
+        /// </summary>
+        public ReleaseTrack FollowedTracks { get; }
+
+        /// <summary>
+        /// Determine which release track a release belongs to.
+        /// </summary>
+        public ReleaseTrack GetReleaseTrack(IGitHubRelease release)
+        {
+            //
+            // GitHub doesn't let us "tag" releases in a good way,
+            // so we look for a tag in the description text.
+            //
+            var description = release.Description ?? string.Empty;
+
+            if (description.Contains("[track:critical]"))
+            {
+                return ReleaseTrack.Critical;
+            }
+            else if (description.Contains("[track:rapid]"))
+            {
+                return ReleaseTrack.Rapid;
+            }
+            else if (description.Contains("[track:optional]"))
+            {
+                return ReleaseTrack.Optional;
+            }
+            else
+            {
+                return ReleaseTrack.Normal;
+            }
+        }
+
+        /// <summary>
+        /// Determine if the user should be advised to install an update.
+        /// </summary>
+        public bool IsUpdateAdvised(IGitHubRelease release) //TODO: Rename to IRelease
+        {
+            Precondition.ExpectNotNull(release, nameof(release));
+
+            return this.FollowedTracks.HasFlag(GetReleaseTrack(release));
+        }
+
+        /// <summary>
+        /// Determine if an update check should be performed.
+        /// </summary>
+        public bool IsUpdateCheckDue(DateTime lastCheck)
+        {
+            return (this.clock.UtcNow - lastCheck).TotalDays >= DaysBetweenUpdateChecks;
+        }
+    }
+
+    /// <summary>
+    /// Available release tracks.
+    /// </summary>
+    [Flags]
+    internal enum ReleaseTrack : uint //TODO: Move to separate file.
+    {
+        /// <summary>
+        /// Critical security updates.
+        /// </summary>
+        Critical = 1,
+
+        /// <summary>
+        /// Normal feature updates (later stage).
+        /// </summary>
+        Normal = 2,
+
+        /// <summary>
+        /// Normal feature updates (early stage).
+        /// </summary>
+        Rapid = 4,
+
+        /// <summary>
+        /// Optional feature updates that might only be relevant to few.
+        /// </summary>
+        Optional = 8,
+    }
+}
