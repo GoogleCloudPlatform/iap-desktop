9df266b37b30373050fff393e280f5b607c487aa
Johannes Passing <jpassing@hotmail.com>
2023-07-22 12:27:10 +1000

    b/292306118 Add class for decoding JWTs
    Alternative to GoogleJsonWebSignature that can be used to decode
    JWTs without verifying them at the same time.

---
+++ sources/Google.Solutions.Apis/Auth/DecodedGoogleJsonWebToken.cs
@@ -0,0 +1,80 @@
+﻿//
+// Copyright 2023 Google LLC
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+using Google.Apis.Auth;
+using Google.Apis.Json;
+using Google.Solutions.Common.Format;
+using Google.Solutions.Common.Util;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Google.Solutions.Apis.Auth
+{
+    /// <summary>
+    /// A decoded but unverified Google JWT.
+    /// </summary>
+    internal class DecodedGoogleJsonWebToken
+    {
+        public GoogleJsonWebSignature.Header Header;
+        public GoogleJsonWebSignature.Payload Payload;
+
+        private DecodedGoogleJsonWebToken(
+            GoogleJsonWebSignature.Header header,
+            GoogleJsonWebSignature.Payload payload)
+        {
+            this.Header = header;
+            this.Payload = payload;
+        }
+
+        /// <summary>
+        /// Decode, but don't verify, a JSON web token.
+        /// </summary>
+        public static DecodedGoogleJsonWebToken Decode(string token)
+        {
+            token.ExpectNotEmpty(nameof(token));
+
+            var tokenParts = token.Split('.');
+            if (tokenParts.Length != 3)
+            {
+                throw new InvalidJwtException(
+                    "The JWT must consist of header, payload, and signature");
+            }
+
+            var encodedHeader = tokenParts[0];
+            var encodedPayload = tokenParts[1];
+
+            try
+            {
+                var header = NewtonsoftJsonSerializer.Instance.Deserialize<GoogleJsonWebSignature.Header>(
+                    Encoding.UTF8.GetString(Base64UrlEncoding.Decode(encodedHeader)));
+                var payload = NewtonsoftJsonSerializer.Instance.Deserialize<GoogleJsonWebSignature.Payload>(
+                    Encoding.UTF8.GetString(Base64UrlEncoding.Decode(encodedPayload)));
+
+                return new DecodedGoogleJsonWebToken(header, payload);
+            } 
+            catch (JsonException e)
+            {
+                throw new InvalidJwtException(
+                    "The JWT contains malformed JSON data");
+            }
+        }
+    }
+}
---
+++ sources/Google.Solutions.Common/Format/Base64UrlEncoding.cs
@@ -0,0 +1,67 @@
+﻿//
+// Copyright 2019 Google LLC
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+using System;
+
+namespace Google.Solutions.Common.Format
+{
+    /// <summary>
+    /// Base64 URL encoding.
+    /// </summary>
+    public class Base64UrlEncoding
+    {
+        public static string Encode(byte[] data)
+        {
+            string base64 = Convert.ToBase64String(data);
+
+            //
+            // Strip trailing '='.
+            //
+            base64 = base64.Split('=')[0];
+
+            return base64
+                .Replace('+', '-')
+                .Replace('/', '_');
+        }
+
+        public static byte[] Decode(string encoded)
+        {
+            string base64 = encoded
+                .Replace('-', '+')
+                .Replace('_', '/');
+
+            //
+            // Add trailing '='.
+            //
+            switch (base64.Length % 4)
+            {
+                case 2:
+                    base64 += "==";
+                    break;
+                case 3:
+                    base64 += "=";
+                    break;
+            }
+
+            return Convert.FromBase64String(base64);
+        }
+    }
+}
