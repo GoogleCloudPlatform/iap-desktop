ca3fad45625b1a473c3e1196508e7899aca2c503
Johannes Passing <jpassing@hotmail.com>
2023-08-02 18:33:42 +1000

    b'293373828 Generate release notes (#1081)
    * Add script to generate release notes
    * Run script in Actions workflow

---
+++ doc/site/sources/generate-releasenotes.ps1
@@ -0,0 +1,17 @@
+﻿$Releases = Invoke-RestMethod `
+    -Uri "https://api.github.com/repos/GoogleCloudPlatform/iap-desktop/releases?per_page=30"
+
+Write-Output "# What's new"
+
+$Releases | % { 
+    $PublishedAt = ([datetime]$_.published_at).ToLongDateString()
+    Write-Output "## Release $($_.tag_name)"
+    Write-Output ""
+    Write-Output "$($_.body)"
+    Write-Output ""
+    Write-Output "_Published on $($PublishedAt)._ "
+    Write-Output "_[More details :octicons-link-external-16:](https://github.com/GoogleCloudPlatform/iap-desktop/releases/tag/$($_.tag_name))._"
+    Write-Output ""
+}
+
+Write-Output "[See all releases](https://github.com/GoogleCloudPlatform/iap-desktop/releases)"
