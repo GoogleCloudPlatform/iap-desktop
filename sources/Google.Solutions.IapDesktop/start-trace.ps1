4ecfb2d6e8cbb435e6c0d62c421aa82c7f0c829d
Johannes Passing <jpassing@hotmail.com>
2023-09-08 10:01:07 +1000

    b/299529838 Add script to control ETW trace session (#1122)
    * Add start-trace.ps1 to installer
    * Use internal and relevant system providers

--- sources/Google.Solutions.IapDesktop/start-trace.ps1
+++ sources/Google.Solutions.IapDesktop/start-trace.ps1
@@ -1,5 +1,5 @@
 #
-# Copyright 2013 Google LLC
+# Copyright 2023 Google LLC
 #
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
@@ -24,14 +24,25 @@ $ErrorActionPreference = "stop"
 
 $TraceName = "IapDesktop"
 $Providers = @{
+	#
+	# Internal providers.
+	#
 	"Google-Solutions-Apis" = "{EC3585B8-5C28-42AE-8CE7-D76CB00303C6}"
 	"Google-IapDesktop-Application" = "{4B23296B-C25A-449C-91F2-897BDABAA1A8}"
+
+	#
+	# Relevant system providers.
+	#
+	"Microsoft-Windows-TerminalServices-ClientActiveXCore" = "{28AA95BB-D444-4719-A36F-40462168127E}"
 }
 
 #
 # Create session and enable all providers.
 #
-& logman create trace $TraceName -ets -o IapDesktop.etl | Out-Default
+$Timestamp = Get-Date -Format "yyyy-MM-dd_HHmm"
+$Filename = "IapDesktop_$($Timestamp).etl"
+
+& logman create trace $TraceName -ets -o $Filename | Out-Default
 $Providers.Values | % { &logman update trace -ets $TraceName -p $_ | Out-Default }
 
 #
@@ -39,8 +50,8 @@ $Providers.Values | % { &logman update trace -ets $TraceName -p $_ | Out-Default
 #
 & logman query $TraceName -ets | Out-Default
 
-Write-Host "Trace session started. Press enter to stop." -ForegroundColor Yellow
+Write-Host "Trace session started ($Filename). Press enter to stop." -ForegroundColor Yellow
 Read-Host
 
 & logman stop   $TraceName -ets | Out-Default
-Write-Host "Trace session stopped." -ForegroundColor Yellow
+Write-Host "Trace session stopped ($Filename)." -ForegroundColor Yellow
