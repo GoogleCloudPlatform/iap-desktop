f2f0f1368829c8c5d08f7a567ec6e8e18a1ffb49
Johannes Passing <jpassing@hotmail.com>
2023-10-11 15:24:47 +1100

    Scaffold dpiawareness-theme

---
+++ sources/Google.Solutions.Mvvm.Test/Theme/TestDpiAwarenessRuleset.cs
@@ -0,0 +1,28 @@
+﻿using Google.Solutions.Testing.Apis.Integration;
+using NUnit.Framework;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Google.Solutions.Mvvm.Test.Theme
+{
+    [TestFixture]
+    [InteractiveTest]
+    [Apartment(ApartmentState.STA)]
+    public partial class TestDpiAwarenessRuleset : Form
+    {
+        public TestDpiAwarenessRuleset()
+        {
+            InitializeComponent();
+        }
+
+        [Test]
+        public void TestUi()
+        {
+            using (var form = new TestDpiAwarenessRuleset())
+            {
+                Application.EnableVisualStyles();
+                Application.Run(form);
+            }
+        }
+    }
+}
---
+++ sources/Google.Solutions.Mvvm/Theme/DpiAwarenessRuleset.cs
@@ -0,0 +1,29 @@
+﻿using Google.Solutions.Common.Util;
+using System.Windows.Forms;
+
+namespace Google.Solutions.Mvvm.Theme
+{
+    /// <summary>
+    /// Theming rules for DPI-awareness.
+    /// </summary>
+    public class DpiAwarenessRuleset : ControlTheme.IRuleSet
+    {
+        //---------------------------------------------------------------------
+        // Theming rules.
+        //---------------------------------------------------------------------
+
+        private void ResizeControl(Control c)
+        { }
+
+        //---------------------------------------------------------------------
+        // IRuleSet
+        //---------------------------------------------------------------------
+
+        public void AddRules(ControlTheme controlTheme)
+        {
+            controlTheme.ExpectNotNull(nameof(controlTheme));
+
+            controlTheme.AddRule<Control>(ResizeControl);
+        }
+    }
+}
