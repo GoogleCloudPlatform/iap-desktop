507ba213eace0b54b775ac37e98bdd36431e2d15
Johannes Passing <jpassing@hotmail.com>
2023-11-05 08:14:41 +0100

    Add CngKeyName

---
+++ sources/Google.Solutions.IapDesktop.Extensions.Session.Test/Protocol/Ssh/TestCngKeyName.cs
@@ -0,0 +1,99 @@
+﻿//
+// Copyright 2023 Google LLC
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+using Google.Solutions.Apis.Auth;
+using Google.Solutions.IapDesktop.Extensions.Session.Protocol.Ssh;
+using Google.Solutions.Ssh.Cryptography;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Security.Cryptography;
+
+namespace Google.Solutions.IapDesktop.Extensions.Session.Test.Protocol.Ssh
+{
+    [TestFixture]
+    public class TestCngKeyName
+    {
+        //---------------------------------------------------------------------
+        // Value.
+        //---------------------------------------------------------------------
+
+        [Test]
+        public void RsaWithDefaultProvider()
+        {
+            var session = new Mock<IOidcSession>();
+            session.SetupGet(s => s.Username).Returns("user");
+
+            var name = new CngKeyName(
+                session.Object,
+                SshKeyType.Rsa3072,
+                CngProvider.MicrosoftSoftwareKeyStorageProvider);
+
+            Assert.AreEqual("IAPDESKTOP_user", name.Value);
+            Assert.AreEqual("IAPDESKTOP_user", name.ToString());
+        }
+
+        [Test]
+        public void EcdsaP256WithDefaultProvider()
+        {
+            var session = new Mock<IOidcSession>();
+            session.SetupGet(s => s.Username).Returns("user");
+
+            var name = new CngKeyName(
+                session.Object,
+                SshKeyType.EcdsaNistp256,
+                CngProvider.MicrosoftSoftwareKeyStorageProvider);
+
+            Assert.AreEqual("IAPDESKTOP_user_00000011_094FE673", name.Value);
+            Assert.AreEqual("IAPDESKTOP_user_00000011_094FE673", name.ToString());
+        }
+
+        [Test]
+        public void EcdsaP384WithDefaultProvider()
+        {
+            var session = new Mock<IOidcSession>();
+            session.SetupGet(s => s.Username).Returns("user");
+
+            var name = new CngKeyName(
+                session.Object,
+                SshKeyType.EcdsaNistp384,
+                CngProvider.MicrosoftSoftwareKeyStorageProvider);
+
+            Assert.AreEqual("IAPDESKTOP_user_00000012_094FE673", name.Value);
+            Assert.AreEqual("IAPDESKTOP_user_00000012_094FE673", name.ToString());
+        }
+
+        [Test]
+        public void EcdsaP521WithDefaultProvider()
+        {
+            var session = new Mock<IOidcSession>();
+            session.SetupGet(s => s.Username).Returns("user");
+
+            var name = new CngKeyName(
+                session.Object,
+                SshKeyType.EcdsaNistp521,
+                CngProvider.MicrosoftSoftwareKeyStorageProvider);
+
+            Assert.AreEqual("IAPDESKTOP_user_00000013_094FE673", name.Value);
+            Assert.AreEqual("IAPDESKTOP_user_00000013_094FE673", name.ToString());
+        }
+    }
+}
---
+++ sources/Google.Solutions.IapDesktop.Extensions.Session/Protocol/Ssh/CngKeyName.cs
@@ -0,0 +1,83 @@
+﻿//
+// Copyright 2023 Google LLC
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+using Google.Solutions.Apis.Auth;
+using Google.Solutions.Ssh.Cryptography;
+using System;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Google.Solutions.IapDesktop.Extensions.Session.Protocol.Ssh
+{
+    /// <summary>
+    /// Name of CNG key to use for SSH authentication.
+    /// </summary>
+    [DebuggerDisplay("{Value}")]
+    internal class CngKeyName
+    {
+        public string Value { get; }
+
+        public CngKeyName(
+            IOidcSession session,
+            SshKeyType keyType,
+            CngProvider provider)
+        {
+            if (keyType == SshKeyType.Rsa3072 &&
+                provider == CngProvider.MicrosoftSoftwareKeyStorageProvider)
+            {
+                //
+                // Use backwards-compatible name.
+                //
+                this.Value = $"IAPDESKTOP_{session.Username}";
+            }
+            else
+            {
+                //
+                // Embed the key type and provider in the name. 
+                //
+                // CNG key names aren't scoped to a provider. By embedding the
+                // provider name in the key, we ensure that we don't accidentally
+                // use a key from a provider different from the one we're 
+                // expecting to use.
+                //
+                using (var sha = new SHA256Managed())
+                {
+                    //
+                    // Instead of using the full provider name (which can be
+                    // very long), hash the name and use the prefix.
+                    //
+                    var providerToken = BitConverter.ToString(
+                        sha.ComputeHash(Encoding.UTF8.GetBytes(provider.Provider)),
+                        0,
+                        4).Replace("-", string.Empty);
+
+                    this.Value = $"IAPDESKTOP_{session.Username}_{keyType:x}_{providerToken}";
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return this.Value;
+        }
+    }
+}
