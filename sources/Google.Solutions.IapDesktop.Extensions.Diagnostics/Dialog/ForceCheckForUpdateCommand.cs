ffa53f38c22fb1d5368956119f860b7e9ef55b57
Johannes Passing <jpassing@hotmail.com>
2023-12-13 13:40:32 +1100

    Add Debug command

---
+++ sources/Google.Solutions.IapDesktop.Extensions.Diagnostics/Dialog/ForceCheckForUpdateCommand.cs
@@ -0,0 +1,49 @@
+﻿//
+// Copyright 2023 Google LLC
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+using Google.Solutions.IapDesktop.Application.Diagnostics;
+using Google.Solutions.IapDesktop.Application.Host;
+using Google.Solutions.IapDesktop.Application.Profile;
+using Google.Solutions.IapDesktop.Application.ToolWindows.Update;
+using Google.Solutions.IapDesktop.Application.Windows;
+using Google.Solutions.IapDesktop.Core.ObjectModel;
+using Google.Solutions.Mvvm.Controls;
+using Google.Solutions.Platform.Net;
+using System.Windows.Forms;
+
+namespace Google.Solutions.IapDesktop.Extensions.Diagnostics.Dialog
+{
+    [MenuCommand(typeof(DebugMenu), Rank = 0x500)]
+    [Service]
+    public class ForceCheckForUpdateCommand : CheckForUpdateCommand<DebugMenu.Context>
+    {
+        public ForceCheckForUpdateCommand(
+            IWin32Window parentWindow,
+            IInstall install,
+            IUpdatePolicyFactory updatePolicyFactory,
+            IReleaseFeed feed,
+            ITaskDialog taskDialog,
+            IBrowser browser)
+            : base(parentWindow, install, updatePolicyFactory, feed, taskDialog, browser)
+        {
+        }
+    }
+}
