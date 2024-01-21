aabfc0887a205d64182099e09ebadabb0d4b74a3
Johannes Passing <jpassing@hotmail.com>
2024-01-22 08:50:11 +1100

    Add ExitCommand

--- sources/Google.Solutions.IapDesktop.Extensions.Profile/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Google.Solutions.IapDesktop.Extensions.Profile
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ sources/Google.Solutions.IapDesktop.Extensions.Profile/ExitCommand.cs
@@ -0,0 +1,37 @@
+﻿using Google.Solutions.Common.Util;
+using Google.Solutions.IapDesktop.Application.Windows;
+using Google.Solutions.IapDesktop.Core.ObjectModel;
+using System.Windows.Forms;
+
+namespace Google.Solutions.IapDesktop.Extensions.Profile
+{
+    [MenuCommand(typeof(ProfileMenu), Rank = 0x100)]
+    [Service]
+    public class ExitCommand : MenuCommandBase<ProfileMenu.Context>
+    {
+        private readonly IMainWindow mainWindow;
+
+        public ExitCommand(IMainWindow mainWindow)
+            : base("E&xit")
+        {
+            this.ShortcutKeys = Keys.Alt | Keys.F4;
+            this.mainWindow = mainWindow.ExpectNotNull(nameof(mainWindow));
+        }
+
+
+        protected override bool IsAvailable(ProfileMenu.Context context)
+        {
+            return true;
+        }
+
+        protected override bool IsEnabled(ProfileMenu.Context context)
+        {
+            return true;
+        }
+
+        public override void Execute(ProfileMenu.Context context)
+        {
+            this.mainWindow.Close();
+        }
+    }
+}
