d52e86afc608344105e32c34635c3527f32fe5a4
Johannes Passing <jpassing@hotmail.com>
2024-03-21 13:51:09 +1100

    Add DelegateCommand

---
+++ sources/Google.Solutions.Mvvm.Test/Binding/Commands/TestDelegateCommand.cs
@@ -0,0 +1,115 @@
+﻿//
+// Copyright 2024 Google LLC
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+using Google.Solutions.Mvvm.Binding.Commands;
+using Google.Solutions.Mvvm.Binding;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Google.Solutions.Mvvm.Test.Binding.Commands
+{
+    [TestFixture]
+    public class TestDelegateCommand
+    {
+        [Test]
+        public void WhenCommandSucceeds_ThenContextIsNotified()
+        {
+            void handler(object sender, EventArgs args) { }
+
+            using (var form = new Form())
+            {
+                var bindingContext = new Mock<IBindingContext>();
+                var command = new DelegateCommand<EventHandler<EventArgs>, EventArgs>(
+                    "Test",
+                    handler,
+                    bindingContext.Object);
+
+                command.Delegate(form, EventArgs.Empty);
+
+                bindingContext.Verify(
+                    ctx => ctx.OnCommandExecuted(command),
+                    Times.Once);
+            }
+        }
+
+        [Test]
+        public void WhenCommandThrowsException_ThenContextIsNotified()
+        {
+            void handler(object sender, EventArgs args) {
+                throw new ArgumentException();
+            }
+
+            using (var form = new Form())
+            {
+                var bindingContext = new Mock<IBindingContext>();
+                var command = new DelegateCommand<EventHandler<EventArgs>, EventArgs>(
+                    "Test",
+                    handler,
+                    bindingContext.Object);
+
+                command.Delegate(form, EventArgs.Empty);
+
+                bindingContext.Verify(
+                    ctx => ctx.OnCommandFailed(
+                        form,
+                        command,
+                        It.IsAny<ArgumentException>()),
+                    Times.Once);
+                bindingContext.Verify(
+                    ctx => ctx.OnCommandExecuted(command),
+                    Times.Never);
+            }
+        }
+
+        [Test]
+        public void WhenCommandThrowsTaskCancelledException_ThenContextIsNotNotified()
+        {
+            void handler(object sender, EventArgs args)
+            {
+                throw new TaskCanceledException();
+            }
+
+            using (var form = new Form())
+            {
+                var bindingContext = new Mock<IBindingContext>();
+                var command = new DelegateCommand<EventHandler<EventArgs>, EventArgs>(
+                    "Test",
+                    handler,
+                    bindingContext.Object);
+
+                command.Delegate(form, EventArgs.Empty);
+
+                bindingContext.Verify(
+                    ctx => ctx.OnCommandFailed(
+                        It.IsAny<IWin32Window>(),
+                        It.IsAny<ICommandBase>(),
+                        It.IsAny<ArgumentException>()),
+                    Times.Never);
+                bindingContext.Verify(
+                    ctx => ctx.OnCommandExecuted(command),
+                    Times.Never);
+            }
+        }
+    }
+}
---
+++ sources/Google.Solutions.Mvvm/Binding/Commands/DelegateCommand.cs
@@ -0,0 +1,72 @@
+﻿//
+// Copyright 2024 Google LLC
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+using Google.Solutions.Common.Util;
+using System;
+using System.Windows.Forms;
+
+namespace Google.Solutions.Mvvm.Binding.Commands
+{
+    /// <summary>
+    /// Command that wraps an arbitrary delegate.
+    /// 
+    /// DelegateCommands are not observable, but (unlike
+    /// ObservableCommands) can for any type of event.
+    /// </summary>
+    public class DelegateCommand<THandler, TEventArgs> : CommandBase
+        where THandler : Delegate
+        where TEventArgs : EventArgs
+    {
+        public DelegateCommand(
+            string text,
+            EventHandler<TEventArgs> handler,
+            IBindingContext bindingContext) : base(text)
+        {
+            void invokeHandler(object sender, TEventArgs args)
+            {
+                try
+                {
+                    handler(sender, args);
+                    
+                    bindingContext.OnCommandExecuted(this);
+                }
+                catch (Exception e) when (e.IsCancellation())
+                {
+                    // Ignore.
+                }
+                catch (Exception e)
+                {
+                    bindingContext.OnCommandFailed(
+                        (sender as Control)?.FindForm(),
+                        this,
+                        e);
+                }
+            }
+
+            this.Delegate = (THandler)(Delegate)(EventHandler<TEventArgs>)invokeHandler;
+        }
+
+        /// <summary>
+        /// Delegate to assign to an event handler.
+        /// </summary>
+        public THandler Delegate { get; }
+    }
+}
