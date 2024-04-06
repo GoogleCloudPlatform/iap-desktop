6811af7a6854386cc105051557451e9bed5e00d4
Johannes Passing <jpassing@hotmail.com>
2024-04-06 12:18:57 +1100

    Add ExpandableDictionaryConverter

---
+++ sources/Google.Solutions.Mvvm.Test/ComponentModel/TestExpandableDictionaryConverter.cs
@@ -0,0 +1,58 @@
+﻿//
+// Copyright 2024 Google LLC
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+using Google.Solutions.Mvvm.ComponentModel;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Google.Solutions.Mvvm.Test.ComponentModel
+{
+    [TestFixture]
+    public class TestExpandableDictionaryConverter
+    {
+        [Test]
+        public void GetProperties()
+        {
+            var dictionary = new Dictionary<string, object>()
+            {
+                { "key-a", "value-a" },
+                { "key-b", 1 }
+            };
+
+            var converter = new ExpandableDictionaryConverter();
+            var properties = converter.GetProperties(dictionary);
+
+            Assert.AreEqual(2, properties.Count);
+
+            Assert.AreEqual("key-a", properties[0].Name);
+            Assert.AreEqual(typeof(string), properties[0].PropertyType);
+            Assert.IsTrue(properties[0].IsBrowsable);
+            Assert.IsTrue(properties[0].IsReadOnly);
+            Assert.AreEqual("value-a", properties[0].GetValue(dictionary));
+
+            Assert.AreEqual("key-b", properties[1].Name);
+            Assert.AreEqual(typeof(int), properties[1].PropertyType);
+            Assert.IsTrue(properties[1].IsBrowsable);
+            Assert.IsTrue(properties[1].IsReadOnly);
+            Assert.AreEqual(1, properties[1].GetValue(dictionary));
+        }
+    }
+}
---
+++ sources/Google.Solutions.Mvvm/ComponentModel/ExpandableDictionaryConverter.cs
@@ -0,0 +1,141 @@
+﻿//
+// Copyright 2024 Google LLC
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+using Google.Solutions.Common.Util;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+
+namespace Google.Solutions.Mvvm.ComponentModel
+{
+    /// <summary>
+    /// Allows dictionaries to be expanded, similar to 
+    /// ExpandableObjectConverter.
+    /// </summary>
+    public class ExpandableDictionaryConverter : TypeConverter
+    {
+        public override PropertyDescriptorCollection GetProperties(
+            ITypeDescriptorContext? context,
+            object value,
+            Attribute[] attributes)
+        {
+            if (value is IDictionary dictionary)
+            {
+                var readOnly = context == null ||
+                    context.PropertyDescriptor
+                        .Attributes
+                        .OfType<ReadOnlyAttribute>()
+                        .Where(a => a.IsReadOnly)
+                        .Any();
+
+                var properties = new List<ItemDescriptor>();
+                foreach (DictionaryEntry item in dictionary)
+                {
+                    properties.Add(new ItemDescriptor(dictionary, item.Key, readOnly));
+                }
+
+                return new PropertyDescriptorCollection(properties.ToArray());
+            }
+            else
+            {
+                return base.GetProperties(context, value, attributes);
+            }
+        }
+
+        public override object ConvertTo(
+            ITypeDescriptorContext context,
+            CultureInfo culture,
+            object value,
+            Type destinationType)
+        {
+            if (destinationType == typeof(string) && value is IDictionary dictionary)
+            {
+                return $"{dictionary.Count} items";
+            }
+            else
+            {
+                return base.ConvertTo(context, culture, value, destinationType);
+            }
+        }
+
+        public override bool GetPropertiesSupported(ITypeDescriptorContext context)
+        {
+            return true;
+        }
+
+        private class ItemDescriptor : PropertyDescriptor
+        {
+            private readonly bool readOnly;
+            private readonly IDictionary dictionary;
+            private readonly object key;
+
+            internal ItemDescriptor(IDictionary dictionary, object key, bool readOnly)
+                : base(key?.ToString(), null)
+            {
+                this.dictionary = dictionary.ExpectNotNull(nameof(dictionary));
+                this.key = key.ExpectNotNull(nameof(key));
+                this.readOnly = readOnly;
+            }
+
+            public override Type PropertyType
+            {
+                get => this.dictionary[this.key].GetType();
+            }
+
+            public override void SetValue(object component, object value)
+            {
+                this.dictionary[this.key] = value;
+            }
+
+            public override object GetValue(object component)
+            {
+                return this.dictionary[this.key];
+            }
+
+            public override bool IsReadOnly
+            {
+                get => this.readOnly;
+            }
+
+            public override Type ComponentType
+            {
+                get => typeof(IDictionary);
+            }
+
+            public override bool CanResetValue(object component)
+            {
+                return false;
+            }
+
+            public override void ResetValue(object component)
+            {
+            }
+
+            public override bool ShouldSerializeValue(object component)
+            {
+                return false;
+            }
+        }
+    }
+}
