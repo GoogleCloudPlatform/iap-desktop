acc751c6158d72f3ae65d763140a3c5fe6400899
Johannes Passing <jpassing@hotmail.com>
2024-05-31 14:20:36 +1000

    Password_16

--- sources/Google.Solutions.IapDesktop.Extensions.Session/Properties/Resources.Designer.cs
+++ sources/Google.Solutions.IapDesktop.Extensions.Session/Properties/Resources.Designer.cs
@@ -173,16 +173,6 @@ namespace Google.Solutions.IapDesktop.Extensions.Session.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap Key_32 {
-            get {
-                object obj = ResourceManager.GetObject("Key_32", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap Refresh_16 {
             get {
                 object obj = ResourceManager.GetObject("Refresh_16", resourceCulture);
@@ -196,16 +186,6 @@ namespace Google.Solutions.IapDesktop.Extensions.Session.Properties {
         internal static System.Drawing.Bitmap Settings_16 {
             get {
                 object obj = ResourceManager.GetObject("Settings_16", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap Spinner {
-            get {
-                object obj = ResourceManager.GetObject("Spinner", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
