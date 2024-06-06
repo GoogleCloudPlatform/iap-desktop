891b4df2ac7bcbec71a6a8a763f4a9e932294f56
Johannes Passing <jpassing@hotmail.com>
2024-06-06 13:10:10 +1000

    Handle Ctrl+Ins to copy

--- sources/Google.Solutions.Terminal.Test/Controls/TestVirtualTerminal.cs
+++ sources/Google.Solutions.Terminal.Test/Controls/TestVirtualTerminal.cs
@@ -1,17 +1,32 @@
-﻿using Google.Solutions.Platform.IO;
+﻿//
+// Copyright 2024 Google LLC
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+using Google.Solutions.Platform.IO;
 using Google.Solutions.Terminal.Controls;
-using Google.Solutions.Testing.Apis;
-using Google.Solutions.Testing.Apis.Integration;
 using Moq;
 using NUnit.Framework;
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Drawing;
-using System.Linq;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Google.Solutions.Terminal.Test.Controls
