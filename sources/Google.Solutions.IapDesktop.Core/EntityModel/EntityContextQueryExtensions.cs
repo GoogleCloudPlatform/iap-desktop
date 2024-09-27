2a3b887a1659ab64a306e797be3cfe4b3dbc335e
Johannes Passing <jpassing@hotmail.com>
2024-09-27 10:26:03 +1000

    Draft EntityContextQueryExtensions

---
+++ sources/Google.Solutions.IapDesktop.Core/EntityModel/EntityContextQueryExtensions.cs
@@ -0,0 +1,56 @@
+﻿using Google.Solutions.Apis.Locator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Google.Solutions.IapDesktop.Core.EntityModel
+{
+    // TODO: Implement
+    public static class EntityContextQueryExtensions
+    {
+        public static Task<AspectQuery<TEntity>> Search<TQuery, TEntity>(
+            this EntityContext context,
+            TQuery query,
+            CancellationToken cancellationToken)
+            where TEntity : IEntity
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Task<AspectQuery<TEntity>> Expand<TEntity>(
+            this EntityContext context,
+            ILocator locator,
+            CancellationToken cancellationToken)
+            where TEntity : IEntity
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class AspectQuery<TEntity>
+        where TEntity : IEntity
+    {
+        public AspectQuery<TEntity> Select<TAspect>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<EntityProjection<TEntity>>> ExecuteAsync(
+            CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class EntityProjection<TEntity>
+        where TEntity : IEntity
+    {
+        public TAspect Get<TAspect>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
