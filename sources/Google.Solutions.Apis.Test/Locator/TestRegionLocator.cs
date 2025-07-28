38d8def001c3645c3cfdb65691d28b7711370a0c
Johannes Passing <jpassing@hotmail.com>
2025-07-28 12:29:05 +1000

    Add region locator

---
+++ sources/Google.Solutions.Apis.Test/Locator/TestRegionLocator.cs
@@ -0,0 +1,200 @@
+﻿//
+// Copyright 2019 Google LLC
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+using Google.Solutions.Apis.Locator;
+using NUnit.Framework;
+using System;
+
+namespace Google.Solutions.Apis.Test.Locator
+{
+    [TestFixture]
+    public class TestRegionLocator
+        : TestLocatorFixtureBase<RegionLocator>
+    {
+        protected override RegionLocator CreateInstance()
+        {
+            return new RegionLocator("project-1", "region-1");
+        }
+
+        [Test]
+        public void Project()
+        {
+            var ref1 = new RegionLocator("project-1", "region-1");
+            Assert.AreEqual(ref1.ProjectId, ref1.Project.Name);
+        }
+
+        //---------------------------------------------------------------------
+        // TryParse.
+        //---------------------------------------------------------------------
+
+        [Test]
+        public void TryParse_WhenPathIsValid()
+        {
+            Assert.IsTrue(RegionLocator.TryParse(
+                "projects/project-1/regions/us-central1",
+                out var ref1));
+
+            Assert.IsNotNull(ref1);
+            Assert.AreEqual("regions", ref1!.ResourceType);
+            Assert.AreEqual("us-central1", ref1.Name);
+            Assert.AreEqual("project-1", ref1.ProjectId);
+        }
+
+        [Test]
+        public void TryParse_WhenQualifiedByComputeGoogleapisHost()
+        {
+            Assert.IsTrue(RegionLocator.TryParse(
+                "https://compute.googleapis.com/compute/v1/projects/project-1/regions/us-central1",
+                out var ref1));
+
+            Assert.IsNotNull(ref1);
+            Assert.AreEqual("regions", ref1!.ResourceType);
+            Assert.AreEqual("us-central1", ref1.Name);
+            Assert.AreEqual("project-1", ref1.ProjectId);
+        }
+
+        [Test]
+        public void TryParse_WhenQualifiedByGoogleapisHost()
+        {
+            Assert.IsTrue(RegionLocator.TryParse(
+                "https://www.googleapis.com/compute/v1/projects/project-1/regions/us-central1",
+                out var ref1));
+
+            Assert.IsNotNull(ref1);
+            Assert.AreEqual("regions", ref1!.ResourceType);
+            Assert.AreEqual("us-central1", ref1.Name);
+            Assert.AreEqual("project-1", ref1.ProjectId);
+        }
+
+        [Test]
+        public void TryParse_WhenPathLacksProject()
+        {
+            Assert.IsFalse(RegionLocator.TryParse(
+                "/project-1/project-1/regions/us-central1",
+                out var _));
+        }
+
+        [Test]
+        public void TryParse_WhenPathInvalid()
+        {
+            Assert.IsFalse(RegionLocator.TryParse(
+                "projects/project-1/region/us-central1",
+                out var _));
+            Assert.IsFalse(RegionLocator.TryParse(
+                "projects/project-1/regions",
+                out var _));
+        }
+
+        //---------------------------------------------------------------------
+        // Parse.
+        //---------------------------------------------------------------------
+
+        [Test]
+        public void Parse_WhenPathIsValid()
+        {
+            var ref1 = RegionLocator.Parse(
+                "projects/project-1/regions/us-central1");
+
+            Assert.AreEqual("regions", ref1.ResourceType);
+            Assert.AreEqual("us-central1", ref1.Name);
+            Assert.AreEqual("project-1", ref1.ProjectId);
+        }
+
+        [Test]
+        public void Parse_WhenQualifiedByComputeGoogleapisHost()
+        {
+            var ref1 = RegionLocator.Parse(
+                "https://compute.googleapis.com/compute/v1/projects/project-1/regions/us-central1");
+
+            Assert.AreEqual("regions", ref1.ResourceType);
+            Assert.AreEqual("us-central1", ref1.Name);
+            Assert.AreEqual("project-1", ref1.ProjectId);
+        }
+
+        [Test]
+        public void Parse_WhenQualifiedByGoogleapisHost()
+        {
+            var ref1 = RegionLocator.Parse(
+                "https://www.googleapis.com/compute/v1/projects/project-1/regions/us-central1");
+
+            Assert.AreEqual("regions", ref1.ResourceType);
+            Assert.AreEqual("us-central1", ref1.Name);
+            Assert.AreEqual("project-1", ref1.ProjectId);
+        }
+
+        [Test]
+        public void Parse_WhenPathLacksProject()
+        {
+            Assert.Throws<ArgumentException>(() => RegionLocator.Parse(
+                "/project-1/project-1/regions/us-central1"));
+        }
+
+        [Test]
+        public void Parse_WhenPathInvalid()
+        {
+            Assert.Throws<ArgumentException>(() => RegionLocator.Parse(
+                "projects/project-1/region/us-central1"));
+            Assert.Throws<ArgumentException>(() => RegionLocator.Parse(
+                "projects/project-1/regions"));
+        }
+
+        //---------------------------------------------------------------------
+        // Equality.
+        //---------------------------------------------------------------------
+
+        [Test]
+        public void Equals_WhenObjectsNotEquivalent_ThenEqualsReturnsFalse()
+        {
+            var ref1 = new RegionLocator("proj-1", "us-central1");
+            var ref2 = new RegionLocator("proj-2", "us-central1");
+
+            Assert.IsFalse(ref1.Equals(ref2));
+            Assert.IsFalse(ref1.Equals((object)ref2));
+            Assert.IsFalse(ref1 == ref2);
+            Assert.IsTrue(ref1 != ref2);
+        }
+
+        //---------------------------------------------------------------------
+        // ToString.
+        //---------------------------------------------------------------------
+
+        [Test]
+        public void ToString_WhenCreatedFromPath_ThenToStringReturnsPath()
+        {
+            var path = "projects/project-1/regions/us-central1";
+
+            Assert.AreEqual(
+                path,
+                RegionLocator.Parse(path).ToString());
+        }
+
+        [Test]
+        public void ToString_WhenCreatedFromUrl_ThenToStringReturnsPath()
+        {
+            var path = "projects/project-1/regions/us-central1";
+
+            Assert.AreEqual(
+                path,
+                RegionLocator.Parse(
+                    "https://www.googleapis.com/compute/v1/" + path).ToString());
+        }
+    }
+}
---
+++ sources/Google.Solutions.Apis/Locator/RegionLocator.cs
@@ -0,0 +1,122 @@
+﻿//
+// Copyright 2025 Google LLC
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+using System;
+using System.Text.RegularExpressions;
+
+namespace Google.Solutions.Apis.Locator
+{
+    /// <summary>
+    /// Locator for regions. These locators are global, but differ from
+    /// other global locators by not having a "global/" part in the URL.
+    /// </summary>
+    public class RegionLocator : ComputeEngineLocator, IEquatable<RegionLocator>
+    {
+        public override string ResourceType => "regions";
+
+        public RegionLocator(string projectId, string name)
+            : base(projectId, name)
+        {
+        }
+
+        public RegionLocator(ProjectLocator project, string name)
+            : this(project.Name, name)
+        {
+        }
+
+        public static bool TryParse(string path, out RegionLocator? locator)
+        {
+            path = StripUrlPrefix(path);
+
+            var match = new Regex("(?:/compute/beta/)?projects/(.*)/regions/(.*)")
+                .Match(path);
+            if (match.Success)
+            {
+                locator = new RegionLocator(
+                    match.Groups[1].Value,
+                    match.Groups[2].Value);
+                return true;
+            }
+            else
+            {
+                locator = null;
+                return false;
+            }
+        }
+
+        public static RegionLocator Parse(string path)
+        {
+            if (TryParse(path, out var locator))
+            {
+                return locator!;
+            }
+            else
+            {
+                throw new ArgumentException(
+                    $"'{path}' is not a valid region locator");
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return
+                this.ProjectId.GetHashCode() ^
+                this.Name.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"projects/{this.ProjectId}/{this.ResourceType}/{this.Name}";
+        }
+
+        public bool Equals(RegionLocator? other)
+        {
+            return other is object &&
+                this.Name == other.Name &&
+                this.ProjectId == other.ProjectId;
+        }
+
+        public override bool Equals(ComputeEngineLocator? obj)
+        {
+            return obj is RegionLocator locator && Equals(locator);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is RegionLocator locator && Equals(locator);
+        }
+
+        public static bool operator ==(RegionLocator? obj1, RegionLocator? obj2)
+        {
+            if (obj1 is null)
+            {
+                return obj2 is null;
+            }
+
+            return obj1.Equals(obj2);
+        }
+
+        public static bool operator !=(RegionLocator? obj1, RegionLocator? obj2)
+        {
+            return !(obj1 == obj2);
+        }
+    }
+}
